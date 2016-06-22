176ff8def525ea998b10593165aa42ae2ca000ab
igboyes <igboyes@gmail.com>
2016-06-22 10:06:58 -0700

    Remove useless <Icon />

--- client/src/js/components/Main/samples/Detail/analysis/NuVs/ORF/Item.jsx
+++ client/src/js/components/Main/samples/Detail/analysis/NuVs/ORF/Item.jsx
@@ -9,11 +9,16 @@ var ORFItem = React.createClass({
 
     render: function () {
 
-        var arrow = <Icon name={'arrow-' + (this.props.strand === 1 ? 'right': 'left')} />;
-
+        var icon = (
+            <Icon
+                name={(this.props.strand === 1 ? 'plus': 'minus') + '-square'}
+                bsStyle={this.props.strand === 1 ? 'primary': 'danger'}
+            />
+        );
+        
         return (
             <ListGroupItem>
-                <h5>{this.props.frame} {arrow} ORF {this.props.index}.{this.props.orf_index}</h5>
+                <h5>{this.props.frame} {icon} ORF {this.props.index}.{this.props.orf_index}</h5>
                 <Input type='textarea' value={this.props.pro} className='sequence' readOnly />
             </ListGroupItem>
         );
