549c9773bdd5fa28f3e531f843c7402556785f6c
igboyes <igboyes@gmail.com>
2016-07-06 14:10:47 -0700

    Change app.js to reflect component directory changes

--- client/src/js/app.js
+++ client/src/js/app.js
@@ -4,7 +4,7 @@
 
 var React = require('react');
 var ReactDOM = require('react-dom');
-var Start = require('./components/Start/Start.jsx');
+var Start = require('./components/Start.jsx');
 
 require('indexeddbshim');
 
