a2721ab2850790a82b447523f139200013734bf1
Ian Boyes <igboyes@gmail.com>
2016-07-22 11:36:08 -0700

    Add modal for checking HMM file consistency with database

---
+++ client/src/js/components/Viruses/HMM/Errors.jsx
@@ -0,0 +1,139 @@
+/**
+ * @license
+ * The MIT License (MIT)
+ * Copyright 2015 Government of Canada
+ *
+ * @author
+ * Ian Boyes
+ *
+ * @exports HMMFiles
+ */
+
+'use strict';
+
+var React = require('react');
+var Alert = require('react-bootstrap/lib/Alert');
+var ListGroup = require('react-bootstrap/lib/ListGroup');
+var ListGroupItem = require('react-bootstrap/lib/ListGroupItem');
+
+var Icon = require('virtool/js/components/Base/Icon.jsx');
+var Flex = require('virtool/js/components/Base/Flex.jsx');
+var Button = require('virtool/js/components/Base/PushButton.jsx');
+var Utils = require('virtool/js/Utils');
+
+var makeSpecifier = function (value, noun) {
+    return [(value === 1 ? "is": "are"), Utils.numberToWord(value), noun + (value === 1 ? "": "s")].join(" ")
+};
+
+/**
+ * A component the contains child components that modify certain general options. A small explanation of each
+ * subcomponent is also rendered.
+ */
+var HMMErrors = React.createClass({
+
+    propTypes: {
+        errors: React.PropTypes.object
+    },
+
+    render: function () {
+
+        var errorComponents = [];
+
+        _.forIn(this.props.errors, function (value, error) {
+
+            var message;
+
+            var alertStyle = "danger";
+
+            var button = (
+                <Button onClick={this.props.retrieveFiles}>
+                    <Icon name="reset"/> Refresh
+                </Button>
+            );
+
+            if (error === "hmm_dir" && value) {
+                message = "The HMM directory could not be found.";
+            }
+
+            if (error === "hmm_file" && value) {
+                message = (
+                    <span>
+                        <strong>The file <code>vFam.hmm</code> could not be found. </strong>
+                        Please add it to the HMM data directory and refresh to see changes.
+                    </span>
+                );
+            }
+
+            if (error === "press" && value) {
+                message = (
+                    <span>
+                        <strong>One or more pressed HMM files are missing. </strong>
+                        Repairing this problem will call <code>hmmpress</code> on the existing HMM files.
+                    </span>
+                );
+
+                button = (
+                    <Flex.Item grow={0} shrink={0} pad={15}>
+                        <Button disabled={this.props.reconciling} onClick={this.props.press}>
+                            <Icon name="hammer" pending={this.props.pressing} /> Repair
+                        </Button>
+                    </Flex.Item>
+                );
+            }
+
+            if (error === "not_in_database" && value) {
+                message = (
+                    <span>
+                        <strong>There {makeSpecifier(value, "profiles")} in the HMM file that do not have annotations in the database.</strong>
+                        <span> Ensure the annotation database and HMM file match. This cannot be done automatically.</span>
+                    </span>
+                );
+            }
+
+            if (error === "not_in_file" && value) {
+                alertStyle = "warning";
+
+                message = (
+                    <span>
+                        <strong>There are {makeSpecifier(value, "annotation")} in the database for which no profiles exist in the HMM file.</strong>
+                        <span> Repairing this problem will remove extra annotations from the database.</span>
+                    </span>
+                );
+
+                button = (
+                    <Flex.Item grow={0} shrink={0} pad={15}>
+                        <Button disabled={this.props.reconciling} onClick={this.props.press}>
+                            <Icon name="hammer" pending={this.props.pressing} /> Repair
+                        </Button>
+                    </Flex.Item>
+                );
+            }
+
+            if (message) {
+                errorComponents.push(
+                    <Alert key={error} bsStyle={alertStyle}>
+                        <Flex alignItems="center">
+                            <Flex.Item grow={1} shrink={1}>
+                                {message}
+                            </Flex.Item>
+
+                            {button}
+                        </Flex>
+                    </Alert>
+                );
+            }
+        }.bind(this));
+
+        return (
+            <div>
+                {this.props.files.length > 0 ? <h5><strong>Errors</strong></h5>: null}
+                {errorComponents}
+            </div>
+        );
+
+
+
+    }
+});
+
+module.exports = HMMErrors;
---
+++ client/src/js/components/Viruses/HMM/Files.jsx
@@ -0,0 +1,145 @@
+/**
+ * @license
+ * The MIT License (MIT)
+ * Copyright 2015 Government of Canada
+ *
+ * @author
+ * Ian Boyes
+ *
+ * @exports HMMFiles
+ */
+
+'use strict';
+
+var React = require('react');
+var Row = require('react-bootstrap/lib/Row');
+var Col = require('react-bootstrap/lib/Col');
+var Modal = require('react-bootstrap/lib/Modal');
+var Alert = require('react-bootstrap/lib/Alert');
+var ListGroup = require('react-bootstrap/lib/ListGroup');
+var ListGroupItem = require('react-bootstrap/lib/ListGroupItem');
+
+var Icon = require('virtool/js/components/Base/Icon.jsx');
+var Flex = require('virtool/js/components/Base/Flex.jsx');
+var Button = require('virtool/js/components/Base/PushButton.jsx');
+var ByteSize = require('virtool/js/components/Base/ByteSize.jsx');
+
+var HMMErrors = require("./Errors.jsx");
+
+/**
+ * A component the contains child components that modify certain general options. A small explanation of each
+ * subcomponent is also rendered.
+ */
+var HMMFiles = React.createClass({
+
+    propTypes: {
+        show: React.PropTypes.bool.isRequired,
+        onHide: React.PropTypes.func.isRequired
+    },
+
+    getInitialState: function () {
+        return {
+            files: null,
+            errors: null,
+
+            pressing: false,
+            reconciling: false
+        };
+    },
+
+    checkFiles: function () {
+        this.setState(this.getInitialState(), function () {
+            dispatcher.collections.hmm.request("check_files", null, this.onChecked);
+        });
+    },
+
+    onChecked: function (data) {
+        this.setState({
+            files: data.files,
+            errors: data.errors,
+        });
+    },
+
+    press: function () {
+        this.setState({pressing: true}, function () {
+            dispatcher.collections.hmm.request("press", null, this.onPressed);
+        });
+    },
+
+    onPressed: function () {
+        this.setState({pressing: false}, this.checkFiles);
+    },
+
+    reset: function (callback) {
+        this.setState(this.getInitialState(), callback);
+    },
+
+    render: function () {
+
+        var content;
+
+        if (this.state.files || this.state.errors) {
+
+            var files;
+
+            if (this.state.files.length > 0) {
+                var fileComponents = _.sortBy(this.state.files, "_id").map(function (file, index) {
+                    return (
+                        <ListGroupItem key={index}>
+                            {file._id}
+                        </ListGroupItem>
+                    );
+                });
+
+                files = (
+                    <div>
+                        <h5><strong>HMM Files</strong></h5>
+                        <ListGroup>
+                            {fileComponents}
+                        </ListGroup>
+                    </div>
+                );
+            }
+
+            var errors = this.state.errors.length === 0 ? null: (
+                <HMMErrors
+                    {...this.state}
+                    press={this.press}
+                    checkFiles={this.checkFiles}
+                />
+            );
+
+            content = (
+                <div>
+                    {files}
+                    {errors}
+                </div>
+            );
+        } else {
+            content = (
+                <div className="text-center">
+                    <Icon name="spinner" pending /> Loading
+                </div>
+            );
+        }
+
+        return (
+            <Modal {...this.props} onEntered={this.checkFiles} onExited={this.reset}>
+
+                <Modal.Header {...this.props} closeButton>
+                    HMM Files
+                </Modal.Header>
+
+                <Modal.Body>
+                    {content}
+                </Modal.Body>
+
+            </Modal>
+        );
+
+
+
+    }
+});
+
+module.exports = HMMFiles;
