5e1678a06672160ddbade4b7b442e2cc266ee31c
Ian Boyes <igboyes@gmail.com>
2016-12-01 09:04:23 -0800

    Move list of Virtool collections to separate module

---
+++ virtool/collections.py
@@ -0,0 +1,13 @@
+#: The names of all collections registered with the dispatcher.
+COLLECTIONS = [
+    "jobs",
+    "samples",
+    "analyses",
+    "viruses",
+    "hmm",
+    "history",
+    "indexes",
+    "hosts",
+    "groups",
+    "users"
+]
