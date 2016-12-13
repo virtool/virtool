ff0641577ae3b39523c943f86e98f58474942af2
Ian Boyes <igboyes@gmail.com>
2016-12-13 10:59:53 -0800

    Add socket mocks

--- virtool/tests/mock_socket.py
+++ virtool/tests/mock_socket.py
@@ -1,6 +1,9 @@
+import tornado.websocket
+
+
 class MockSocket:
 
-    def __init__(self, settings):
+    def __init__(self, settings, missing_method=None):
         self.handle_message = settings["handle_message"]
         self.add_connection = settings["add_connection"]
         self.remove_connection = settings["remove_connection"]
@@ -17,11 +20,23 @@ class MockSocket:
 
         self.messages = []
 
+        self._closed = False
+
+        if missing_method:
+            getattr(self, missing_method)
+
     def check_origin(self, origin):
         return True
 
     def open(self):
         self.add_connection(self)
+
+    def close(self):
+        if self._closed is True:
+            raise tornado.websocket.WebSocketClosedError
+        else:
+            self._closed = True
+            self.on_close()
 
     def on_message(self, message):
         self.handle_message(message, self)
