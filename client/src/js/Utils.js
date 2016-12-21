64dfdb339f7a15c61d6b875042a9b751e035a3ef
Ian Boyes <igboyes@gmail.com>
2016-12-21 14:34:16 -0800

    Removed unneeded standard deviation function from utils.js

--- client/src/js/Utils.js
+++ client/src/js/Utils.js
@@ -1,6 +1,7 @@
-var _ = require('lodash');
-var SuperAgent = require('superagent');
-var Numeral = require('numeral');
+import { capitalize, map, sum, difference, findIndex } from "lodash";
+
+import SuperAgent from "superagent";
+import Numeral from "numeral";
 
 var VTUtils = {
 
@@ -21,12 +22,12 @@ var VTUtils = {
             isolate.sourceType && isolate.sourceType !== 'unknown'
         ) {
             return (
-                _.capitalize(isolate.source_type || isolate.sourceType) + ' '
+                capitalize(isolate.source_type || isolate.sourceType) + ' '
                 + (isolate.source_name || isolate.sourceName)
             );
         }
 
-        return 'Unamed';
+        return 'Unnamed';
     },
 
     square: function (x) {
@@ -40,12 +41,7 @@ var VTUtils = {
     },
 
     mean: function (values) {
-        return _.sum(values) / values.length;
-    },
-
-    standardDeviation: function (values) {
-        var sumOfSquares = _.sum(this.squares(values));
-        return Math.sqrt((sumOfSquares / values.length) - this.square(mean(values)));
+        return sum(values) / values.length;
     },
 
     postJSON: function (uri, data, callback) {
@@ -64,13 +60,13 @@ var VTUtils = {
         // In this case a new document has been added and should become the new activeId.
         if (newDocuments.length > oldDocuments.length) {
             // Find the new document id.
-            return _.difference(_.map(newDocuments, '_id'), _.map(oldDocuments, '_id'))[0];
+            return difference(map(newDocuments, '_id'), map(oldDocuments, '_id'))[0];
         }
 
         // Remove a user.
         if (newDocuments.length < oldDocuments.length) {
             // Find the index of the user document with the current activeId.
-            var activeIndex = _.findIndex(oldDocuments, {_id: currentActiveId});
+            var activeIndex = findIndex(oldDocuments, {_id: currentActiveId});
 
             if (activeIndex >= newDocuments.length) activeIndex -= 1;
 
