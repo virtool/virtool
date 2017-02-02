14aab773575a93da01bab851b79294c44395e67e
Ian Boyes <igboyes@gmail.com>
2017-02-02 15:04:48 -0800

    Move viruses module collection organization functions to organize module :

--- virtool/organize.py
+++ virtool/organize.py
@@ -1,3 +1,4 @@
+import os
 import virtool.utils
 
 from virtool.permissions import PERMISSIONS
@@ -70,12 +71,130 @@ def organize_analyses(database):
     database.analyses.remove({"ready": False})
 
 
+def get_bowtie2_index_names(index_path):
+    """
+    Get the headers of all the FASTA sequences used to build the Bowtie2 index in *index_path*.
+    *Requires Bowtie2 in path.*
+
+    :param index_path: the patch to the Bowtie2 index.
+    :return: list of FASTA headers.
+    """
+    try:
+        inspect = subprocess.check_output(["bowtie2-inspect", "-n", index_path], stderr=subprocess.DEVNULL)
+    except subprocess.CalledProcessError:
+        return None
+
+    inspect_list = str(inspect, "utf-8").split("\n")
+    inspect_list.remove("")
+
+    return inspect_list
+
+
+def extract_sequence_ids(joined_virus):
+    """
+    Get the ids of all sequences in a joined virus.
+
+    :param joined_virus: a joined virus comprising the virus document and its associated sequences.
+    :return: a list of sequence ids.
+
+    """
+    sequence_ids = list()
+
+    for isolate in joined_virus["isolates"]:
+        for sequence in isolate["sequences"]:
+            sequence_ids.append(sequence["_id"])
+
+    return sequence_ids
+
+
 def organize_viruses(database):
     database.viruses.update({}, {
         "$unset": {
             "segments": ""
         }
     }, multi=True)
+
+    indexes_path = os.path.join(data_path, "reference/viruses/")
+
+    response = {
+        "missing_index": False,
+        "mismatched_index": False,
+        "missing_history": list(),
+        "missing_recent_history": list(),
+        "orphaned_analyses": os.listdir(indexes_path)
+    }
+
+    ref_names = None
+
+    # Get the entry describing the most recently built (active) index from the DB.
+    try:
+        active_index = db.indexes.find({"ready": True}).sort("index_version", -1)[0]
+    except IndexError:
+        active_index = None
+
+    # Check that there is an active index.
+    if active_index:
+        active_index_path = os.path.join(indexes_path, active_index["_id"])
+
+        # Set missing index to True if we can find the directory for the active index on disk.
+        response["missing_index"] = not os.path.exists(active_index_path)
+
+        # This key-value is initially a list of all indexes on disk. Remove the index_id of the active index.
+        try:
+            response["orphaned_analyses"].remove(active_index["_id"])
+        except ValueError:
+            pass
+
+        # Get the FASTA headers of all the sequences used to build the reference.
+        ref_names = get_bowtie2_index_names(os.path.join(active_index_path, "reference"))
+
+    sequence_ids = list()
+
+    for virus in db.viruses.find({}):
+        default_isolate = get_default_isolate(virus)
+
+        sequences = list(db.sequences.find({"isolate_id": default_isolate["isolate_id"]}))
+
+        patched_and_joined = merge_virus(virus, sequences)
+
+        if virus["_version"] > 0:
+            # Get all history entries associated with the virus entry.
+            history = list(db.history.find({"entry_id": virus["_id"]}).sort("entry_version", -1))
+
+            # If this tests true, the virus has a greater version number than can be accounted for by the history. This
+            # is not a fatal problem.
+            if virus["_version"] > len(history):
+                response["missing_history"].append(virus["_id"])
+
+            # If the virus entry version is higher than the last_indexed_version, check that the unbuilt changes are
+            # stored in history. Also patch the virus back to its last_index_version state and store in patched_viruses.
+            if virus["last_indexed_version"] != virus["_version"]:
+                # The number of virus entry versions between the current version and the last_indexed_version.
+                required_unbuilt_change_count = int(virus["_version"] - virus["last_indexed_version"])
+
+                # Count the number of history entries containing unbuilt changes for this virus.
+                recent_history = virtool.utils.where(history, lambda x: x["index_version"] == x["unbuilt"])
+
+                # The two previously assigned variables must be equal. Otherwise the virus_id will be added to the
+                # missing_recent_history list in the response dict returned by this function.
+                if required_unbuilt_change_count != len(recent_history):
+                    response["missing_recent_history"].append(virus["_id"])
+
+                _, patched_and_joined, _ = virus(patched_and_joined, recent_history)
+
+        sequence_ids += extract_sequence_ids(patched_and_joined)
+
+    sequence_id_set = set(sequence_ids)
+
+    response["duplicate_sequence_ids"] = len(sequence_id_set) < len(sequence_ids)
+
+    if ref_names:
+        response["mismatched_index"] = sequence_id_set != set(ref_names)
+
+    response["failed"] = response["missing_index"] or response["mismatched_index"] or response[
+        "missing_recent_history"]
+
+    return response
 
 
 def organize_hosts(database):
