fa4c40a5292c8e37b6d19d08730661e48e64728f
Ian Boyes <igboyes@gmail.com>
2017-03-28 08:35:20 -0700

    Add __init__.py to pathoscope

--- virtool/pathoscope/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from virtool.pathoscope import em
-from virtool.pathoscope import matrix
-from virtool.pathoscope import reassign
-from virtool.pathoscope import report
-from virtool.pathoscope import sam
-from virtool.pathoscope import subtract
