a9f9b27cc6a6482f11b36aa9399ce775eb1182a3
Ian Boyes <igboyes@gmail.com>
2017-03-29 14:11:14 -0700

    Clean up fixtures

--- virtool/tests/fixtures.py
+++ virtool/tests/fixtures.py
@@ -1,19 +1,26 @@
 import os
 import pytest
-import virtool.gen
+import pymongo
 
+from virtool.web import create_app
 from virtool.permissions import PERMISSIONS
 
-from virtool.tests.mock_settings import MockSettings
-from virtool.tests.mock_interface import EmptyInterface, MockInterface
-from virtool.tests.mock_connection import MockConnection
-from virtool.tests.mock_transaction import MockTransaction
+
+@pytest.fixture
+def test_db():
+    client = pymongo.MongoClient()
+    yield client["test"]
+    client.drop_database("test")
 
 
-@pytest.fixture(scope="session")
-def static_time():
-    from datetime import datetime
-    return datetime(2000, 1, 1)
+@pytest.fixture
+def all_permissions():
+    return {permission: True for permission in PERMISSIONS}
+
+
+@pytest.fixture
+def no_permissions():
+    return {permission: False for permission in PERMISSIONS}
 
 
 @pytest.fixture(scope="session")
@@ -26,50 +33,7 @@ def data_dir(tmpdir_factory):
 
     return mock_dir
 
-
-@pytest.fixture
-def coroutine_stub(mocker):
-    def create(name):
-        stub = mocker.stub(name=name)
-
-        @virtool.gen.coroutine
-        def decorated(*args, **kwargs):
-            return stub(*args, **kwargs)
-
-        decorated.stub = stub
-
-        return decorated
-
-    return create
-
-
-@pytest.fixture
-def mock_collection(mocker):
-    class MockCollection:
-
-        def __init__(self):
-            self.stubs = dict()
-
-        def add_method(self, name):
-            stub = mocker.stub(name=name)
-            self.stubs[name] = stub
-
-            setattr(self, name, stub)
-
-        def add_coroutine(self, name):
-            stub = mocker.stub(name=name)
-
-            self.stubs[name] = stub
-
-            @virtool.gen.coroutine
-            def coroutine(*args, **kwargs):
-                return stub(*args, **kwargs)
-
-            setattr(self, name, coroutine)
-
-    return MockCollection()
-
-
+'''
 @pytest.fixture()
 def mock_settings(mock_pymongo, mock_motor):
     settings = MockSettings()
@@ -82,16 +46,7 @@ def mock_settings(mock_pymongo, mock_motor):
     settings.get_db_client = get_db_client
 
     return settings
-
-
-@pytest.fixture()
-def empty_interface(called_tester):
-    return EmptyInterface
-
-
-@pytest.fixture()
-def mock_interface():
-    return MockInterface
+'''
 
 
 @pytest.fixture(scope="session")
@@ -118,67 +73,3 @@ def user():
         }
 
     return create_user
-
-
-@pytest.fixture(scope="function")
-def mock_connection(user, called_tester):
-    def create_connection(source=None, username="test", permissions="all", administrator=False, authorized=True,
-                          ip="127.0.0.1", auto=True):
-
-        method_keys = ["add_connection", "remove_connection", "handle"]
-
-        if isinstance(source, dict):
-            web_settings = {key: source[key] for key in method_keys}
-        elif source is None:
-            web_settings = {key: called_tester() for key in method_keys}
-        else:
-            web_settings = {key: getattr(source, key) for key in method_keys}
-
-        new_user = user(
-            name=username,
-            permissions=permissions,
-            administrator=administrator
-        )
-
-        conn = MockConnection(web_settings, new_user, authorized, ip)
-
-        if auto:
-            conn.open()
-
-        return conn
-
-    return create_connection
-
-
-@pytest.fixture
-def mock_transaction(mock_connection):
-    def create_transaction(message, username="test", permissions=None, administrator=False, authorized=True,
-                           ip="127.0.0.1"):
-        connection = mock_connection(
-            username=username,
-            permissions=permissions,
-            administrator=administrator,
-            authorized=authorized,
-            ip=ip
-        )
-        return MockTransaction(message, connection)
-
-    return create_transaction
-
-
-@pytest.fixture(scope="session")
-def called_tester():
-
-    class Func:
-
-        def __init__(self):
-            self.was_called = False
-            self.with_args = None
-            self.with_kwargs = None
-
-        def __call__(self, *args, **kwargs):
-            self.was_called = True
-            self.with_args = args
-            self.with_kwargs = kwargs
-
-    return Func
