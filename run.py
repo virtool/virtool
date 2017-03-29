9e90fcc7322b882000d5ae3c31afcc4abb767843
Ian Boyes <igboyes@gmail.com>
2017-03-29 11:39:57 -0700

    Clean up run.py

--- run.py
+++ run.py
@@ -2,20 +2,20 @@ import os
 import sys
 import uvloop
 import asyncio
-import virtool.args
-import virtool.logs
 
 from aiohttp import web
 from setproctitle import setproctitle
 from virtool.web import create_app
+from virtool.logs import configure
+from virtool.args import get_args
 
 sys.dont_write_bytecode = True
 
 setproctitle("virtool")
 
-args = virtool.args.get_args()
+args = get_args()
 
-logger = virtool.logs.configure(verbose=args.verbose)
+logger = configure(verbose=args.verbose)
 
 if args.write_pid:
     logger.debug("Writing pid file")
@@ -37,5 +37,4 @@ loop = asyncio.get_event_loop()
 
 if __name__ == "__main__":
     app = create_app(loop)
-    web.run_app(app)
-    server = virtool.web.Application(args.development)
+    web.run_app(app, host="localhost", port=9950)
