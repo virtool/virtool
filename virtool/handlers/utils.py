ffa8b40530c1be118a230312141cfb8ee4165bae
Ian Boyes <igboyes@gmail.com>
2017-03-30 16:16:32 -0700

    Add utils for shortcutting error responses

--- virtool/handlers/utils.py
+++ virtool/handlers/utils.py
@@ -1,2 +1,17 @@
+from aiohttp import web
+
+
 async def unpack_json_request(req):
     return req.app["db"], await req.json()
+
+
+def not_found():
+    return web.json_response({"message": "Not found"}, status=404)
+
+
+def requires_login():
+    return web.json_response({"message": "Requires login"}, status=400)
+
+
+def invalid_input(errors):
+    return web.json_response({"message": "Invalid input", "errors": errors}, status=422)
