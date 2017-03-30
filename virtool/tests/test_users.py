0ef78781d0c4048f2fbe26c05cf81b3ec4f59d26
Ian Boyes <igboyes@gmail.com>
2017-03-30 16:16:00 -0700

    Add tests for password hashing and checking

---
+++ virtool/tests/test_users.py
@@ -0,0 +1,20 @@
+import hashlib
+
+from virtool.utils import random_alphanumeric
+from virtool.users import hash_password, check_password, check_legacy_password
+
+
+class TestHashPassword:
+
+    def test_basic(self):
+        assert check_password("hello_world", hash_password("hello_world"))
+
+
+class TestLegacyHashPassword:
+
+    def test_basic(self):
+        salt = random_alphanumeric(24)
+
+        hashed = hashlib.sha512(salt.encode("utf-8") + "hello_world".encode("utf-8")).hexdigest()
+
+        assert check_legacy_password("hello_world", salt, hashed)
