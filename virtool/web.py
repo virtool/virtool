2fc2512dc64c47d543b9689b5d48ecbbd6e37707
Ian Boyes <igboyes@gmail.com>
2017-04-03 12:37:39 -0700

    Put some old functions into the new web module

--- virtool/web.py
+++ virtool/web.py
@@ -1,4 +1,7 @@
+import os
+import sys
 import logging
+import subprocess
 import virtool.routes
 import virtool.sessions
 
@@ -16,3 +19,51 @@ def create_app(loop, db_name="virtool"):
     virtool.routes.setup_routes(app)
 
     return app
+
+
+def reload():
+    exe = sys.executable
+
+    if exe.endswith("python"):
+        os.execl(exe, exe, *sys.argv)
+
+    if exe.endswith("run"):
+        os.execv(exe, sys.argv)
+
+    raise SystemError("Could not determine executable type")
+
+
+def shutdown(app, exit_code=0):
+    """
+    Shutdown the server by cancelling all jobs and calling :func:`sys.exit`.
+
+    :param exit_code: the exit code to return.
+    :type exit_code: int
+
+    """
+    logging.info("Shutting down")
+
+    job_manager = app.get("job_manager")
+
+    if job_manager:
+        for job_id in job_manager.jobs_dict:
+            job_manager.cancel(job_id)
+
+    logging.info("Exiting")
+
+    sys.exit(exit_code)
+
+
+def find_server_version(install_path="."):
+    try:
+        return subprocess.check_output(["git", "describe", "--tags"]).decode().rstrip()
+    except (subprocess.CalledProcessError, FileNotFoundError):
+        try:
+            version_file_path = os.path.join(install_path, "VERSION")
+
+            with open(version_file_path, "r") as version_file:
+                return version_file.read().rstrip()
+
+        except FileNotFoundError:
+            logger.critical("Could not determine software version.")
+            return "Unknown"
