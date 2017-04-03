40a60972bb528a52d86b42da323de9c8edd92f21
Ian Boyes <igboyes@gmail.com>
2017-04-03 14:10:37 -0700

    Fix static time fixture

--- virtool/tests/fixtures/core.py
+++ virtool/tests/fixtures/core.py
@@ -1,25 +1,9 @@
+import pytz
 import pytest
 
-from datetime import datetime, timedelta
-
-
-ZERO = timedelta(0)
-
-
-class UTC(datetime.tzinfo):
-
-    def utcoffset(self, dt):
-        return ZERO
-
-    def tzname(self, dt):
-        return "UTC"
-
-    def dst(self, dt):
-        return ZERO
+from datetime import datetime
 
 
 @pytest.fixture
 def static_time():
-    dt = datetime(2017, 10, 6, 13, 0, 0, UTC())
-    print(str(dt))
-    return dt
+    return datetime(2017, 10, 6, 20, 0, 0, tzinfo=pytz.utc)
