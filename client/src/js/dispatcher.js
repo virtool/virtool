ae792d645136b25baf09bb518805ad156dfaa29c
Ian Boyes <igboyes@gmail.com>
2017-05-02 16:27:56 -0700

    Move client dispatcher code to new file

---
+++ client/src/js/dispatcher.js
@@ -0,0 +1,29 @@
+import User from "./user";
+import Router from "./router";
+
+export default function Dispatcher () {
+
+    this.user = new User();
+    this.router = new Router();
+
+    // When a websocket message is received, this method is called with the message as the sole argument. Every message
+    // has a property "operation" that tells the dispatcher what to do. Illegal operation names will throw an error.
+    this.handle = (message) => {
+        console.log(`${message.interface}.${message.operation}`); // eslint-disable-line
+    };
+
+    this.establishConnection = () => {
+
+        var protocol = window.location.protocol === "https:" ? "wss" : "ws";
+
+        this.connection = new window.WebSocket(`${protocol}://${window.location.host}/ws`);
+
+        this.connection.onmessage = (event) => {
+            dispatcher.handle(JSON.parse(event.data));
+        };
+
+        this.connection.onclose = () => {
+            dispatcher.emit("closed");
+        };
+    };
+}
