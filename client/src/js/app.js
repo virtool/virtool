bb915b409483f66bf239f8948588e12f50bd77c8
Ian Boyes <igboyes@gmail.com>
2017-05-02 16:28:20 -0700

    Fix eslint warning on app.js CSS imports

--- client/src/js/app.js
+++ client/src/js/app.js
@@ -7,15 +7,15 @@ import ReactDOM from "react-dom";
 import Start from "./components/Start";
 
 import Request from "superagent";
-import Dispatcher from "virtool/js/dispatcher/main";
+import Dispatcher from "virtool/js/dispatcher";
 
-import * as bootstrap from "../css/bootstrap.css";
-import * as font from "../css/font.css";
-import * as roboto from "../css/roboto.css";
-import * as perfectScrollbar from "../css/perfect-scrollbar.css";
-import * as typeahead from "../css/typeahead.css";
-import * as graphics from "../css/graphics.css";
-import * as style from "../css/style.css";
+export * from "../css/bootstrap.css";
+export * from "../css/font.css";
+export * from "../css/roboto.css";
+export * from "../css/perfect-scrollbar.css";
+export * from "../css/typeahead.css";
+export * from "../css/graphics.css";
+export * from "../css/style.css";
 
 window.dispatcher = new Dispatcher();
 window.dispatcher.establishConnection();
