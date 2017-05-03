736db2c44f3f4fefa1207cd38007f15b0d722f63
Ian Boyes <igboyes@gmail.com>
2017-05-03 16:04:51 -0700

    Add action types and some virus action creators

--- client/src/js/actions/VirusActions.js
+++ client/src/js/actions/VirusActions.js
@@ -7,25 +7,62 @@
  *
  */
 
-import { LOAD_VIRUSES, UPDATE_VIRUS, REMOVE_VIRUS } from "./actionTypes";
+import {
+    WS_UPDATE_VIRUS,
+    WS_REMOVE_VIRUS,
+    FIND_VIRUSES_REQUESTED,
+    GET_VIRUS_REQUESTED,
+    EDIT_VIRUS_REQUESTED,
+    REMOVE_VIRUS_REQUESTED
+} from "./actionTypes";
 
-export function loadViruses(viruses) {
+
+export function wsUpdateVirus(virusUpdate) {
     return {
-        type: LOAD_VIRUSES,
-        viruses
+        type: WS_UPDATE_VIRUS,
+        virusUpdate
     }
 }
 
-export function removeVirus(virusId) {
+export function wsRemoveVirus(virusId) {
     return {
-        type: REMOVE_VIRUS,
+        type: WS_REMOVE_VIRUS,
         virusId
     }
 }
 
-export function updateVirus(virusUpdate) {
+export function findViruses () {
     return {
-        type: UPDATE_VIRUS,
-        virusUpdate
+        type: FIND_VIRUSES_REQUESTED
+    }
+}
+
+export function getVirus (virusId) {
+    return {
+        type: GET_VIRUS_REQUESTED,
+        virusId: virusId
+    }
+}
+
+export function editVirus (name, abbreviation) {
+    let action = {
+        type: EDIT_VIRUS_REQUESTED
+    };
+
+    if (name) {
+        action.name = name;
+    }
+
+    if (abbreviation) {
+        action.abbreviation = abbreviation;
+    }
+
+    return action;
+}
+
+export function removeVirus (virusId) {
+    return {
+        type: REMOVE_VIRUS_REQUESTED,
+        virusId: virusId
     }
 }
