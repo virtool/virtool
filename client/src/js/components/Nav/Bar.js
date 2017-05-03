9137a5995b3c75ae110c9688966653c76d633f66
Ian Boyes <igboyes@gmail.com>
2017-05-03 16:08:13 -0700

    Pare down Bar component

--- client/src/js/components/Nav/Bar.js
+++ client/src/js/components/Nav/Bar.js
@@ -10,48 +10,17 @@
  */
 
 import React from "react";
-import { forIn } from "lodash";
 import ChildBar from "./Child/Bar";
 import ParentBar from "./Parent/Bar";
-import LostConnection from "./LostConnection";
 
 /**
  * A container component that renders the primary and secondary navigation bars.
  */
-export default class Bar extends React.Component {
+const Bar = () => (
+    <div>
+        <ParentBar />
+        <ChildBar />
+    </div>
+);
 
-    constructor (props) {
-        super(props);
-
-        this.state = {
-            closed: false
-        };
-    }
-
-    componentDidMount () {
-        dispatcher.on("closed", this.showLostConnection);
-    }
-
-    componentWillUnmount () {
-        dispatcher.off("closed", this.showLostConnection);
-    }
-
-    showLostConnection = () => {
-        forIn(dispatcher.db.collectionNames, collectionName => {
-            dispatcher.db[collectionName].clear();
-        });
-
-        dispatcher.db.loki.saveDatabase(() => this.setState({closed: true}));
-    };
-
-    render = () => (
-        <div>
-            <ParentBar />
-            <ChildBar />
-            <LostConnection
-                show={this.state.closed}
-                onHide={this.showLostConnection}
-            />
-        </div>
-    );
-}
+export default Bar;
