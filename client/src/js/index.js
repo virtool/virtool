0f450eee6f9c2204627020f13bd4d44eeaf60360
Ian Boyes <igboyes@gmail.com>
2017-06-02 10:25:32 -0700

    Modify styles

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -7,14 +7,7 @@ import { store } from "./store";
 import { getAccount } from "./nav/actions";
 import { getSettings } from "./settings/actions";
 
-
-export * from "../css/bootstrap.css";
-export * from "../css/font.css";
-export * from "../css/roboto.css";
-export * from "../css/perfect-scrollbar.css";
-export * from "../css/typeahead.css";
-export * from "../css/graphics.css";
-export * from "../css/style.css";
+export * from "../style/style.less";
 
 window.ws = new WSConnection();
 window.ws.establishConnection();
