d41fad291e6fb33c57885773437b197c821ca336
Ian Boyes <igboyes@gmail.com>
2017-06-08 15:58:39 -0700

    Add app-level reducer

--- client/src/js/store.js
+++ client/src/js/store.js
@@ -7,9 +7,11 @@
  *
  */
 
+import { assign } from "lodash";
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import createSagaMiddleware from "redux-saga";
 
+import { SET_APP_PENDING, UNSET_APP_PENDING } from "./actionTypes";
 import samplesReducer from "./samples/reducers";
 import { virusesReducer } from "./viruses/reducers";
 import { accountReducer } from "./nav/reducers";
@@ -20,7 +22,27 @@ import { rootSaga } from "./sagas";
 
 const sagaMiddleware = createSagaMiddleware();
 
+const appInitialState = {
+    pending: false
+};
+
+const appReducer = (state = appInitialState, action) => {
+
+    switch (action.type) {
+
+        case SET_APP_PENDING:
+            return assign({}, state, {pending: true});
+
+        case UNSET_APP_PENDING:
+            return assign({}, state, {pending: false});
+
+    }
+
+    return state;
+};
+
 const reducer = combineReducers({
+    app: appReducer,
     samples: samplesReducer,
     viruses: virusesReducer,
     settings: settingsReducer,
