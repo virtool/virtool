fe92323dfa1067d552abefa60910e758500f0920
Ian Boyes <igboyes@gmail.com>
2017-06-26 13:47:01 -0700

    Add uploaded file removal endpoint

--- virtool/handlers/files.py
+++ virtool/handlers/files.py
@@ -1,11 +1,14 @@
+import os
+
 import virtool.file
-from virtool.handlers.utils import json_response
+import virtool.utils
+from virtool.handlers.utils import json_response, not_found
 
 
 async def find(req):
     db = req.app["db"]
 
-    cursor = db.files.find({"eof": True}, virtool.file.LIST_PROJECTION)
+    cursor = db.files.find({"ready": True}, virtool.file.LIST_PROJECTION)
 
     found_count = await cursor.count()
 
@@ -14,4 +17,24 @@ async def find(req):
     return json_response({
         "documents": documents,
         "found_count": found_count
+    })
+
+
+async def remove(req):
+    file_id = req.match_info["file_id"]
+
+    file_path = os.path.join(req.app["settings"].get("data_path"), "files", file_id)
+
+    delete_result = await req.app["db"].files.delete_one({"_id": file_id})
+
+    virtool.utils.rm(file_path)
+
+    if delete_result.deleted_count == 0:
+        return not_found("Document does not exist")
+
+    await req.app["dispatcher"].dispatch("files", "remove", [file_id])
+
+    return json_response({
+        "file_id": file_id,
+        "removed": True
     })
