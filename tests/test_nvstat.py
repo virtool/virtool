e9739c564127d7720e3313680cba6518d6f7779c
Ian Boyes <igboyes@gmail.com>
2017-06-28 11:51:12 -0700

    Add own nvstat module so I don't have to deal with pycuda

---
+++ tests/test_nvstat.py
@@ -0,0 +1,194 @@
+import pytest
+import virtool.nvstat
+
+
+class TestDriverVersion:
+
+    def test(self, mocker):
+        return_value = bytes((
+            "Wed Jun 28 10:15:13 2017\n"
+            "+-----------------------------------------------------------------------------+\n"
+            "| NVIDIA-SMI 375.66                 Driver Version: 555.55                    |\n"
+            "|-------------------------------+----------------------+----------------------+\n"
+            "| GPU  Name        Persistence-M| Bus-Id        Disp.A | Volatile Uncorr. ECC |\n"
+            "| Fan  Temp  Perf  Pwr:Usage/Cap|         Memory-Usage | GPU-Util  Compute M. |\n"
+            "|===============================+======================+======================|\n"
+            "|   0  GeForce GTX 1080    Off  | 0000:06:00.0      On |                  N/A |\n"
+            "|  0%   46C    P8    11W / 198W |   1699MiB /  8110MiB |     12%      Default |\n"
+            "+-------------------------------+----------------------+----------------------+\n"
+            "                                                                               \n"
+            "+-----------------------------------------------------------------------------+\n"
+            "| Processes:                                                       GPU Memory |\n"
+            "|  GPU       PID  Type  Process name                               Usage      |\n"
+            "|=============================================================================|\n"
+            "|    0      5183    G   /usr/lib/xorg/Xorg                            1201MiB |\n"
+            "|    0      7326    G   cinnamon                                       262MiB |\n"
+            "|    0      7511    G   ...el-token=3AC7789447AEC469F8CF8195C364C622   172MiB |\n"
+            "|    0     14813    G   /proc/self/exe                                  61MiB |\n"
+            "+-----------------------------------------------------------------------------+"
+        ), encoding="utf-8")
+
+        mocker.patch("subprocess.check_output", new=lambda x: return_value)
+
+        assert virtool.nvstat.driver_version() == "555.55"
+
+    def test_no_smi(self, mocker):
+        def raise_not_found_error(*args, **kwargs):
+            raise FileNotFoundError
+
+        mocker.patch("subprocess.check_output", new=raise_not_found_error)
+
+        with pytest.raises(FileNotFoundError) as err:
+            virtool.nvstat.driver_version()
+
+        assert "nvidia-smi could not be called" in str(err)
+
+
+class TestListDevices:
+
+    def test(self, mocker):
+        return_value = bytes((
+            "GPU 0: GeForce GTX 1080 (UUID: GPU-b334763a-21fa-1302-5dff-1eaecd0f5428)\n"
+            "GPU 1: GeForce GTX 1080 (UUID: GPU-b334763a-21fa-1302-5dff-1skjlekas980)\n"
+            "GPU 2: GeForce GTX 1080 (UUID: GPU-b334763a-21fa-1302-5dff-9djaskkn2l31)\n"
+        ), encoding="utf-8")
+
+        mocker.patch("subprocess.check_output", lambda x: return_value)
+
+        output = virtool.nvstat.list_devices()
+
+        assert output == [
+            {"uuid": "GPU-b334763a-21fa-1302-5dff-1eaecd0f5428", "model": "GeForce GTX 1080", "index": 0},
+            {"uuid": "GPU-b334763a-21fa-1302-5dff-1skjlekas980", "model": "GeForce GTX 1080", "index": 1},
+            {"uuid": "GPU-b334763a-21fa-1302-5dff-9djaskkn2l31", "model": "GeForce GTX 1080", "index": 2}
+        ]
+
+    def test_no_smi(self, mocker):
+        def raise_not_found_error(*args, **kwargs):
+            raise FileNotFoundError
+
+        mocker.patch("subprocess.check_output", new=raise_not_found_error)
+
+        with pytest.raises(FileNotFoundError) as err:
+            virtool.nvstat.driver_version()
+
+        assert "nvidia-smi could not be called" in str(err)
+
+
+class TestDeviceMemory:
+
+    def test(self, mocker):
+        return_value = bytes((
+            "==============NVSMI LOG==============\n"
+            "\n"
+            "Timestamp                           : Wed Jun 28 11:21:36 2017\n"
+            "Driver Version                      : 375.66\n"
+            "\n"
+            "Attached GPUs                       : 1\n"
+            "GPU 0000:06:00.0\n"
+            "    FB Memory Usage\n"
+            "        Total                       : 8110 MiB\n"
+            "        Used                        : 1796 MiB\n"
+            "        Free                        : 6314 MiB\n"
+            "    BAR1 Memory Usage\n"
+            "        Total                       : 256 MiB\n"
+            "        Used                        : 5 MiB\n"
+            "        Free                        : 251 MiB\n"
+            ""
+        ), encoding="utf-8")
+
+        mocker.patch("subprocess.check_output", new=lambda x: return_value)
+
+        assert virtool.nvstat.device_memory(0) == {
+            "FB": {
+                "used": 1883242496,
+                "free": 6620708864,
+                "total": 8503951360
+            },
+            "BAR1": {
+                "used": 5242880,
+                "free": 263192576,
+                "total": 268435456
+            }
+        }
+
+    def test_no_device(self, mocker):
+        mocker.patch("subprocess.check_output", new=lambda x: "No devices were found")
+
+        with pytest.raises(IndexError) as err:
+            virtool.nvstat.device_memory(5)
+
+        assert "No device with index 5" in str(err)
+
+    def test_no_smi(self, mocker):
+        def raise_not_found_error(*args, **kwargs):
+            raise FileNotFoundError
+
+        mocker.patch("subprocess.check_output", new=raise_not_found_error)
+
+        with pytest.raises(FileNotFoundError) as err:
+            virtool.nvstat.driver_version()
+
+        assert "nvidia-smi could not be called" in str(err)
+
+
+class TestDeviceClock:
+
+    def test(self, mocker):
+        return_value = bytes((
+            "==============NVSMI LOG==============\n"
+            "\n"
+            "Timestamp                           : Wed Jun 28 11:29:56 2017\n"
+            "Driver Version                      : 375.66\n"
+            "\n"
+            "Attached GPUs                       : 1\n"
+            "GPU 0000:06:00.0\n"
+            "    Clocks\n"
+            "        Graphics                    : 1670 MHz\n"
+            "        SM                          : 1670 MHz\n"
+            "        Memory                      : 5005 MHz\n"
+            "        Video                       : 1493 MHz\n"
+            "    Applications Clocks\n"
+            "        Graphics                    : N/A\n"
+            "        Memory                      : N/A\n"
+            "    Default Applications Clocks\n"
+            "        Graphics                    : N/A\n"
+            "        Memory                      : N/A\n"
+            "    Max Clocks\n"
+            "        Graphics                    : 1974 MHz\n"
+            "        SM                          : 1974 MHz\n"
+            "        Memory                      : 5005 MHz\n"
+            "        Video                       : 1708 MHz\n"
+            "    SM Clock Samples\n"
+            "        Duration                    : 395.20 sec\n"
+            "        Number of Samples           : 100\n"
+            "        Max                         : 1670 MHz\n"
+            "        Min                         : 202 MHz\n"
+            "        Avg                         : 1484 MHz\n"
+            "    Memory Clock Samples\n"
+            "        Duration                    : 395.20 sec\n"
+            "        Number of Samples           : 100\n"
+            "        Max                         : 5005 MHz\n"
+            "        Min                         : 405 MHz\n"
+            "        Avg                         : 3643 MHz\n"
+            "    Clock Policy\n"
+            "        Auto Boost                  : N/A\n"
+            "        Auto Boost Default          : N/A"
+        ), encoding="utf-8")
+
+        mocker.patch("subprocess.check_output", new=lambda x: return_value)
+
+        assert virtool.nvstat.device_clock(0) == {
+            "base": {
+                "graphics": 1751121920,
+                "memory": 5248122880,
+                "sm": 1751121920,
+                "video": 1565523968
+            },
+            "max": {
+                "graphics": 2069889024,
+                "memory": 5248122880,
+                "sm": 2069889024,
+                "video": 1790967808
+            }
+        }
---
+++ virtool/nvstat.py
@@ -0,0 +1,139 @@
+import subprocess
+
+
+BASE_COMMAND = ["nvidia-smi"]
+
+
+def convert_memory_value(value_string):
+    value, unit = value_string.split(" ")
+    value = int(value)
+
+    if unit == "KiB":
+        return value * 1024
+
+    if unit == "MiB":
+        return value * 1024 * 1024
+
+    raise ValueError("Could not parse memory value {}".format(value_string))
+
+
+def convert_clock_value(value_string):
+    value, unit = value_string.split(" ")
+    value = int(value)
+
+    if unit == "KHz":
+        return value * 1024
+
+    if unit == "MHz":
+        return value * 1024 * 1024
+
+    if unit == "GHz":
+        return value * 1024 * 1024 * 1024
+
+    raise ValueError("Could not parse clock value {}".format(value_string))
+
+
+def driver_version():
+    try:
+        output = subprocess.check_output(BASE_COMMAND).decode("utf-8").rstrip().split("\n")
+    except FileNotFoundError:
+        raise FileNotFoundError("nvidia-smi could not be called. Make sure it is installed")
+
+    for line in output:
+        if "Driver Version" in line:
+            return line.split("Driver Version: ")[1].split(" ")[0]
+
+
+def list_devices():
+    try:
+        output = subprocess.check_output(BASE_COMMAND + ["-L"]).decode("utf-8").rstrip().split("\n")
+    except FileNotFoundError:
+        raise FileNotFoundError("nvidia-smi could not be called. Make sure it is installed")
+
+    device_list = list()
+
+    for line in output:
+        device_dict = dict()
+
+        # Extract device UUID
+        split = line.strip().split("(UUID: ")
+        device_dict["uuid"] = split[1].replace(")", "")
+
+        # Extract device model name.
+        split = split[0].split(":")
+        device_dict["model"] = split[1].strip()
+
+        # Extract device index.
+        device_dict["index"] = int(split[0].replace("GPU ", ""))
+
+        device_list.append(device_dict)
+
+    return device_list
+
+
+def device_memory(index):
+    try:
+        output = subprocess.check_output(BASE_COMMAND + ["-i", str(index), "-d", "MEMORY", "-q"])
+    except FileNotFoundError:
+        raise FileNotFoundError("nvidia-smi could not be called. Make sure it is installed")
+
+    if output == "No devices were found":
+        raise IndexError("No device with index {}".format(index))
+
+    result = {
+        "FB": dict(),
+        "BAR1": dict()
+    }
+
+    mode = False
+
+    for line in output.decode().split("\n"):
+        if "FB Memory Usage" in line:
+            mode = "FB"
+        elif "BAR1 Memory Usage" in line:
+            mode = "BAR1"
+
+        if not mode:
+            continue
+
+        if ":" in line:
+            key, value = [s.strip() for s in line.split(":")]
+            result[mode][key.lower()] = convert_memory_value(value)
+
+    return result
+
+
+def device_clock(index):
+    try:
+        output = subprocess.check_output(BASE_COMMAND + ["-i", str(index), "-d", "CLOCK", "-q"])
+    except FileNotFoundError:
+        raise FileNotFoundError("nvidia-smi could not be called. Make sure it is installed")
+
+    if output == "No devices were found":
+        raise IndexError("No device with index {}".format(index))
+
+    mode = False
+
+    result = {
+        "base": dict(),
+        "max": dict()
+    }
+
+    for line in output.decode().split("\n"):
+        if line.startswith(8 * " ") and ":" in line:
+            if not mode:
+                continue
+            else:
+                key, value = [s.strip() for s in line.split(":")]
+                result[mode][key.lower()] = convert_clock_value(value)
+
+        elif line.startswith("    Clocks"):
+            mode = "base"
+
+        elif line.startswith("    Max Clocks"):
+            mode = "max"
+
+        else:
+            mode = False
+
+    return result
