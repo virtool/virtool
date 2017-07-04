769d94f582f252207e9f4f8e0e5b71a6fd05e0ea
Ian Boyes <igboyes@gmail.com>
2017-07-04 13:50:49 -0700

    Make root endpoint show API information

--- virtool/handlers/root.py
+++ virtool/handlers/root.py
@@ -1,4 +1,4 @@
-from aiohttp import web
+from virtool.handlers.utils import json_response
 
 
 async def get(req):
@@ -6,6 +6,31 @@ async def get(req):
     Returns a generic message. Used during testing for acquiring a ``session_id``.
          
     """
-    return web.json_response({
-        "status": "healthy"
+    return json_response({
+        "endpoints": {
+            "account": {
+                "url": "/api/account",
+                "doc": "https://docs.virtool.ca/web-api/account.html"
+            },
+
+            "jobs": {
+                "url": "/api/jobs",
+                "doc": "https://docs.virtool.ca/web-api/jobs.html"
+            },
+
+            "samples": {
+                "url": "/api/samples",
+                "doc": "https://docs.virtool.ca/web-api/samples.html"
+            },
+
+            "analyses": {
+                "url": "/api/analyses",
+                "doc": "https://docs.virtool.ca/web-api/analyses.html"
+            },
+
+            "viruses": {
+                "url": "/api/viruses",
+                "doc": "https://docs.virtool.ca/web-api/viruses.html"
+            }
+        }
     })
