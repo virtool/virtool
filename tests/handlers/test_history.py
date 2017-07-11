9451df3f6ef9200af4a08e8f626c526137a00d44
Ian Boyes <igboyes@gmail.com>
2017-07-11 16:28:06 -0700

    Fix all broken API tests

--- tests/handlers/test_history.py
+++ tests/handlers/test_history.py
@@ -1,6 +1,9 @@
 import pytest
+from copy import deepcopy
+from operator import itemgetter
 
 import virtool.virus
+import virtool.virus_history
 
 
 class TestFind:
@@ -16,62 +19,73 @@ class TestFind:
 
         assert resp.status == 200
 
-        assert await resp.json() == [
-            {
-                "description": ["Edited virus", "Prunus virus E"],
-                "id": "6116cba1.1",
-                "index": {
-                    "id": "unbuilt",
-                    "version": "unbuilt"
+        resp_json = await resp.json()
+
+        resp_json["documents"] = sorted(resp_json["documents"], key=itemgetter("id"))
+
+        assert resp_json == {
+            "found_count": 3,
+            "page": 1,
+            "page_count": 1,
+            "per_page": 15,
+            "total_count": 3,
+            "documents": sorted([
+                {
+                    "description": "Edited virus Prunus virus E",
+                    "id": "6116cba1.1",
+                    "index": {
+                        "id": "unbuilt",
+                        "version": "unbuilt"
+                    },
+                    "method_name": "edit",
+                    "created_at": "2017-10-06T20:00:00Z",
+                    "user": {
+                        "id": "test"
+                    },
+                    "virus": {
+                        "id": "6116cba1",
+                        "name": "Prunus virus F",
+                        "version": 1
+                    }
                 },
-                "method_name": "edit",
-                "timestamp": "2017-10-06T20:00:00Z",
-                "user": {
-                    "id": "test"
+                {
+                    "description": "Edited virus Prunus virus E",
+                    "id": "foobar.1",
+                    "index": {
+                        "id": "unbuilt",
+                        "version": "unbuilt"
+                    },
+                    "method_name": "edit",
+                    "created_at": "2017-10-06T20:00:00Z",
+                    "user": {
+                        "id": "test"
+                    },
+                    "virus": {
+                        "id": "6116cba1",
+                        "name": "Prunus virus F",
+                        "version": 1
+                    }
                 },
-                "virus": {
-                    "id": "6116cba1",
-                    "name": "Prunus virus F",
-                    "version": 1
+                {
+                    "description": "Edited virus Prunus virus E",
+                    "id": "foobar.2",
+                    "index": {
+                        "id": "unbuilt",
+                        "version": "unbuilt"
+                    },
+                    "method_name": "edit",
+                    "created_at": "2017-10-06T20:00:00Z",
+                    "user": {
+                        "id": "test"
+                    },
+                    "virus": {
+                        "id": "6116cba1",
+                        "name": "Prunus virus F",
+                        "version": 1
+                    }
                 }
-            },
-            {
-                "description": ["Edited virus", "Prunus virus E"],
-                "id": "foobar.1",
-                "index": {
-                    "id": "unbuilt",
-                    "version": "unbuilt"
-                },
-                "method_name": "edit",
-                "timestamp": "2017-10-06T20:00:00Z",
-                "user": {
-                    "id": "test"
-                },
-                "virus": {
-                    "id": "6116cba1",
-                    "name": "Prunus virus F",
-                    "version": 1
-                }
-            },
-            {
-                "description": ["Edited virus", "Prunus virus E"],
-                "id": "foobar.2",
-                "index": {
-                    "id": "unbuilt",
-                    "version": "unbuilt"
-                },
-                "method_name": "edit",
-                "timestamp": "2017-10-06T20:00:00Z",
-                "user": {
-                    "id": "test"
-                },
-                "virus": {
-                    "id": "6116cba1",
-                    "name": "Prunus virus F",
-                    "version": 1
-                }
-            }
-        ]
+            ], key=itemgetter("id"))
+        }
         
 
 class TestGet:
@@ -88,7 +102,7 @@ class TestGet:
         assert resp.status == 200
 
         assert await resp.json() == {
-            "description": ["Edited virus", "Prunus virus E"],
+            "description": "Edited virus Prunus virus E",
             "diff": [
                 ["change", "abbreviation", ["PVF", ""]],
                 ["change", "name", ["Prunus virus F", "Prunus virus E"]],
@@ -100,7 +114,7 @@ class TestGet:
                 "version": "unbuilt"
             },
             "method_name": "edit",
-            "timestamp": "2017-10-06T20:00:00Z",
+            "created_at": "2017-10-06T20:00:00Z",
             "user": {
                 "id": "test"
             },
@@ -121,6 +135,7 @@ class TestGet:
         assert resp.status == 404
 
         assert await resp.json() == {
+            "id": "not_found",
             "message": "Not found"
         }
 
@@ -128,12 +143,55 @@ class TestGet:
 class TestRemove:
 
     @pytest.mark.parametrize("remove", [True, False])
-    async def test(self, remove, test_motor, do_delete, setup_test_history):
+    async def test(self, remove, test_motor, do_delete, test_merged_virus):
         """
         Test that a valid request results in a reversion and a ``204`` response.
          
         """
-        expected = await setup_test_history(remove=remove)
+        # Apply a series of changes to a test virus document to build up a history.
+        await virtool.virus_history.add(test_motor, "create", None, test_merged_virus, "Description", "test")
+
+        old = deepcopy(test_merged_virus)
+
+        test_merged_virus.update({
+            "abbreviation": "TST",
+            "version": 1
+        })
+
+        await virtool.virus_history.add(test_motor, "update", old, test_merged_virus, "Description", "test")
+
+        old = deepcopy(test_merged_virus)
+
+        # We will try to patch to this version of the joined virus.
+        expected = deepcopy(old)
+
+        test_merged_virus.update({
+            "name": "Test Virus",
+            "version": 2
+        })
+
+        await virtool.virus_history.add(test_motor, "update", old, test_merged_virus, "Description", "test")
+
+        old = deepcopy(test_merged_virus)
+
+        test_merged_virus.update({
+            "isolates": [],
+            "version": 3
+        })
+
+        await virtool.virus_history.add(test_motor, "remove_isolate", old, test_merged_virus, "Description", "test")
+
+        if remove:
+            old = deepcopy(test_merged_virus)
+
+            test_merged_virus = {
+                "_id": "6116cba1"
+            }
+
+            await virtool.virus_history.add(test_motor, "remove", old, test_merged_virus, "Description", "test")
+        else:
+            virus, sequences = virtool.virus.split_virus(test_merged_virus)
+            await test_motor.viruses.insert_one(virus)
 
         await do_delete("/api/history/6116cba1.2")
 
@@ -151,5 +209,6 @@ class TestRemove:
         assert resp.status == 404
 
         assert await resp.json() == {
+            "id": "not_found",
             "message": "Not found"
         }
