e7f4eb6de8f650c5cf430bc0d670fab8695d8f42
Ian Boyes <igboyes@gmail.com>
2017-08-11 16:30:49 -0700

    Provide better headers for FASTA downloads and allow virus download

--- virtool/handlers/downloads.py
+++ virtool/handlers/downloads.py
@@ -4,20 +4,40 @@ Request handlers for file downloads.
 """
 from aiohttp import web
 
+import virtool.virus
+
 async def download_sequence(req):
     db = req.app["db"]
 
     sequence_id = req.match_info["sequence_id"]
 
-    document = await db.sequences.find_one(sequence_id, ["sequence"])
+    sequence = await db.sequences.find_one(sequence_id, ["sequence", "virus_id", "isolate_id"])
 
-    if document is None:
+    if sequence is None:
         return web.Response(status=404)
 
-    fasta = ">{}\n{}".format(sequence_id, document["sequence"])
+    virus = await db.viruses.find_one({"_id": sequence["virus_id"]}, ["name", "isolates"])
+
+    if virus is None:
+        return web.Response(status=404)
+
+    isolate = virtool.virus.find_isolate(virus["isolates"], sequence["isolate_id"])
+
+    isolate_name = virtool.virus.format_isolate_name(isolate)
+
+    if isolate is None:
+        return web.Response(status=404)
+
+    seq = sequence["sequence"]
+
+    fasta = ">{}|{}|{}|{}\n{}".format(virus["name"], isolate_name, sequence["_id"], len(seq), seq)
 
     return web.Response(text=fasta, headers={
-        "Content-Disposition": "attachment; filename='seq_{}.fa'".format(sequence_id)
+        "Content-Disposition": "attachment; filename='{}.{}.{}.fa'".format(
+            virus["name"].replace(" ", "_").lower(),
+            isolate_name.replace(" ", "_").lower(),
+            sequence_id.lower()
+        )
     })
 
 
@@ -27,19 +47,58 @@ async def download_isolate_sequences(req):
     virus_id = req.match_info["virus_id"]
     isolate_id = req.match_info["isolate_id"]
 
-    if not await db.viruses.count({"_id": virus_id, "isolates.id": isolate_id}):
+    virus = await db.viruses.find_one({"_id": virus_id, "isolates.id": isolate_id}, ["name", "isolates"])
+
+    if not virus:
         return web.Response(status=404)
+
+    isolate = virtool.virus.find_isolate(virus["isolates"], isolate_id)
+
+    if isolate is None:
+        return web.Response(status=404)
+
+    isolate_name = virtool.virus.format_isolate_name(isolate)
 
     fasta = list()
 
-    async for document in db.sequences.find({"virus_id": virus_id, "isolate_id": isolate_id}, ["sequence"]):
-        fasta += [
-            ">{}".format(document["_id"]),
-            document["sequence"]
-        ]
+    async for sequence in db.sequences.find({"virus_id": virus_id, "isolate_id": isolate_id}, ["sequence"]):
+        seq = sequence["sequence"]
+
+        fasta.append(">{}|{}|{}|{}\n{}".format(virus["name"], isolate_name, sequence["_id"], len(seq), seq))
 
     fasta = "\n".join(fasta)
 
     return web.Response(text=fasta, headers={
-        "Content-Disposition": "attachment; filename='isolate_{}.fa'".format(isolate_id)
+        "Content-Disposition": "attachment; filename='{}.{}.fa'".format(
+            virus["name"].replace(" ", "_").lower(),
+            isolate_name.replace(" ", "_").lower()
+        )
+    })
+
+
+async def download_virus_sequences(req):
+    db = req.app["db"]
+
+    virus_id = req.match_info["virus_id"]
+
+    virus = await db.viruses.find_one(virus_id, ["name", "isolates"])
+
+    if not virus:
+        return web.Response(status=404)
+
+    fasta = list()
+
+    for isolate in virus["isolates"]:
+        isolate_name = virtool.virus.format_isolate_name(isolate)
+
+        async for sequence in db.sequences.find({"isolate_id": isolate["id"]}, ["sequence"]):
+            seq = sequence["sequence"]
+            fasta.append(">{}|{}|{}|{}\n{}".format(virus["name"], isolate_name, sequence["_id"], len(seq), seq))
+
+    fasta = "\n".join(fasta)
+
+    return web.Response(text=fasta, headers={
+        "Content-Disposition": "attachment; filename='{}.fa'".format(
+            virus["name"].replace(" ", "_").lower()
+        )
     })
