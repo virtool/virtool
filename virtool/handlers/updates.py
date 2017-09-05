dad970e9db2d3985a4995982d91a995898c8781b
Ian Boyes <igboyes@gmail.com>
2017-09-05 13:16:43 -0700

    Make retrieval of releases from GitHub functional

--- virtool/handlers/updates.py
+++ virtool/handlers/updates.py
@@ -4,15 +4,17 @@ from virtool.handlers.utils import json_response
 
 
 async def get(req):
-    db = req.app["db"]
+    # db = req.app["db"]
     settings = req.app["settings"]
 
     repo = settings.get("software_repo")
     server_version = virtool.app.find_server_version()
 
-    await virtool.updates.get_releases(repo, server_version)
+    releases = await virtool.updates.get_releases(repo, server_version)
 
-    return json_response({"message": "YAY"})
+    return json_response({
+        "releases": releases
+    })
 
 
 
