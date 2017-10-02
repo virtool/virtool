2a53970fe2d78b8d1a00799acc047ba7f8784e58
Ian Boyes <igboyes@gmail.com>
2017-10-02 12:20:19 -0700

    Don't show GPU listing

--- client/src/js/home/components/Welcome.js
+++ client/src/js/home/components/Welcome.js
@@ -12,27 +12,16 @@
 import React from "react";
 import { Panel, ButtonToolbar } from "react-bootstrap";
 import { Icon } from "../../components/Base/Icon";
-import CUDA from "./CUDA";
 
 const Welcome  = () => (
     <div className="container">
         <Panel>
             <p>Viral infection diagnostics using next-generation sequencing</p>
 
-            <ButtonToolbar>
-                <a className="btn btn-default" href="http://www.virtool.ca/" target="_blank">
-                    <Icon name="vtlogo" /> Website
-                </a>
-                <a className="btn btn-default" href="https://github.com/virtool/virtool" target="_blank">
-                    <Icon name="github" /> Github
-                </a>
-                <a className="btn btn-default" href="/doc/index.html?v=2" target="_blank">
-                    <Icon name="book" /> Documentation
-                </a>
-            </ButtonToolbar>
+            <a className="btn btn-default" href="http://www.virtool.ca/" target="_blank">
+                <Icon name="vtlogo" /> Website
+            </a>
         </Panel>
-
-        <CUDA />
     </div>
 );
 
