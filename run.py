448368713639dd31b8556a4d3eb89d4ee6c563ee
Ian Boyes <igboyes@gmail.com>
2017-10-03 08:39:53 -0700

    Fix SSL settings

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import ssl
 import json
 import uvloop
 import asyncio
@@ -46,9 +47,19 @@ except FileNotFoundError:
     settings_temp = dict()
 
 if __name__ == "__main__":
+    ssl_context = None
+
+    if settings_temp.get("use_ssl", False):
+        cert_path = settings_temp.get("cert_path", None)
+        key_path = settings_temp.get("key_path", None)
+
+        if cert_path and key_path:
+            ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+            ssl_context.load_cert_chain(cert_path, key_path)
+
     app = create_app(loop, skip_setup=skip_setup)
 
     host = args.host or settings_temp.get("server_host", "localhost")
     port = args.port or settings_temp.get("server_port", 9950)
 
-    web.run_app(app, host=host, port=port)
+    web.run_app(app, host=host, port=port, ssl_context=ssl_context)
