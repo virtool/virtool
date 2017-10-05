3d1cd85eadbf1aa69a08003e66bf24651ca75e29
igboyes <igboyes@gmail.com>
2017-10-05 15:13:01 -0700

    Get account settings view working

--- client/src/js/account/components/Settings.js
+++ client/src/js/account/components/Settings.js
@@ -17,79 +17,72 @@ import { updateAccountSettings } from "../actions";
 import { AlgorithmSelect, Checkbox, Flex, FlexItem, Icon } from "../../base";
 
 
-class AccountSettings extends React.Component {
+const AccountSettings = (props) => {
+    const settings = props.account.settings;
 
-    constructor (props) {
-        super(props);
-    }
+    return (
+        <div>
+            <Panel header="User Interface">
+                <ListGroup fill>
+                    <ListGroupItem>
+                        <Flex>
+                            <FlexItem>
+                                <Checkbox
+                                    checked={settings.show_ids}
+                                    onClick={() => props.onUpdateSetting("show_ids", !settings.show_ids)}
+                                />
+                            </FlexItem>
+                            <FlexItem pad={10}>
+                                <div>Show Unique ID Fields</div>
+                                <small>
+                                    Show the unique database IDs for Virtool records where possible. This is not
+                                    required for normal use, but is useful for debugging.
+                                </small>
+                            </FlexItem>
+                        </Flex>
+                    </ListGroupItem>
+                </ListGroup>
+            </Panel>
 
-    render () {
-        const settings = this.props.account.settings;
+            <Panel header="Quick Analyze">
+                <ListGroup fill>
+                    <ListGroupItem>
+                        <Flex>
+                            <FlexItem>
+                                <Checkbox
+                                    checked={settings.skip_quick_analyze_dialog}
+                                    onClick={() => props.onUpdateSetting(
+                                        "skip_quick_analyze_dialog",
+                                        !settings.skip_quick_analyze_dialog
+                                    )}
+                                />
+                            </FlexItem>
+                            <FlexItem pad={10}>
+                                <div>Skip Dialog</div>
+                                <small>
+                                    Allow samples to be analyzed with a single click of
+                                    &nbsp;<Icon bsStyle="success" name="bars" /> using a preconfigured algorithm.
+                                    The algorithm selection dialog is not shown.
+                                </small>
 
-        return (
-            <div>
-                <Panel header="User Interface">
-                    <ListGroup fill>
-                        <ListGroupItem>
-                            <Flex>
-                                <FlexItem>
-                                    <Checkbox
-                                        checked={settings.show_ids}
-                                        onClick={(e) => this.props.onUpdateSetting("show_ids", e.target.value)}
-                                    />
-                                </FlexItem>
-                                <FlexItem pad={10}>
-                                    <div>Show Unique ID Fields</div>
-                                    <small>
-                                        Show the unique database IDs for Virtool records where possible. This is not
-                                        required for normal use, but is useful for debugging.
-                                    </small>
-                                </FlexItem>
-                            </Flex>
-                        </ListGroupItem>
-                    </ListGroup>
-                </Panel>
-
-                <Panel header="Quick Analyze">
-                    <ListGroup fill>
-                        <ListGroupItem>
-                            <Flex>
-                                <FlexItem>
-                                    <Checkbox
-                                        checked={settings.skip_quick_analyze_dialog}
-                                        onClick={(e) => this.props.onUpdateSetting(
-                                            "skip_quick_analyze_dialog",
+                                <div style={{marginTop: "7px"}}>
+                                    <AlgorithmSelect
+                                        value={settings.quick_analyze_algorithm}
+                                        noLabel={true}
+                                        onChange={(e) => props.onUpdateSetting(
+                                            "quick_analyze_algorithm",
                                             e.target.value
                                         )}
                                     />
-                                </FlexItem>
-                                <FlexItem pad={10}>
-                                    <div>Skip Dialog</div>
-                                    <small>
-                                        Allow samples to be analyzed with a single click of
-                                        &nbsp;<Icon bsStyle="success" name="bars" /> using a preconfigured algorithm.
-                                        The algorithm selection dialog is not shown.
-                                    </small>
-
-                                    <div style={{marginTop: "7px"}}>
-                                        <AlgorithmSelect
-                                            value={settings.quick_analyze_algorithm}
-                                            noLabel={true}
-                                            onChange={(e) => this.props.onUpdateSetting(
-                                                "quick_analyze_algorithm",
-                                                e.target.value
-                                            )}
-                                        />
-                                    </div>
-                                </FlexItem>
-                            </Flex>
-                        </ListGroupItem>
-                    </ListGroup>
-                </Panel>
-            </div>
-        );
-    }
-}
+                                </div>
+                            </FlexItem>
+                        </Flex>
+                    </ListGroupItem>
+                </ListGroup>
+            </Panel>
+        </div>
+    );
+};
 
 const mapStateToProps = (state) => {
     return {
