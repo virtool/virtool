236cdbedbcc4a734fa31a9d1a7a76ac4deaf139e
igboyes <igboyes@gmail.com>
2017-10-31 16:30:58 -0700

    Try to fix setting error during setup

--- virtool/app.py
+++ virtool/app.py
@@ -255,6 +255,8 @@ def create_app(loop, db_name=None, disable_job_manager=False, disable_file_manag
     if "client_path" not in app:
         raise FileNotFoundError("Could not find client path")
 
+    app.on_startup.append(init_settings)
+
     if not skip_setup:
         virtool.setup.setup_routes(app)
         app.on_startup.append(init_setup)
@@ -262,7 +264,6 @@ def create_app(loop, db_name=None, disable_job_manager=False, disable_file_manag
         virtool.app_routes.setup_routes(app)
 
         app.on_startup.append(init_executors)
-        app.on_startup.append(init_settings)
         app.on_startup.append(init_dispatcher)
         app.on_startup.append(init_db)
         app.on_startup.append(init_resources)
