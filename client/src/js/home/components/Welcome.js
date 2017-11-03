37c5bdf7f69b7b1d2da431c3681a3607eca36e42
igboyes <igboyes@gmail.com>
2017-11-02 19:56:20 -0700

    Show software version on welcome page

--- client/src/js/home/components/Welcome.js
+++ client/src/js/home/components/Welcome.js
@@ -10,19 +10,54 @@
  */
 
 import React from "react";
+import { get } from "lodash";
+import { connect } from "react-redux";
 import { Panel } from "react-bootstrap";
+
+import { getSoftwareUpdates } from "../../updates/actions";
 import { Icon } from "../../base";
 
-const Welcome  = () => (
-    <div className="container">
-        <Panel>
-            <p>Viral infection diagnostics using next-generation sequencing</p>
+class Welcome extends React.Component {
 
-            <a className="btn btn-default" href="http://www.virtool.ca/" target="_blank" rel="noopener noreferrer">
-                <Icon name="vtlogo" /> Website
-            </a>
-        </Panel>
-    </div>
-);
+    componentDidMount () {
+        this.props.onGet();
+    }
 
-export default Welcome;
+    render () {
+        if (!this.props.version) {
+            return <div />;
+        }
+
+        return (
+            <div className="container">
+                <Panel>
+                    <h3>Virtool <small className="text-muted">{this.props.version}</small></h3>
+                    <p>Viral infection diagnostics using next-generation sequencing</p>
+
+                    <a className="btn btn-default" href="http://www.virtool.ca/" target="_blank"
+                       rel="noopener noreferrer">
+                        <Icon name="vtlogo"/> Website
+                    </a>
+                </Panel>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        version: get(state.updates.software, "current_version")
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onGet: () => {
+            dispatch(getSoftwareUpdates());
+        }
+    };
+};
+
+const Container = connect(mapStateToProps, mapDispatchToProps)(Welcome);
+
+export default Container;
