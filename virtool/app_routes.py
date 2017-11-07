5a2d18d3dfc13545f5d219e24b8ae5d1022a7b17
igboyes <igboyes@gmail.com>
2017-11-07 15:58:14 -0800

    Update server HMM routes

--- virtool/app_routes.py
+++ virtool/app_routes.py
@@ -171,13 +171,13 @@ def setup_history_routes(app):
 
 
 def setup_hmm_routes(app):
-    app.router.add_get("/api/hmm/annotations", hmm.find)
+    app.router.add_get("/api/hmms", hmm.find)
 
-    app.router.add_get("/api/hmm/annotations/{hmm_id}", hmm.get)
-    app.router.add_patch("/api/hmm/annotations/{hmm_id}", hmm.update)
+    app.router.add_get("/api/hmms/annotations/{hmm_id}", hmm.get)
+    app.router.add_patch("/api/hmms/annotations/{hmm_id}", hmm.update)
 
-    app.router.add_get("/api/hmm/check", hmm.check)
-    app.router.add_get("/api/hmm/clean", hmm.clean)
+    app.router.add_get("/api/hmms/check", hmm.check)
+    app.router.add_get("/api/hmms/clean", hmm.clean)
 
 
 def setup_subtraction_routes(app):
