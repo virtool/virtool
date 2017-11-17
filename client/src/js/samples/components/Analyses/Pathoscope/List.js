2c517e33a5e4ee0972644b13917ed74b39a9de90
igboyes <igboyes@gmail.com>
2017-11-17 14:57:06 -0800

    Show no results message when no Pathoscope hits found

--- client/src/js/samples/components/Analyses/Pathoscope/List.js
+++ client/src/js/samples/components/Analyses/Pathoscope/List.js
@@ -1,10 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
 import FlipMove from "react-flip-move"
+import { Panel } from "react-bootstrap";
 import { forIn, includes, sortBy, flatten } from "lodash";
 
 import PathoscopeEntry from "./Entry";
 import PathoscopeIsolate from "./Isolate";
+import { Icon } from "../../../../base";
 
 export default class PathoscopeList extends React.Component {
 
@@ -25,60 +27,67 @@ export default class PathoscopeList extends React.Component {
 
     render () {
 
-        let rows = this.props.data.map((item, index) => {
+        if (this.props.data.length) {
+            let rows = this.props.data.map((item, index) => {
 
-            const expanded = includes(this.props.expanded, item.id);
+                const expanded = includes(this.props.expanded, item.id);
 
-            const components = [
-                <PathoscopeEntry
-                    key={item.id}
-                    {...item}
-                    toggleIn={this.props.toggleIn}
-                    showReads={this.props.showReads}
-                    in={expanded}
-                />
-            ];
-
-            if (expanded) {
-                const isolateComponents = sortBy(item.isolates, "pi").reverse().map((isolate) => {
-                    const key = `${item.id}-${isolate.id}`;
-
-                    return <PathoscopeIsolate
-                        ref={key}
-                        key={key}
-                        virusId={item._id}
-                        maxDepth={item.maxDepth}
-                        maxGenomeLength={item.maxGenomeLength}
-                        {...isolate}
-                        setScroll={this.setScroll}
+                const components = [
+                    <PathoscopeEntry
+                        key={item.id}
+                        {...item}
+                        toggleIn={this.props.toggleIn}
                         showReads={this.props.showReads}
+                        in={expanded}
                     />
-                });
+                ];
 
-                return components.concat(
-                    <div key={index} className="list-group-item pathoscope-virus-detail spaced">
-                        {isolateComponents}
-                    </div>
-                );
-            }
+                if (expanded) {
+                    const isolateComponents = sortBy(item.isolates, "pi").reverse().map((isolate) => {
+                        const key = `${item.id}-${isolate.id}`;
 
-            return components;
+                        return <PathoscopeIsolate
+                            ref={key}
+                            key={key}
+                            virusId={item._id}
+                            maxDepth={item.maxDepth}
+                            maxGenomeLength={item.maxGenomeLength}
+                            {...isolate}
+                            setScroll={this.setScroll}
+                            showReads={this.props.showReads}
+                        />
+                    });
 
-        });
+                    return components.concat(
+                        <div key={index} className="list-group-item pathoscope-virus-detail spaced">
+                            {isolateComponents}
+                        </div>
+                    );
+                }
 
-        rows = flatten(rows);
+                return components;
 
+            });
+
+            return (
+                <div>
+                    <FlipMove
+                        typeName="div"
+                        className="list-group"
+                        enterAnimation="accordionVertical"
+                        leaveAnimation={false}
+                    >
+                        {flatten(rows)}
+                    </FlipMove>
+                </div>
+            );
+        }
+
+        // Show a message if no hits matched the filters.
         return (
-            <div>
-                <FlipMove
-                    typeName="div"
-                    className="list-group"
-                    enterAnimation="accordionVertical"
-                    leaveAnimation={false}
-                >
-                    {rows}
-                </FlipMove>
-            </div>
+            <Panel className="text-center">
+                <Icon name="info" /> No hits found.
+            </Panel>
         );
     }
 
