6c483cfafb7ac117c619a8e823237e725d78d153
igboyes <igboyes@gmail.com>
2017-11-20 14:55:40 -0800

    Organize HMMs collection

--- virtool/organize.py
+++ virtool/organize.py
@@ -107,6 +107,18 @@ async def organize_samples(db, settings):
             })
 
 
+async def organize_hmms(db):
+    await virtool.organize_utils.unset_version_field(db.hmm)
+    await db.hmm.update_many({}, {
+        "$rename": {
+            "definition": "names"
+        },
+        "$unset": {
+            "label"
+        }
+    })
+
+
 async def organize_analyses(db, logger_cb=None):
     """
     Bring analysis documents up-to-date by doing the following:
