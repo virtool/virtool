e5b00f8e8bffbfbd08ba9508ff8e720cd344918a
igboyes <igboyes@gmail.com>
2017-12-12 12:36:17 -0800

    Style improvement in app.py

--- virtool/app.py
+++ virtool/app.py
@@ -230,9 +230,9 @@ async def on_shutdown(app):
     """
     await app["dispatcher"].close()
 
-    if "job_manager" in app:
-        job_manager = app["job_manager"]
+    job_manager = app.get("job_manager", None)
 
+    if job_manager is not None:
         await job_manager.close()
 
     file_manager = app.get("file_manager", None)
