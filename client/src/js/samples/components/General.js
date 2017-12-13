3056665d9689f10582711a050abfe4e5d9c69583
igboyes <igboyes@gmail.com>
2017-12-13 08:53:28 -0800

    Improve code style in SampleGeneral component

--- client/src/js/samples/components/General.js
+++ client/src/js/samples/components/General.js
@@ -1,12 +1,10 @@
 import React from "react";
 import Moment from "moment";
 import Numeral from "numeral";
-import PropTypes from "prop-types";
 import { capitalize } from "lodash";
 import { connect } from "react-redux";
 import { Panel, Table } from "react-bootstrap";
 
-import { updateSample } from "../actions";
 import EditSample from "./Edit";
 
 const SampleDetailGeneral = (props) => {
@@ -89,46 +87,12 @@ const SampleDetailGeneral = (props) => {
     );
 };
 
-SampleDetailGeneral.propTypes = {
-    showIds: PropTypes.bool,
-    canModify: PropTypes.bool,
-    sampleId: PropTypes.string,
-    name: PropTypes.string,
-    host: PropTypes.string,
-    files: PropTypes.array,
-    isolate: PropTypes.string,
-    lengthRange: PropTypes.string,
-    encoding: PropTypes.string,
-    gc: PropTypes.string,
-    count: PropTypes.string,
-    paired: PropTypes.bool,
-    userId: PropTypes.string,
-    createdAt: PropTypes.string,
-    quality: PropTypes.object,
-    onChangeValue: PropTypes.func
-};
-
 const mapStateToProps = (state) => {
     const detail = state.samples.detail;
 
-    const isOwner = state.account.id === detail.user.id;
-
-    const canModify = (
-        detail.all_write ||
-        (detail.group_write && state.account.groups.indexOf(detail.group) > -1) ||
-        isOwner
-    );
-
     return {
-        canModify: canModify,
-        sampleId: detail.id,
-        name: detail.name,
-        host: detail.host,
-        createdAt: detail.created_at,
-        isolate: detail.isolate,
+        ...detail,
         showIds: state.account.settings.show_ids,
-        files: detail.files,
-        paired: detail.paired,
         gc: Numeral(detail.quality.gc / 100).format("0.0 %"),
         count: Numeral(detail.quality.count).format("0.0 a"),
         encoding: detail.quality.encoding,
@@ -137,16 +101,6 @@ const mapStateToProps = (state) => {
     };
 };
 
-const mapDispatchToProps = (dispatch) => {
-    return {
-        onChangeValue: (sampleId, field, value) => {
-            let update = {};
-            update[field] = value;
-            dispatch(updateSample(sampleId, update));
-        }
-    };
-};
-
-const Container = connect(mapStateToProps, mapDispatchToProps)(SampleDetailGeneral);
+const Container = connect(mapStateToProps)(SampleDetailGeneral);
 
 export default Container;
