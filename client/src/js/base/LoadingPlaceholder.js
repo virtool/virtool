21db9f15c984e43a1ad79ce37adc6f6085243cc3
igboyes <igboyes@gmail.com>
2017-12-19 14:45:22 -0800

    Increase LoadingPlaceholder spinner size

--- client/src/js/base/LoadingPlaceholder.js
+++ client/src/js/base/LoadingPlaceholder.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { ClipLoader } from "halogenium";
 
-export const LoadingPlaceholder = ({ color = "#3c8786", margin = "220px", message = null, size = "16px" }) => (
+export const LoadingPlaceholder = ({ color = "#3c8786", margin = "220px", message = null, size = "22px" }) => (
     <div className="text-center" style={{marginTop: margin}}>
         {message ? <p>{message}</p> : null}
         <ClipLoader color={color} size={size} />
