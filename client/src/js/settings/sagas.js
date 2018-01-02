251aec233c14a07333d81282501ae1693a3f1184
igboyes <igboyes@gmail.com>
2018-01-02 13:05:40 -0800

    Update settings sagas

--- client/src/js/settings/sagas.js
+++ client/src/js/settings/sagas.js
@@ -5,14 +5,20 @@ import virusesAPI from "../viruses/api";
 import { apiCall, setPending } from "../sagaUtils";
 import { GET_SETTINGS, UPDATE_SETTINGS, GET_CONTROL_READAHEAD } from "../actionTypes";
 
+export function* watchSettings () {
+    yield takeLatest(GET_SETTINGS.REQUESTED, getSettings);
+    yield takeEvery(UPDATE_SETTINGS.REQUESTED, updateSettings);
+    yield throttle(120, GET_CONTROL_READAHEAD.REQUESTED, getControlReadahead);
+}
+
 function* getSettings (action) {
-    yield setPending(apiCall(settingsAPI.get, action, GET_SETTINGS));
+    yield apiCall(settingsAPI.get, action, GET_SETTINGS);
 }
 
 function* updateSettings (action) {
-    yield setPending(function* () {
+    yield setPending(function* (action) {
         try {
-            const response = yield settingsAPI.update(action.update);
+            const response = yield settingsAPI.update(action);
             yield put({
                 type: UPDATE_SETTINGS.SUCCEEDED,
                 settings: response.body,
@@ -22,15 +28,9 @@ function* updateSettings (action) {
         } catch (error) {
             yield put({type: UPDATE_SETTINGS.FAILED, key: action.key});
         }
-    }, action);
+    }(action));
 }
 
 function* getControlReadahead (action) {
     yield setPending(apiCall(virusesAPI.listNames, action, GET_CONTROL_READAHEAD));
-}
-
-export function* watchSettings () {
-    yield takeLatest(GET_SETTINGS.REQUESTED, getSettings);
-    yield takeEvery(UPDATE_SETTINGS.REQUESTED, updateSettings);
-    yield throttle(120, GET_CONTROL_READAHEAD.REQUESTED, getControlReadahead);
 }
