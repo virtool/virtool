f2bb774af1ba76307bbe7f69e2c0d4c19e6e0a8b
igboyes <igboyes@gmail.com>
2018-01-17 15:16:29 -0800

    Fix unwanted remove on analysis click

--- client/src/js/samples/components/Analyses/Item.js
+++ client/src/js/samples/components/Analyses/Item.js
@@ -1,6 +1,8 @@
 import React from "react";
 import CX from "classnames";
+import { get } from "lodash";
 import { connect } from "react-redux";
+import { LinkContainer } from "react-router-bootstrap";
 import { ClipLoader } from "halogenium";
 import { Row, Col, Label } from "react-bootstrap";
 
@@ -9,56 +11,56 @@ import { Icon, RelativeTime } from "../../../base";
 import { removeAnalysis } from "../../actions";
 import { getCanModify } from "../../selectors";
 
-export class AnalysisItem extends React.Component {
+export const AnalysisItem = (props) => {
 
-    render () {
+    const itemClass = CX("list-group-item spaced", {hoverable: props.ready});
 
-        const itemClass = CX("list-group-item spaced", {hoverable: this.props.ready});
+    let end;
 
-        let end;
-
-        if (this.props.ready) {
-            if (this.props.canModify) {
-                end = (
-                    <Icon
-                        name="remove"
-                        bsStyle="danger"
-                        onClick={() => this.props.onRemove(this.props.id)}
-                        style={{fontSize: "17px"}}
-                        pullRight
-                    />
-                );
-            }
-        } else {
+    if (props.ready) {
+        if (props.canModify) {
             end = (
-                <strong className="pull-right">
-                    <ClipLoader size="14px" color="#3c8786" style={{display: "inline"}}/> In Progress
-                </strong>
+                <Icon
+                    name="remove"
+                    bsStyle="danger"
+                    onClick={() => props.onRemove(props.id)}
+                    style={{fontSize: "17px"}}
+                    pullRight
+                />
             );
         }
+    } else {
+        end = (
+            <strong className="pull-right">
+                <ClipLoader size="14px" color="#3c8786" style={{display: "inline"}}/> In Progress
+            </strong>
+        );
+    }
 
-        return (
-            <div className={itemClass} onClick={this.props.ready ? this.props.onRemove : null}>
+    return (
+        <LinkContainer to={`/samples/${props.sampleId}/analyses/${props.id}`}>
+            <div className={itemClass}>
                 <Row>
                     <Col md={3}>
-                        <strong>{getTaskDisplayName(this.props.algorithm)}</strong>
+                        <strong>{getTaskDisplayName(props.algorithm)}</strong>
                     </Col>
                     <Col md={4}>
-                        Started <RelativeTime time={this.props.created_at}/> by {this.props.user.id}
+                        Started <RelativeTime time={props.created_at}/> by {props.user.id}
                     </Col>
                     <Col md={1}>
-                        <Label>{this.props.index.version}</Label>
+                        <Label>{props.index.version}</Label>
                     </Col>
                     <Col md={4}>
                         {end}
                     </Col>
                 </Row>
             </div>
-        );
-    }
-}
+        </LinkContainer>
+    );
+};
 
 const mapStateToProps = (state) => ({
+    sampleId: get(state.viruses.detail, "id"),
     canModify: getCanModify(state)
 });
 
