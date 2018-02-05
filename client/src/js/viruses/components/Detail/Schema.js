b4d79b340e8678a9dba4a0996160459aa3ae77cb
Naomi <mimiijima@gmail.com>
2018-02-05 15:10:29 -0800

    Add virus schema

--- client/src/js/viruses/components/Detail/Schema.js
+++ client/src/js/viruses/components/Detail/Schema.js
@@ -1,7 +1,7 @@
 import React from "react";
-import PropTypes from "prop-types";
+//import PropTypes from "prop-types";
 
-const Schema = ({ match }) => (
+const Schema = () => (
     <div>
         <div>
             Seq-A
