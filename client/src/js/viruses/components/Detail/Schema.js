9a67b89d20022492e9a253a561c7ab2dccd54a81
naomi <mimiijima@gmail.com>
2018-02-08 14:49:50 -0800

    Stash changes

--- client/src/js/viruses/components/Detail/Schema.js
+++ client/src/js/viruses/components/Detail/Schema.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { connect } from "react-redux";
 import { DragDropContext } from "react-dnd";
 import HTML5Backend from "react-dnd-html5-backend";
 import { map, filter, concat } from "lodash-es";
 import Segment from "./Segment";
 import AddSegment from "./AddSegment";
+//import EditSegment from "./EditSegment";
+//import RemoveSegment from "./RemoveSegment";
+//import { showAddSegment, showEditSegment, showRemoveSegment } from "../../actions";
 import { Icon, Button } from "../../../base";
 
 class Schema extends React.Component {
@@ -110,4 +114,49 @@ class Schema extends React.Component {
     }
 }
 
-export default DragDropContext(HTML5Backend)(Schema);
+//export default DragDropContext(HTML5Backend)(Schema);
+
+const mapStateToProps = (state) => {
+    let sequences = null;
+    let activeIsolate = null;
+
+    const activeIsolateId = state.viruses.activeIsolateId;
+
+    if (state.viruses.detail.isolates.length) {
+        activeIsolate = find(state.viruses.detail.isolates, {id: activeIsolateId});
+        sequences = activeIsolate.sequences;
+    }
+
+    return {
+        activeIsolateId,
+        sequences,
+        virusId: state.viruses.detail.id,
+        canModify: state.account.permissions.modify_virus,
+        editing: state.viruses.editSequence,
+        isolateName: formatIsolateName(activeIsolate),
+        showAddSequence: state.viruses.showAddSequence,
+        showEditSequence: state.viruses.showEditSequence,
+        showRemoveSequence: state.viruses.showRemoveSequence
+    };
+};
+
+const mapDispatchToProps = (dispatch) => ({
+
+    showAddSegment: () => {
+        dispatch(showAddSegment());
+    },
+
+    showEditSequence: (sequenceId) => {
+        dispatch(showEditSegment(sequenceId));
+    },
+
+    showRemoveSequence: (sequenceId) => {
+        dispatch(showRemoveSegment(sequenceId));
+    }
+
+});
+
+const Schemas = DragDropContext(HTML5Backend)(Schema);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Schemas);
+
