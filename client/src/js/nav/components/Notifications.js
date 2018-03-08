b75c9bfe637703411358029a1bec7590e02bd2d2
naomi <mimiijima@gmail.com>
2018-03-08 14:41:15 -0800

    Edit rebuild notification

--- client/src/js/nav/components/Notifications.js
+++ client/src/js/nav/components/Notifications.js
@@ -17,7 +17,7 @@ const getInitialState = (props) => {
 
     }
 
-    if (props.unbuilt && props.unbuilt.history.length) {
+    if (props.modifiedCount || props.modifiedVirusCount) {
         notifArray.push({
             message: "Rebuild Index",
             link: "/viruses/indexes"
@@ -96,7 +96,8 @@ class Notifications extends React.Component {
 
 const mapStateToProps = (state) => ({
     updates: state.updates.software,
-    unbuilt: state.indexes.unbuilt,
+    modifiedVirusCount: state.indexes.modified_virus_count,
+    modifiedCount: state.viruses.modified_count,
     canUpdate: state.account.permissions.modify_settings
 });
 
