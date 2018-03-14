d00345284c7642dbad32cc39877c18224a7f037e
naomi <mimiijima@gmail.com>
2018-03-14 09:24:53 -0700

    Edit component error render

--- client/src/js/viruses/components/Create.js
+++ client/src/js/viruses/components/Create.js
@@ -18,15 +18,21 @@ class CreateVirus extends React.Component {
     constructor (props) {
         super(props);
 
-        this.state = {show: false, ...getInitialState()};
+        this.state = getInitialState();
     }
 
     componentWillReceiveProps (nextProps) {
-        if (nextProps.error !== this.props.error) {
-            this.setState({
-                error: nextProps.error
+        console.log("this.props.errors: ", this.props.errors);
+        console.log("nextProps.errors: ", nextProps.errors);
+
+        if (nextProps.errors && nextProps.errors.CREATE_VIRUS_ERROR) {
+            console.log("set state error message");
+            this.setState(() => {
+                return { error: nextProps.errors.CREATE_VIRUS_ERROR.message };
             });
         }
+
+        console.log("current state: ", this.state);
     }
 
     handleChange = (e) => {
@@ -42,25 +48,29 @@ class CreateVirus extends React.Component {
     };
 
     handleModalExited = () => {
+        console.log("modal exit");
         this.setState(getInitialState());
     };
 
     handleSubmit = (e) => {
         e.preventDefault();
 
-        if (this.state.name) {
-            this.setState({show: false});
-            this.props.onSubmit(this.state.name, this.state.abbreviation);
-            this.handleHide();
-        } else {
+        console.log("inside submit");
+
+        if (this.state.name && !this.state.error) {
+            console.log("nonempty name, no error: submit form");
+            () => (this.props.onSubmit(this.state.name, this.state.abbreviation));
+            console.log("check for error update: ", this.state.error);
+            // this.props.onHide();
+        } else if (!this.state.name) {
             this.setState({
-                show: true,
                 error: "Required Field"
             });
         }
     };
 
     render () {
+        console.log("RENDER");
 
         return (
             <Modal show={this.props.show} onHide={this.handleHide} onExited={this.handleModalExited}>
@@ -105,6 +115,7 @@ class CreateVirus extends React.Component {
 const mapStateToProps = state => ({
     show: !!state.router.location.state && state.router.location.state.createVirus,
     error: state.viruses.createError,
+    errors: state.errors,
     pending: state.viruses.createPending
 });
 
