3608ff808319723f5f7502c33e9d44b61a6041e0
naomi <mimiijima@gmail.com>
2018-03-22 13:46:27 -0700

    Edit email

--- client/src/__tests__/Email.test.js
+++ client/src/__tests__/Email.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Enzyme, { mount } from "enzyme";
+import Enzyme, { mount, shallow, render } from "enzyme";
 import Adapter from "enzyme-adapter-react-15";
 import EmailContainer, { Email } from "../js/account/components/Email";
 
@@ -20,9 +20,43 @@ function setup() {
 }
 
 describe("Email component", () => {
-    it("should render self and child components", () => {
+    it("should render without throwing error", () => {
         const { enzymeWrapper } = setup();
 
-        
+        //TODO
     });
+
+    it("should be selectable by class 'classname'", () => {
+        // Shallow Rendering
+        const wrapper = shallow(<Email />);
+
+        expect(wrapper.is(".classname")).toBe(true);
+    });
+
+    it("should mount in full DOM", () => {
+        // Full rendering
+        const wrapper = mount(<Email />);
+
+        expect(wrapper.find(".classname")).toBe(1);
+    });
+
+    it("should render to static HTML", () => {
+        // Static rendering
+        const wrapper = render(<Email />);
+
+        expect(wrapper.text()).toBe("text");
+    });
+
+
 });
+
+
+// BASE COMPONENT
+// renders, renders children, no props, invalid props, valid props, edge case props
+// functions, arguments; valid, invalid, missing
+
+// given a set of props and state, assert on component output
+// given an event, assert on behaviour of component
+
+// CONTAINER COMPONENT
+// renders, redux testing
