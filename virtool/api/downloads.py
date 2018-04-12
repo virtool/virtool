8c25111c2a956e693551398ecfb2a4c0252862bd
igboyes <igboyes@gmail.com>
2018-04-12 08:37:17 -0700

    Use db.downloads in handlers

--- virtool/api/downloads.py
+++ virtool/api/downloads.py
@@ -9,88 +9,59 @@ import virtool.db.downloads
 import virtool.kinds
 
 
-async def download_sequence(req):
+async def download_isolate(req):
+    """
+    Download a FASTA file containing the sequences for a single Virtool isolate.
+
+    """
     db = req.app["db"]
 
-    sequence_id = req.match_info["sequence_id"]
+    kind_id = req.match_info["kind_id"]
+    isolate_id = req.match_info["isolate_id"]
 
-    fasta = await virtool.db.downloads.generate_sequence_fasta(db, sequence_id)
+    filename, fasta = await virtool.db.downloads.generate_isolate_fasta(db, kind_id, isolate_id)
 
     if fasta is None:
         return web.Response(status=404)
 
     return web.Response(text=fasta, headers={
-        "Content-Disposition": "attachment; filename='{}.{}.{}.fa'".format(
-            virus["name"].replace(" ", "_").lower(),
-            isolate_name.replace(" ", "_").lower(),
-            sequence_id.lower()
-        )
+        "Content-Disposition": "attachment; filename='{}'".format(filename)
     })
 
 
-async def download_isolate_sequences(req):
+async def download_kind(req):
+    """
+    Download a FASTA file containing the sequences for all isolates in a single Virtool kind.
+
+    """
     db = req.app["db"]
 
-    virus_id = req.match_info["virus_id"]
-    isolate_id = req.match_info["isolate_id"]
+    kind_id = req.match_info["kind_id"]
 
-    virus = await db.viruses.find_one({"_id": virus_id, "isolates.id": isolate_id}, ["name", "isolates"])
+    filename, fasta = await virtool.db.downloads.generate_isolate_fasta(db, kind_id)
 
-    if not virus:
+    if not fasta:
         return web.Response(status=404)
-
-    isolate = virtool.kinds.find_isolate(virus["isolates"], isolate_id)
-
-    if isolate is None:
-        return web.Response(status=404)
-
-    isolate_name = virtool.kinds.format_isolate_name(isolate)
-
-    fasta = list()
-
-    async for sequence in db.sequences.find({"virus_id": virus_id, "isolate_id": isolate_id}, ["sequence"]):
-        seq = sequence["sequence"]
-
-        fasta.append(">{}|{}|{}|{}\n{}".format(virus["name"], isolate_name, sequence["_id"], len(seq), seq))
-
-    fasta = "\n".join(fasta)
 
     return web.Response(text=fasta, headers={
-        "Content-Disposition": "attachment; filename='{}.{}.fa'".format(
-            virus["name"].replace(" ", "_").lower(),
-            isolate_name.replace(" ", "_").lower()
-        )
+        "Content-Disposition": "attachment; filename='{}'".format(filename)
     })
 
 
-async def download_virus_sequences(req):
+async def download_sequence(req):
+    """
+    Download a FASTA file containing a single Virtool sequence.
+
+    """
     db = req.app["db"]
 
-    virus_id = req.match_info["virus_id"]
+    sequence_id = req.match_info["sequence_id"]
 
-    virus = await db.viruses.find_one(virus_id, ["name", "isolates"])
+    filename, fasta = await virtool.db.downloads.generate_sequence_fasta(db, sequence_id)
 
-    if not virus:
+    if fasta is None:
         return web.Response(status=404)
 
-    fasta = list()
-
-    for isolate in virus["isolates"]:
-        isolate_name = virtool.kinds.format_isolate_name(isolate)
-
-        async for sequence in db.sequences.find({"isolate_id": isolate["id"]}, ["sequence"]):
-            seq = sequence["sequence"]
-            fasta.append(virtool.downloads.format_fasta_entry(
-                virus["name"],
-                isolate_name,
-                sequence["_id"],
-                seq
-            ))
-
-    fasta = "\n".join(fasta)
-
     return web.Response(text=fasta, headers={
-        "Content-Disposition": "attachment; filename='{}.fa'".format(
-            virus["name"].replace(" ", "_").lower()
-        )
+        "Content-Disposition": "attachment; filename='{}'".format(filename)
     })
