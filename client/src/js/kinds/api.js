0e05e11964c5cbc888661d0331008b5b42917f79
igboyes <igboyes@gmail.com>
2018-04-24 16:06:01 -0700

    Add basis for kinds in client src

---
+++ client/src/js/kinds/api.js
@@ -0,0 +1,105 @@
+import Request from "superagent";
+
+export const find = () => (
+    Request.get(`/api/viruses${window.location.search}`)
+);
+
+export const listNames = () => (
+    Request.get("/api/viruses?names=true")
+);
+
+export const get = ({ virusId }) => (
+    Request.get(`/api/viruses/${virusId}`)
+);
+
+export const getHistory = ({ virusId }) => (
+    Request.get(`/api/viruses/${virusId}/history`)
+);
+
+export const getGenbank = (accession) => (
+    Request.get(`/api/genbank/${accession}`)
+);
+
+export const create = ({ name, abbreviation }) => (
+    Request.post("/api/viruses")
+        .send({
+            name,
+            abbreviation
+        })
+);
+
+export const edit = ({ virusId, name, abbreviation, schema }) => (
+    Request.patch(`/api/viruses/${virusId}`)
+        .send({
+            name,
+            abbreviation,
+            schema
+        })
+);
+
+export const remove = ({ virusId }) => (
+    Request.delete(`/api/viruses/${virusId}`)
+);
+
+export const addIsolate = ({ virusId, sourceType, sourceName }) => (
+    Request.post(`/api/viruses/${virusId}/isolates`)
+        .send({
+            source_type: sourceType,
+            source_name: sourceName
+        })
+);
+
+export const editIsolate = ({ virusId, isolateId, sourceType, sourceName }) => (
+    Request.patch(`/api/viruses/${virusId}/isolates/${isolateId}`)
+        .send({
+            source_type: sourceType,
+            source_name: sourceName
+        })
+);
+
+export const setIsolateAsDefault = ({ virusId, isolateId }) => (
+    Request.put(`/api/viruses/${virusId}/isolates/${isolateId}/default`)
+);
+
+export const removeIsolate = ({ virusId, isolateId }) => (
+    Request.delete(`/api/viruses/${virusId}/isolates/${isolateId}`)
+);
+
+export const addSequence = ({ virusId, isolateId, sequenceId, definition, host, sequence, segment }) => (
+    Request.post(`/api/viruses/${virusId}/isolates/${isolateId}/sequences`)
+        .send({
+            id: sequenceId,
+            definition,
+            host,
+            sequence,
+            segment
+        })
+);
+
+export const editSequence = ({ virusId, isolateId, sequenceId, definition, host, sequence, segment }) => (
+    Request.patch(`/api/viruses/${virusId}/isolates/${isolateId}/sequences/${sequenceId}`)
+        .send({
+            definition,
+            host,
+            sequence,
+            segment
+        })
+);
+
+export const removeSequence = ({ virusId, isolateId, sequenceId }) => (
+    Request.delete(`/api/viruses/${virusId}/isolates/${isolateId}/sequences/${sequenceId}`)
+);
+
+export const revert = ({ virusId, version }) => (
+    Request.delete(`/api/history/${virusId}.${version}`)
+);
+
+export const getImport = ({ fileId }) => (
+    Request.get("/api/viruses/import")
+        .query({file_id: fileId})
+);
+
+export const commitImport = ({ fileId }) => (
+    Request.post("/api/viruses/import")
+        .send({file_id: fileId})
+);
---
+++ client/src/js/kinds/reducer.js
@@ -0,0 +1,187 @@
+import { find, map } from "lodash-es";
+
+import { formatIsolateName } from "../utils";
+import {
+    WS_UPDATE_STATUS,
+    FIND_VIRUSES,
+    GET_VIRUS,
+    CREATE_VIRUS,
+    EDIT_VIRUS,
+    REMOVE_VIRUS,
+    ADD_ISOLATE,
+    EDIT_ISOLATE,
+    SET_ISOLATE_AS_DEFAULT,
+    REMOVE_ISOLATE,
+    ADD_SEQUENCE,
+    EDIT_SEQUENCE,
+    REMOVE_SEQUENCE,
+    REVERT,
+    UPLOAD_IMPORT,
+    SELECT_ISOLATE,
+    SHOW_EDIT_VIRUS,
+    SHOW_REMOVE_VIRUS,
+    SHOW_ADD_ISOLATE,
+    SHOW_EDIT_ISOLATE,
+    SHOW_REMOVE_ISOLATE,
+    SHOW_ADD_SEQUENCE,
+    SHOW_EDIT_SEQUENCE,
+    SHOW_REMOVE_SEQUENCE,
+    HIDE_VIRUS_MODAL,
+    GET_VIRUS_HISTORY
+} from "../actionTypes";
+
+const initialState = {
+    documents: null,
+    detail: null,
+    detailHistory: null,
+    edit: false,
+    remove: false,
+    addIsolate: false,
+    editIsolate: false,
+    removeIsolate: false,
+    addSequence: false,
+    editSequence: false,
+    removeSequence: false,
+    activeIsolateId: null,
+    createError: "",
+    editError: "",
+    importData: null
+};
+
+const hideVirusModal = state => ({
+    ...state,
+    edit: false,
+    remove: false,
+    addIsolate: false,
+    editIsolate: false,
+    removeIsolate: false,
+    addSequence: false,
+    editSequence: false,
+    removeSequence: false
+});
+
+const getActiveIsolate = (state) => {
+    const isolates = state.detail.isolates;
+
+    if (isolates.length) {
+        const activeIsolate = find(isolates, {id: state.activeIsolateId}) || isolates[0];
+
+        return {
+            ...state,
+            activeIsolate,
+            activeIsolateId: activeIsolate.id
+        };
+    }
+
+    return {
+        ...state,
+        activeIsolate: null,
+        activeIsolateId: null
+    };
+};
+
+const receiveVirus = (state, action) => {
+    const detail = {
+        ...action.data,
+        isolates: map(action.data.isolates, isolate => ({...isolate, name: formatIsolateName(isolate)}))
+    };
+
+    return getActiveIsolate({...state, detail});
+};
+
+export default function virusesReducer (state = initialState, action) {
+
+    switch (action.type) {
+
+        case WS_UPDATE_STATUS:
+            if (action.data.id === "virus_import") {
+                return {...state, importData: {...state.importData, ...action.data, inProgress: true}};
+            }
+
+            return state;
+
+        case FIND_VIRUSES.REQUESTED:
+            return {...state, ...action.terms};
+
+        case FIND_VIRUSES.SUCCEEDED:
+            return {...state, ...action.data};
+
+        case CREATE_VIRUS.REQUESTED:
+            return {...state, createError: null};
+
+        case CREATE_VIRUS.FAILED:
+            return {...state, createError: action.message};
+
+        case GET_VIRUS.REQUESTED:
+        case REMOVE_VIRUS.SUCCEEDED:
+            return hideVirusModal({...state, detail: null, activeIsolateId: null});
+
+        case EDIT_VIRUS.FAILED:
+            if (action.status === 409) {
+                return {...state, editError: action.message};
+            }
+
+            return state;
+
+        case GET_VIRUS.SUCCEEDED:
+        case EDIT_VIRUS.SUCCEEDED:
+        case EDIT_ISOLATE.SUCCEEDED:
+        case ADD_SEQUENCE.SUCCEEDED:
+        case EDIT_SEQUENCE.SUCCEEDED:
+        case REMOVE_SEQUENCE.SUCCEEDED:
+        case SET_ISOLATE_AS_DEFAULT.SUCCEEDED:
+        case ADD_ISOLATE.SUCCEEDED:
+        case REMOVE_ISOLATE.SUCCEEDED: {
+            return hideVirusModal(receiveVirus(state, action));
+        }
+
+        case GET_VIRUS_HISTORY.REQUESTED:
+            return {...state, detailHistory: null};
+
+        case GET_VIRUS_HISTORY.SUCCEEDED:
+            return {...state, detailHistory: action.data};
+
+        case REVERT.SUCCEEDED:
+            return {...receiveVirus(state, action), detailHistory: action.history};
+
+        case UPLOAD_IMPORT.SUCCEEDED:
+            return {...state, importData: {...action.data, inProgress: false}};
+
+        case SELECT_ISOLATE:
+            return {
+                ...state,
+                activeIsolate: find(state.detail.isolates, {id: action.isolateId}),
+                activeIsolateId: action.isolateId
+            };
+
+        case SHOW_EDIT_VIRUS:
+            return {...state, edit: true, editError: ""};
+
+        case SHOW_REMOVE_VIRUS:
+            return {...state, remove: true};
+
+        case SHOW_ADD_ISOLATE:
+            return {...state, addIsolate: true};
+
+        case SHOW_EDIT_ISOLATE:
+            return {...state, editIsolate: true};
+
+        case SHOW_REMOVE_ISOLATE:
+            return {...state, removeIsolate: true};
+
+        case SHOW_ADD_SEQUENCE:
+            return {...state, addSequence: true};
+
+        case SHOW_EDIT_SEQUENCE:
+            return {...state, editSequence: action.sequenceId};
+
+        case SHOW_REMOVE_SEQUENCE:
+            return {...state, removeSequence: action.sequenceId};
+
+        case HIDE_VIRUS_MODAL:
+            return hideVirusModal(state);
+
+        default:
+            return state;
+    }
+}
