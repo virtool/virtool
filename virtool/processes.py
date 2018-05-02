f946ca92b74bb945c3884fcfa3e515132ec56b06
igboyes <igboyes@gmail.com>
2018-05-02 12:49:03 -0700

    Add ProgressTracker class for tracking progress in long operations

--- virtool/processes.py
+++ virtool/processes.py
@@ -18,3 +18,32 @@ UNIQUES = [
     "update_software",
     "install_hmms"
 ]
+
+
+class ProgressTracker:
+
+    def __init__(self, total, db=None, increment=0.05, factor=1):
+        self.total = total
+        self.db = db
+        self.increment = increment
+        self.factor = factor
+
+        self.count = 0
+        self.last_reported = 0
+
+    def add(self, value):
+        count = self.count + value
+
+        if count > self.total:
+            raise ValueError("Count cannot exceed total")
+
+        self.count = count
+
+        return self.progress
+
+    async def reported(self):
+        self.last_reported = self.progress
+
+    @property
+    def progress(self):
+        return round(self.count / self.total, 2)
