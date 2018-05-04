273a6e4a1d2c84c447d79eedb0c86ab54e82af84
igboyes <igboyes@gmail.com>
2018-05-04 09:01:19 -0700

    Simplify ReferenceItem

--- client/src/js/references/components/ReferenceItem.js
+++ client/src/js/references/components/ReferenceItem.js
@@ -1,12 +1,13 @@
 import React from "react";
-import { LinkContainer } from "react-router-bootstrap";
-import { FlexItem, RelativeTime, Button } from "../../base";
-import { Panel, Table, Row, Badge } from "react-bootstrap";
+import { connect } from "react-redux";
+import { push } from "react-router-redux";
+import { RelativeTime } from "../../base";
+import { Panel, Table, Row } from "react-bootstrap";
 
 const ReferenceHeader = ({ name, createdAt, user }) => (
     <div style={{ marginLeft: "5px" }}>
         <Row>
-            <strong>{name}</strong><Badge style={{ margin: "5px" }}>count</Badge>
+            <strong>{name}</strong>
         </Row>
         <Row>
             <small>
@@ -16,67 +17,39 @@ const ReferenceHeader = ({ name, createdAt, user }) => (
     </div>
 );
 
-const ReferenceMetadata = ({ dataType, organism, isPublic }) => (
+const ReferenceMetadata = ({ data_type, organism }) => (
     <Table bordered>
         <tbody>
             <tr>
                 <th>Data Type</th>
                 <td className="text-capitalize">
-                    { dataType }
+                    {data_type}
                 </td>
             </tr>
             <tr>
                 <th>Organism</th>
                 <td className="text-capitalize">
-                    { organism || "unknown" }
+                    {organism || "unknown"}
                 </td>
-            </tr>
-            <tr>
-                <th>Public</th>
-                <td>{`${isPublic}`}</td>
             </tr>
         </tbody>
     </Table>
 );
 
-const ReferenceFooter = ({ id }) => (
-    <div style={{ margin: "0 5px" }}>
-        <Row>
-            <Button>
-                View Kinds
-            </Button>
-            <LinkContainer to={`/refs/${id}`} key={id} className="spaced">
-                <Button icon="edit" pullRight>
-                    Manage
-                </Button>
-            </LinkContainer>
-        </Row>
-    </div>
-);
-
 const ReferenceItem = (props) => (
-    <FlexItem
-        alignSelf="auto"
-        grow={0}
-        shrink={0}
-        basis="auto"
-        style={{ margin: "1rem 1rem 0 0", minWidth: "300px" }}
-    >
-        <Panel>
-            <Panel.Heading>
-                <ReferenceHeader name={props.name} createdAt={props.created_at} user={props.user.id} />
-            </Panel.Heading>
-            <ReferenceMetadata
-                dataType={props.data_type}
-                build={props.latest_build}
-                organism={props.organism}
-                isPublic={props.public}
-            />
-            <Panel.Footer>
-                <ReferenceFooter id={props.id} />
-            </Panel.Footer>
-        </Panel>
-    </FlexItem>
+    <Panel className="reference-item" onClick={props.onClick}>
+        <Panel.Heading>
+            <ReferenceHeader name={props.name} createdAt={props.created_at} user={props.user.id} />
+        </Panel.Heading>
+
+        <ReferenceMetadata {...props} />
+    </Panel>
 );
 
-export default ReferenceItem;
+const mapDispatchToProps = (dispatch, ownProps) => ({
+    onClick: () => {
+        dispatch(push(`/refs/${ownProps.id}`));
+    }
+});
+
+export default connect(() => ({}), mapDispatchToProps)(ReferenceItem);
