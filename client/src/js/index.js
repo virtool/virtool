00e815fec7a84722ab87bccd27cec7e86217cf40
Ian Boyes <igboyes@gmail.com>
2018-05-07 10:21:54 -0700

    Update index.js

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -20,7 +20,7 @@ import groupsReducer from "./groups/reducer";
 import hmmsReducer from "./hmm/reducer";
 import indexesReducer from "./indexes/reducer";
 import jobsReducer from "./jobs/reducer";
-import OTUsReducer from "./otus/reducer";
+import otusReducer from "./otus/reducer";
 import subtractionReducer from "./subtraction/reducer";
 import referencesReducer from "./references/reducer";
 import samplesReducer from "./samples/reducer";
@@ -68,7 +68,7 @@ const store = createStore(
         hmms: hmmsReducer,
         indexes: indexesReducer,
         jobs: jobsReducer,
-        OTUs: OTUsReducer,
+        otus: otusReducer,
         references: referencesReducer,
         router: routerReducer,
         samples: samplesReducer,
