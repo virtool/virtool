c76a20672f6e07fdab3d41f9ca10b55918a87eab
igboyes <igboyes@gmail.com>
2018-05-17 10:26:16 -0700

    Remove /api/refs/{ref_id}/unbuilt

--- virtool/api/references.py
+++ virtool/api/references.py
@@ -311,26 +311,6 @@ async def edit(req):
     return json_response(document)
 
 
-@routes.get("/api/refs/{ref_id}/unbuilt")
-async def get_unbuilt_changes(req):
-    """
-    Get a JSON document describing the unbuilt changes that could be used to create a new index.
-
-    """
-    db = req.app["db"]
-
-    ref_id = req.match_info["ref_id"]
-
-    history = await db.history.find({
-        "reference.id": ref_id,
-        "index.id": "unbuilt"
-    }, virtool.db.history.LIST_PROJECTION).to_list(None)
-
-    return json_response({
-        "history": [virtool.utils.base_processor(c) for c in history]
-    })
-
-
 @routes.delete("/api/refs/{ref_id}")
 async def remove(req):
     """
