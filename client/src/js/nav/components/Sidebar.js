47e6bb63c720cb555e16f8760f3aafecb09118dd
igboyes <igboyes@gmail.com>
2018-06-13 13:51:02 -0700

    Use Switch-Route to populate SideBar

--- client/src/js/nav/components/Sidebar.js
+++ client/src/js/nav/components/Sidebar.js
@@ -1,117 +1,101 @@
 import React from "react";
 import { connect } from "react-redux";
-import { withRouter } from "react-router-dom";
+import { Route, Switch, withRouter } from "react-router-dom";
 import { Nav } from "react-bootstrap";
 import SidebarItem from "./SidebarItem";
-import { startsWith } from "lodash-es";
 
-const isHomeActive = (location) => {
-    if (startsWith(location.pathname, "/jobs")) {
-        return "Jobs";
-    } else if (startsWith(location.pathname, "/samples")) {
-        return "Samples";
-    } else if (startsWith(location.pathname, "/refs")) {
-        return "References";
-    } else if (startsWith(location.pathname, "/subtraction")) {
-        return "Subtraction";
-    } else if (startsWith(location.pathname, "/hmm")) {
-        return "HMM";
-    }
-};
-
-const Sidebar = (props) => {
-
-    const activeTab = isHomeActive(props.location);
-
-    switch (activeTab) {
-        case "Jobs":
-            return (
-                <Nav className="sidebar">
-                    <SidebarItem
-                        exclude={["/jobs/resources", "/jobs/settings"]}
-                        title="Browse"
-                        link="/jobs"
-                        icon="th-list"
-                    />
-                    <SidebarItem
-                        title="Resources"
-                        link="/jobs/resources"
-                        icon="tachometer-alt"
-                    />
-                    <SidebarItem
-                        title="Settings"
-                        link="/jobs/settings"
-                        icon="cogs"
-                    />
-                </Nav>
-            );
-        case "Samples":
-            return (
-                <Nav className="sidebar">
-                    <SidebarItem
-                        exclude={["/samples/files", "/samples/settings"]}
-                        title="Browse"
-                        link="/samples"
-                        icon="th-list"
-                    />
-                    <SidebarItem
-                        title="Files"
-                        link="/samples/files"
-                        icon="folder-open"
-                    />
-                    <SidebarItem
-                        title="Settings"
-                        link="/samples/settings"
-                        icon="cogs"
-                    />
-                </Nav>
-            );
-        case "References":
-            return (
-                <Nav className="sidebar">
-                    <SidebarItem
-                        exclude={["/refs/settings"]}
-                        title="Browse"
-                        link="/refs"
-                        icon="th-list"
-                    />
-                    <SidebarItem
-                        title="Settings"
-                        link="/refs/settings"
-                        icon="cogs"
-                    />
-                </Nav>
-            );
-        case "Subtraction":
-            return (
-                <Nav className="sidebar">
-                    <SidebarItem
-                        exclude={["/subtraction/files"]}
-                        title="Browse"
-                        link="/subtraction"
-                        icon="th-list"
-                    />
-                    <SidebarItem
-                        title="Files"
-                        link="/subtraction/files"
-                        icon="folder-open"
-                    />
-                </Nav>
-            );
-        case "HMM":
-            return (
-                <Nav className="sidebar">
-                    <SidebarItem
-                        title="Browse"
-                        link="/hmm"
-                        icon="th-list"
-                    />
-                </Nav>
-            );
-        default:
-            return <Nav className="sidebar" />;
-    }
-};
+const Sidebar = () => (
+    <Switch>
+        <Route path="/home">
+            <Nav className="sidebar">
+                <SidebarItem
+                    title="About"
+                    link="/home"
+                    icon="info-circle"
+                />
+            </Nav>
+        </Route>
+        <Route path="/jobs">
+            <Nav className="sidebar">
+                <SidebarItem
+                    exclude={["/jobs/resources", "/jobs/settings"]}
+                    title="Browse"
+                    link="/jobs"
+                    icon="th-list"
+                />
+                <SidebarItem
+                    title="Resources"
+                    link="/jobs/resources"
+                    icon="tachometer-alt"
+                />
+                <SidebarItem
+                    title="Settings"
+                    link="/jobs/settings"
+                    icon="cogs"
+                />
+            </Nav>
+        </Route>
+        <Route path="/samples">
+            <Nav className="sidebar">
+                <SidebarItem
+                    exclude={["/samples/files", "/samples/settings"]}
+                    title="Browse"
+                    link="/samples"
+                    icon="th-list"
+                />
+                <SidebarItem
+                    title="Files"
+                    link="/samples/files"
+                    icon="folder-open"
+                />
+                <SidebarItem
+                    title="Settings"
+                    link="/samples/settings"
+                    icon="cogs"
+                />
+            </Nav>
+        </Route>
+        <Route path="/refs">
+            <Nav className="sidebar">
+                <SidebarItem
+                    exclude={["/refs/settings"]}
+                    title="Browse"
+                    link="/refs"
+                    icon="th-list"
+                />
+                <SidebarItem
+                    title="Settings"
+                    link="/refs/settings"
+                    icon="cogs"
+                />
+            </Nav>
+        </Route>
+        <Route path="/subtraction">
+            <Nav className="sidebar">
+                <SidebarItem
+                    exclude={["/subtraction/files"]}
+                    title="Browse"
+                    link="/subtraction"
+                    icon="th-list"
+                />
+                <SidebarItem
+                    title="Files"
+                    link="/subtraction/files"
+                    icon="folder-open"
+                />
+            </Nav>
+        </Route>
+        <Route path="/hmm">
+            <Nav className="sidebar">
+                <SidebarItem
+                    title="Browse"
+                    link="/hmm"
+                    icon="th-list"
+                />
+            </Nav>
+        </Route>
+    </Switch>
+);
 
 const mapStateToProps = (state) => ({
     ...state.account
