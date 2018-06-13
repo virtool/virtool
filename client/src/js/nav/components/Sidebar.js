8201e609e5623b28c1b144cda1f295659d54032d
igboyes <igboyes@gmail.com>
2018-06-13 13:34:07 -0700

    Update Sidebar appearance

--- client/src/js/nav/components/Sidebar.js
+++ client/src/js/nav/components/Sidebar.js
@@ -2,7 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 import { Nav } from "react-bootstrap";
-import { SidebarItem } from "./SidebarItem";
+import SidebarItem from "./SidebarItem";
 import { startsWith } from "lodash-es";
 
 const isHomeActive = (location) => {
@@ -27,37 +27,85 @@ const Sidebar = (props) => {
         case "Jobs":
             return (
                 <Nav className="sidebar">
-                    <SidebarItem title="Browse" link="/jobs" icon="th-list" />
-                    <SidebarItem title="Resources" link="/jobs/resources" icon="tachometer-alt" />
-                    <SidebarItem title="Settings" link="/jobs/settings" icon="cogs" />
+                    <SidebarItem
+                        exclude={["/jobs/resources", "/jobs/settings"]}
+                        title="Browse"
+                        link="/jobs"
+                        icon="th-list"
+                    />
+                    <SidebarItem
+                        title="Resources"
+                        link="/jobs/resources"
+                        icon="tachometer-alt"
+                    />
+                    <SidebarItem
+                        title="Settings"
+                        link="/jobs/settings"
+                        icon="cogs"
+                    />
                 </Nav>
             );
         case "Samples":
             return (
                 <Nav className="sidebar">
-                    <SidebarItem title="Browse" link="/samples" icon="th-list" />
-                    <SidebarItem title="Files" link="/samples/files" icon="folder-open" />
-                    <SidebarItem title="Settings" link="/samples/settings" icon="cogs" />
+                    <SidebarItem
+                        exclude={["/samples/files", "/samples/settings"]}
+                        title="Browse"
+                        link="/samples"
+                        icon="th-list"
+                    />
+                    <SidebarItem
+                        title="Files"
+                        link="/samples/files"
+                        icon="folder-open"
+                    />
+                    <SidebarItem
+                        title="Settings"
+                        link="/samples/settings"
+                        icon="cogs"
+                    />
                 </Nav>
             );
         case "References":
             return (
                 <Nav className="sidebar">
-                    <SidebarItem title="Browse" link="/refs" icon="th-list" />
-                    <SidebarItem title="Settings" link="/refs/settings" icon="cogs" />
+                    <SidebarItem
+                        exclude={["/refs/settings"]}
+                        title="Browse"
+                        link="/refs"
+                        icon="th-list"
+                    />
+                    <SidebarItem
+                        title="Settings"
+                        link="/refs/settings"
+                        icon="cogs"
+                    />
                 </Nav>
             );
         case "Subtraction":
             return (
                 <Nav className="sidebar">
-                    <SidebarItem title="Browse" link="/subtraction" icon="th-list" />
-                    <SidebarItem title="Files" link="/subtraction/files" icon="folder-open" />
+                    <SidebarItem
+                        exclude={["/subtraction/files"]}
+                        title="Browse"
+                        link="/subtraction"
+                        icon="th-list"
+                    />
+                    <SidebarItem
+                        title="Files"
+                        link="/subtraction/files"
+                        icon="folder-open"
+                    />
                 </Nav>
             );
         case "HMM":
             return (
                 <Nav className="sidebar">
-                    <SidebarItem title="Browse" link="/hmm" icon="th-list" />
+                    <SidebarItem
+                        title="Browse"
+                        link="/hmm"
+                        icon="th-list"
+                    />
                 </Nav>
             );
         default:
--- client/src/js/nav/components/SidebarItem.js
+++ client/src/js/nav/components/SidebarItem.js
@@ -1,15 +1,29 @@
 import React from "react";
-import { NavItem } from "react-bootstrap";
-import { LinkContainer } from "react-router-bootstrap";
+import { includes } from "lodash-es";
+import { NavLink } from "react-router-dom";
 import { Icon } from "../../base";
 
-export const SidebarItem = (props) => (
+function excludePaths (paths = []) {
+    return function (match, location) {
+        console.log(match, location);
 
-    <LinkContainer to={props.link}>
-        <NavItem className="sidebar-item">
-            <Icon name={props.icon} />
-            <div>{props.title}</div>
-        </NavItem>
-    </LinkContainer>
+        if (includes(paths, location.pathname)) {
+            return false;
+        }
 
-);
+        return !!match;
+    };
+}
+
+export default function SidebarItem ({ exclude, icon, link, title }) {
+    return (
+        <NavLink to={link} className="sidebar-item" activeClassName="active" isActive={excludePaths(exclude)}>
+            <Icon name={icon} />
+            <div>
+                <small>
+                    {title}
+                </small>
+            </div>
+        </NavLink>
+    );
+}
