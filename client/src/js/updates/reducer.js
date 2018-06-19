589ff017165e62dc2e20624f3f377c64be5085d5
igboyes <igboyes@gmail.com>
2018-06-19 15:22:08 -0700

    Update updates reducer

--- client/src/js/updates/reducer.js
+++ client/src/js/updates/reducer.js
@@ -1,4 +1,6 @@
+import { get } from "lodash-es";
 import {
+    WS_UPDATE_PROCESS,
     WS_UPDATE_STATUS,
     GET_SOFTWARE_UPDATES,
     SHOW_INSTALL_MODAL,
@@ -6,7 +8,7 @@ import {
 } from "../actionTypes";
 
 export const initialState = {
-    software: null,
+    releases: null,
     showInstallModal: false
 };
 
@@ -14,15 +16,22 @@ export default function updatesReducer (state = initialState, action) {
 
     switch (action.type) {
 
+        case WS_UPDATE_PROCESS:
+            if (action.data.id === get(state, "process.id")) {
+                return {...state, process: action.data};
+            }
+
+            return state;
+
         case WS_UPDATE_STATUS:
             if (action.data.id === "software") {
-                return {...state, software: action.data};
+                return {...state, ...action.data};
             }
 
             return state;
 
         case GET_SOFTWARE_UPDATES.SUCCEEDED:
-            return {...state, software: action.data};
+            return {...state, ...action.data};
 
         case SHOW_INSTALL_MODAL:
             return {...state, showInstallModal: true};
