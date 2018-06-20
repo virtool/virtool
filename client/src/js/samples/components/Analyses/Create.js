2fcce1e30f6914b72287fd723651cbb4146f40cb
naomi <mimiijima@gmail.com>
2018-06-20 16:23:16 -0700

    Fix pathoscope empty reference analysis creation

--- client/src/js/samples/components/Analyses/Create.js
+++ client/src/js/samples/components/Analyses/Create.js
@@ -5,37 +5,52 @@ import { Modal, ListGroup, Col, Label } from "react-bootstrap";
 import { AlgorithmSelect, Button, ListGroupItem, NoneFound, Checkbox } from "../../../base";
 import { getTaskDisplayName } from "../../../utils";
 
-const IndexSelect = ({ indexes, onSelect, selected }) => (
-    <div>
-        <label className="control-label">References</label>
-        {indexes.length
-            ? (
-                <ListGroup style={{maxHeight: "165px", overflowY: "auto"}}>
-                    {map(indexes, index =>
-                        <ListGroupItem
-                            key={index.id}
-                            onClick={() => onSelect({ id: index.id, refId: index.reference.id })}
-                        >
-                            <Col xs={1}>
-                                <Checkbox checked={!!find(selected, ["id", index.id])} />
-                            </Col>
-                            <Col xs={8}>
-                                <strong>{index.reference.name}</strong>
-                            </Col>
-                            <Col xs={3}>
-                                Index Version <Label>{index.version}</Label>
-                            </Col>
-                        </ListGroupItem>
-                    )}
-                </ListGroup>
-            ) : <NoneFound noun="source references" />
-        }
-    </div>
-);
+const IndexSelect = ({ indexes, onSelect, selected, error }) => {
+
+    const errorStyle = error.length ? {border: "1px solid #d44b40"} : {border: "1px solid transparent"};
+
+    return (
+        <div style={{marginBottom: "16px"}}>
+            <label className="control-label">References</label>
+            {indexes.length
+                ? (
+                    <div>
+                        <ListGroup style={{maxHeight: "165px", overflowY: "auto", marginBottom: "3px", ...errorStyle}}>
+                            {map(indexes, index =>
+                                <ListGroupItem
+                                    key={index.id}
+                                    onClick={() => onSelect({ id: index.id, refId: index.reference.id })}
+                                >
+                                    <Col xs={1}>
+                                        <Checkbox checked={!!find(selected, ["id", index.id])} />
+                                    </Col>
+                                    <Col xs={8}>
+                                        <strong>{index.reference.name}</strong>
+                                    </Col>
+                                    <Col xs={3}>
+                                        Index Version <Label>{index.version}</Label>
+                                    </Col>
+                                </ListGroupItem>
+                            )}
+                        </ListGroup>
+                        <Col xs={12}>
+                            <div className="input-form-error">
+                                <span className="input-error-message">
+                                    {error}
+                                </span>
+                            </div>
+                        </Col>
+                    </div>
+                ) : <NoneFound noun="source references" />
+            }
+        </div>
+    );
+};
 
 const getInitialState = () => ({
     algorithm: "pathoscope_bowtie",
-    selected: []
+    selected: [],
+    errorRef: ""
 });
 
 export default class CreateAnalysis extends React.Component {
@@ -64,11 +79,16 @@ export default class CreateAnalysis extends React.Component {
             newSelected = [...newSelected, {...newEntry}];
         }
 
-        this.setState({selected: newSelected});
+        this.setState({ selected: newSelected, errorRef: "" });
     };
 
     handleSubmit = (e) => {
         e.preventDefault();
+
+        if (!this.state.selected.length) {
+            return this.setState({ errorRef: "Please select reference(s)" });
+        }
+
         this.props.onSubmit(this.props.id, this.state.selected, this.state.algorithm);
         this.props.onHide();
     };
@@ -98,6 +118,7 @@ export default class CreateAnalysis extends React.Component {
                             indexes={this.props.refIndexes}
                             onSelect={this.handleSelect}
                             selected={this.state.selected}
+                            error={this.state.errorRef}
                         />
                     </Modal.Body>
                     <Modal.Footer>
@@ -106,6 +127,7 @@ export default class CreateAnalysis extends React.Component {
                             type="submit"
                             bsStyle="primary"
                             icon="play"
+                            disabled={!!this.state.errorRef}
                         >
                             Start
                         </Button>
