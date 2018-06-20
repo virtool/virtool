a2a9b4fbf7a8ab0b7ccd2f51a0eb836f179a782b
naomi <mimiijima@gmail.com>
2018-06-20 12:48:55 -0700

    Add ScrollList base component tests

---
+++ client/src/js/base/ScrollList.test.js
@@ -0,0 +1,24 @@
+import { ScrollList } from "./ScrollList";
+
+decsribe("<ScrollList />", () => {
+
+    it("renders correctly", () => {
+
+    });
+
+    it("adds event listener on componentDidMount", () => {
+
+    });
+
+    it("removes event listener on componentWillUnmount", () => {
+
+    });
+
+    it("onScroll()", () => {
+
+    });
+
+    it("loadMore()", () => {
+        
+    });
+});
