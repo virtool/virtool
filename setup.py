8e3abcd310b7e932d769f05fa0a7135cc1a53b76
igboyes <igboyes@gmail.com>
2018-06-29 10:40:57 -0700

    Include missing numpy modules in build

--- setup.py
+++ setup.py
@@ -3,12 +3,15 @@ from cx_Freeze import setup, Executable
 # Dependencies are automatically detected, but it might need
 # fine tuning.
 build_exe_options = {
-    "excludes": [
-        "numpy"
-    ],
     "bin_includes": [
         "libcrypto.so.1.0.0",
         "libssl.so.1.0.0"
+    ],
+    "includes": [
+        "numpy",
+        "numpy.core._methods",
+        "numpy.lib",
+        "numpy.lib.format"
     ],
     "packages": [
         "_cffi_backend",
