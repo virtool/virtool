5a16e5b0bd6bf35ab7999416d22566296b5ec09a
naomi <mimiijima@gmail.com>
2018-07-09 12:14:13 -0700

    Remove comments in test

--- client/src/js/analyses/analyses-actions.test.js
+++ client/src/js/analyses/analyses-actions.test.js
@@ -1,16 +1,6 @@
 import {
-    //wsUpdateSample,
-    //wsRemoveSample,
     wsUpdateAnalysis,
     wsRemoveAnalysis,
-    //findReadyHosts,
-    //getSample,
-    //createSample,
-    //editSample,
-    //updateSampleRights,
-    //removeSample,
-    //showRemoveSample,
-    //hideSampleModal,
     findAnalyses,
     getAnalysis,
     getAnalysisProgress,
@@ -19,50 +9,18 @@ import {
     removeAnalysis
 } from "./actions";
 import {
-    //WS_UPDATE_SAMPLE,
-    //WS_REMOVE_SAMPLE,
     WS_UPDATE_ANALYSIS,
     WS_REMOVE_ANALYSIS,
-    //FIND_READY_HOSTS,
-    //GET_SAMPLE,
-    //CREATE_SAMPLE,
-    //UPDATE_SAMPLE,
-    //UPDATE_SAMPLE_RIGHTS,
-    //REMOVE_SAMPLE,
     FIND_ANALYSES,
     GET_ANALYSIS,
     ANALYZE,
     BLAST_NUVS,
     REMOVE_ANALYSIS,
-    //SHOW_REMOVE_SAMPLE,
-    //HIDE_SAMPLE_MODAL,
     GET_ANALYSIS_PROGRESS
 } from "../actionTypes";
 
 describe("Analyses Action Creators:", () => {
-/*
-    it("wsUpdateSample: returns action for sample update via websocket", () => {
-        const update = {};
-        const result = wsUpdateSample(update);
-        const expected = {
-            type: "WS_UPDATE_SAMPLE",
-            update
-        };
 
-        expect(result).toEqual(expected);
-    });
-
-    it("wsRemoveSample: returns action for sample removal via websocket", () => {
-        const removed = "";
-        const result = wsRemoveSample(removed);
-        const expected = {
-            type: "WS_REMOVE_SAMPLE",
-            removed
-        };
-
-        expect(result).toEqual(expected);
-    });
-*/
     it("wsUpdateAnalysis: returns action for analysis update via websocket", () => {
         const update = {};
         const result = wsUpdateAnalysis(update);
@@ -84,105 +42,7 @@ describe("Analyses Action Creators:", () => {
 
         expect(result).toEqual(expected);
     });
-/*
-    it("findReadyHosts: returns simple action", () => {
-        const result = findReadyHosts();
-        const expected = {
-            type: "FIND_READY_HOSTS_REQUESTED"
-        };
 
-        expect(result).toEqual(expected);
-    });
-
-    it("getSample: returns action for getting specific sample", () => {
-        const sampleId = "testsample";
-        const result = getSample(sampleId);
-        const expected = {
-            type: "GET_SAMPLE_REQUESTED",
-            sampleId
-        };
-
-        expect(result).toEqual(expected);
-    });
-
-    it("createSample: returns action for creating sample", () => {
-        const name = "name";
-        const isolate = "isolate";
-        const host = "host";
-        const locale = "locale";
-        const srna = false;
-        const subtraction = "subtraction";
-        const files = {};
-        const result = createSample(name, isolate, host, locale, srna, subtraction, files);
-        const expected = {
-            type: "CREATE_SAMPLE_REQUESTED",
-            name,
-            isolate,
-            host,
-            locale,
-            srna,
-            subtraction,
-            files
-        };
-
-        expect(result).toEqual(expected);
-    });
-
-    it("editSample: returns action for modifying a sample", () => {
-        const sampleId = "testid";
-        const update = {};
-        const result = editSample(sampleId, update);
-        const expected = {
-            type: "UPDATE_SAMPLE_REQUESTED",
-            sampleId,
-            update
-        };
-
-        expect(result).toEqual(expected);
-    });
-
-    it("updateSampleRights: returns action for updating a sample's permissions", () => {
-        const sampleId = "testid";
-        const update = {};
-        const result = updateSampleRights(sampleId, update);
-        const expected = {
-            type: "UPDATE_SAMPLE_RIGHTS_REQUESTED",
-            sampleId,
-            update
-        };
-
-        expect(result).toEqual(expected);
-    });
-
-    it("removeSample: returns action to remove a sample", () => {
-        const sampleId = "testid";
-        const result = removeSample(sampleId);
-        const expected = {
-            type: "REMOVE_SAMPLE_REQUESTED",
-            sampleId
-        };
-
-        expect(result).toEqual(expected);
-    });
-
-    it("showRemoveSample: returns simple action", () => {
-        const result = showRemoveSample();
-        const expected = {
-            type: "SHOW_REMOVE_SAMPLE"
-        };
-
-        expect(result).toEqual(expected);
-    });
-
-    it("hideSampleModal: returns simple action", () => {
-        const result = hideSampleModal();
-        const expected = {
-            type: "HIDE_SAMPLE_MODAL"
-        };
-
-        expect(result).toEqual(expected);
-    });
-*/
     it("findAnalyses: returns action to find analyses for a sample", () => {
         const sampleId = "testid";
         const result = findAnalyses(sampleId);
