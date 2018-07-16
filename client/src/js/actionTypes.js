fc7048dc7d8b6d2ff880866996a75102391479e0
naomi <mimiijima@gmail.com>
2018-07-16 15:48:10 -0700

    Add websocket subtraction actions

--- client/src/js/actionTypes.js
+++ client/src/js/actionTypes.js
@@ -141,9 +141,10 @@ export const GET_INDEX_HISTORY = createRequestActionType("GET_INDEX_HISTORY");
 export const LIST_READY_INDEXES = createRequestActionType("LIST_READY_INDEXES");
 
 // Subtraction
+export const WS_INSERT_SUBTRACTION = "WS_INSERT_SUBTRACTION";
 export const WS_UPDATE_SUBTRACTION = "WS_UPDATE_SUBTRACTION";
-
-export const FIND_SUBTRACTIONS = createRequestActionType("FIND_SUBTRACTIONS");
+export const WS_REMOVE_SUBTRACTION = "WS_REMOVE_SUBTRACTION";
+export const LIST_SUBTRACTIONS = createRequestActionType("LIST_SUBTRACTIONS");
 export const LIST_SUBTRACTION_IDS = createRequestActionType("LIST_SUBTRACTION_IDS");
 export const GET_SUBTRACTION = createRequestActionType("GET_SUBTRACTION");
 export const CREATE_SUBTRACTION = createRequestActionType("CREATE_SUBTRACTION");
