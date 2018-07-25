af6fc680f0b35d7f92d8ad903d16bfa61987adea
naomi <mimiijima@gmail.com>
2018-07-25 16:17:25 -0700

    Update hmms actions and reducer tests

--- client/src/js/hmm/hmm-actions.test.js
+++ client/src/js/hmm/hmm-actions.test.js
@@ -1,34 +1,65 @@
-import { fetchHmms, getHmm, installHMMs } from "./actions";
-import { GET_HMM, INSTALL_HMMS, FETCH_HMMS } from "../actionTypes";
+import {
+    filterHmms,
+    listHmms,
+    getHmm,
+    installHMMs,
+    purgeHMMs
+} from "./actions";
+import {
+    GET_HMM,
+    INSTALL_HMMS,
+    FILTER_HMMS,
+    LIST_HMMS,
+    PURGE_HMMS
+} from "../actionTypes";
 
 describe("HMM Action Creators:", () => {
+    let result;
+    let expected;
 
-    it("fetchHmms: returns simple action", () => {
-        const result = fetchHmms();
-        const expected = {
-            type: FETCH_HMMS
+    it("filterHmms: returns action for filtering results by search term", () => {
+        const term = "search";
+        result = filterHmms(term);
+        expected = {
+            type: FILTER_HMMS.REQUESTED,
+            term
         };
+        expect(result).toEqual(expected);
+    });
 
+    it("listHmms: returns action to fetch specific page of hmm documents", () => {
+        const page = 1;
+        result = listHmms(page);
+        expected = {
+            type: LIST_HMMS.REQUESTED,
+            page
+        };
         expect(result).toEqual(expected);
     });
 
     it("getHmm: returns action for getting specific hmm document", () => {
         const hmmId = "tester";
-        const result = getHmm(hmmId);
-        const expected = {
+        result = getHmm(hmmId);
+        expected = {
             type: GET_HMM.REQUESTED,
             hmmId
         };
-
         expect(result).toEqual(expected);
     });
 
     it("installHMMs: returns action for installing HMMs", () => {
-        const result = installHMMs();
-        const expected = {
-            type: INSTALL_HMMS.REQUESTED
+        const releaseId = "123abc";
+        result = installHMMs(releaseId);
+        expected = {
+            type: INSTALL_HMMS.REQUESTED,
+            release_id: releaseId
         };
+        expect(result).toEqual(expected);
+    });
 
+    it("purgeHMMs: returns simple action to delete HMM data", () => {
+        result = purgeHMMs();
+        expected = { type: PURGE_HMMS.REQUESTED };
         expect(result).toEqual(expected);
     });
 
