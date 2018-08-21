f00638f2cc683ebad8d443ead894c9b9045e4506
naomi <mimiijima@gmail.com>
2018-08-21 10:24:56 -0700

    Update Account component and tests

--- client/src/js/account/components/Account.test.js
+++ client/src/js/account/components/Account.test.js
@@ -3,11 +3,7 @@ import Account from "./Account";
 describe("<Account />", () => {
 
     it("renders correctly", () => {
-        const initialState = {
-            account: {}
-        };
-        const store = mockStore(initialState);
-
+        const store = mockStore({});
         const wrapper = shallow(<Account store={store} />).dive();
 
         expect(wrapper).toMatchSnapshot();
