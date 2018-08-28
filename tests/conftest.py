f200d0b3da7d59baa51846b7e1f14459d680c6cd
igboyes <igboyes@gmail.com>
2018-08-28 16:15:28 -0700

    Update pytest config

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,6 @@ from tests.fixtures.groups import *
 from tests.fixtures.history import *
 from tests.fixtures.indexes import *
 from tests.fixtures.jobs import *
-from tests.fixtures.nv import *
 from tests.fixtures.references import *
 from tests.fixtures.response import *
 from tests.fixtures.users import *
