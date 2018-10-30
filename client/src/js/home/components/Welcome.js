f24fd128b91ba1e1c539845e7bfaf63dfceeeedd
igboyes <igboyes@gmail.com>
2018-10-30 11:52:05 -0700

    Update home code formatting

--- client/src/js/home/components/Welcome.js
+++ client/src/js/home/components/Welcome.js
@@ -5,37 +5,37 @@ import { Panel } from "react-bootstrap";
 import { Icon } from "../../base";
 
 const Welcome = props => {
-  let version;
+    let version;
 
-  if (props.version) {
-    version = <small className="text-muted">{props.version}</small>;
-  }
-  return (
-    <div className="container">
-      <Panel>
-        <Panel.Body>
-          <h3>Virtool {version}</h3>
-          <p>Viral infection diagnostics using next-generation sequencing</p>
+    if (props.version) {
+        version = <small className="text-muted">{props.version}</small>;
+    }
+    return (
+        <div className="container">
+            <Panel>
+                <Panel.Body>
+                    <h3>Virtool {version}</h3>
+                    <p>Viral infection diagnostics using next-generation sequencing</p>
 
-          <a
-            className="btn btn-default"
-            href="http://www.virtool.ca/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Icon name="globe" /> Website
-          </a>
-        </Panel.Body>
-      </Panel>
-    </div>
-  );
+                    <a
+                        className="btn btn-default"
+                        href="http://www.virtool.ca/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <Icon name="globe" /> Website
+                    </a>
+                </Panel.Body>
+            </Panel>
+        </div>
+    );
 };
 
 const mapStateToProps = state => ({
-  version: get(state.updates, "version")
+    version: get(state.updates, "version")
 });
 
 export default connect(
-  mapStateToProps,
-  null
+    mapStateToProps,
+    null
 )(Welcome);
