9d9eb2dc9d64e35bcf82eaa4244e8c4c5c1b5887
igboyes <igboyes@gmail.com>
2018-12-10 14:44:51 -0800

    Use db_host config in db fixture

--- tests/fixtures/db.py
+++ tests/fixtures/db.py
@@ -18,16 +18,16 @@ def test_db_name(worker_id):
 
 
 @pytest.fixture
-def test_motor(test_db_name, loop):
-    client = motor.motor_asyncio.AsyncIOMotorClient(io_loop=loop)
+def test_motor(test_db_name, loop, request):
+    client = motor.motor_asyncio.AsyncIOMotorClient(host=request.config.getoption("db_host"), io_loop=loop)
     loop.run_until_complete(client.drop_database(test_db_name))
     yield client[test_db_name]
     loop.run_until_complete(client.drop_database(test_db_name))
 
 
 @pytest.fixture
-def dbs(test_db_name):
-    client = pymongo.MongoClient()
+def dbs(test_db_name, request):
+    client = pymongo.MongoClient(host=request.config.getoption("db_host"))
     client.drop_database(test_db_name)
     yield client[test_db_name]
     client.drop_database(test_db_name)
