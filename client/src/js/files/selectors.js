01b6e83d318fd56cdc099f50a36c1252dd645f45
igboyes <igboyes@gmail.com>
2019-01-10 12:34:48 -0800

    Don't render unready or reserved files in FileManager

--- client/src/js/files/selectors.js
+++ client/src/js/files/selectors.js
@@ -1,6 +1,8 @@
 import { createSelector } from "reselect";
-import { map } from "lodash-es";
+import { filter, map } from "lodash-es";
 
 const getFiles = state => state.files.documents;
 
-export const filesSelector = createSelector(getFiles, list => map(list, "id"));
+export const filesSelector = createSelector(getFiles, list =>
+    map(filter(list, document => document.ready && !document.reserved), "id")
+);
