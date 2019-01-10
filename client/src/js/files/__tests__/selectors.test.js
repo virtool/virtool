4e19bcabc4be71850bcd3fb868d72fb1d091d37e
igboyes <igboyes@gmail.com>
2019-01-10 12:43:49 -0800

    Update file selector tests

--- client/src/js/files/__tests__/selectors.test.js
+++ client/src/js/files/__tests__/selectors.test.js
@@ -1,16 +1,31 @@
 import { filesSelector } from "../selectors";
 
 describe("Test Files Selectors", () => {
-    const state = {
-        files: { documents: [{ id: "foo" }, { id: "fud" }] }
-    };
-
-    let result;
+    let state;
     let expected;
 
-    it("filesSelector: returns data used to render a list of files", () => {
-        result = filesSelector(state);
-        expected = ["foo", "fud"];
-        expect(result).toEqual(expected);
+    beforeEach(() => {
+        state = {
+            files: {
+                documents: [{ id: "foo", ready: true, reserved: false }, { id: "fud", ready: true, reserved: false }]
+            }
+        };
+    });
+
+    it("returns all document ids when appropriate", () => {
+        const result = filesSelector(state);
+        expect(result).toEqual(["foo", "fud"]);
+    });
+
+    it("excludes ids unready documents", () => {
+        state.files.documents[0].ready = false;
+        const result = filesSelector(state);
+        expect(result).toEqual(["fud"]);
+    });
+
+    it("excludes ids reserved documents", () => {
+        state.files.documents[1].reserved = true;
+        const result = filesSelector(state);
+        expect(result).toEqual(["foo"]);
     });
 });
