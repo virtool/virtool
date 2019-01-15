3ba9ef2039d101684abe689fd2dc8253e3739d92
igboyes <igboyes@gmail.com>
2019-01-15 10:42:45 -0800

    Add virtool.db.settings.get()

--- virtool/db/settings.py
+++ virtool/db/settings.py
@@ -6,10 +6,17 @@ PROJECTION = {
     "_id": False
 }
 
+
+async def get(db):
+    settings = await db.settings.find_one("settings", projection=PROJECTION)
+
+    if settings:
+        return settings
+
+    return dict()
+
+
 async def update(db, updates):
     return await db.settings.find_one_and_update({"_id": "settings"}, {
         "$set": updates
     })
-
-
-
