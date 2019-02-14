d86b9068a9977bb48173bb71d7452928cce5d440
igboyes <igboyes@gmail.com>
2019-02-14 12:56:12 -0800

    Fix problems in groups modal component

---
+++ client/src/js/groups/components/Group.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { connect } from "react-redux";
+import { ListGroupItem } from "../../base";
+import { changeActiveGroup } from "../actions";
+
+const Group = ({ id, active, onSelect }) => (
+    <ListGroupItem key={id} active={active} onClick={onSelect}>
+        <span className="text-capitalize">{id}</span>
+    </ListGroupItem>
+);
+
+export const mapStateToProps = (state, ownProps) => ({
+    active: state.groups.activeId === ownProps.id
+});
+
+export const mapDispatchToProps = (dispatch, ownProps) => ({
+    onSelect: () => {
+        dispatch(changeActiveGroup(ownProps.id));
+    }
+});
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Group);
