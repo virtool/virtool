bdc410029e7ab5f1faf6233f763bff8b905d4138
igboyes <igboyes@gmail.com>
2019-02-19 12:22:50 -0800

    Add Redux router pushState helper code

--- client/src/js/app/sagas.js
+++ client/src/js/app/sagas.js
@@ -4,9 +4,10 @@
  * @module sagas
  */
 
+import { push } from "connected-react-router";
 import { watchAccount } from "../account/sagas";
 import { watchAnalyses } from "../analyses/sagas";
-import { watchFiles } from "../files/sagas";
+import { findFiles, removeFile, upload, watchFiles } from "../files/sagas";
 import { watchGroups } from "../groups/sagas";
 import { watchHmms } from "../hmm/sagas";
 import { watchIndexes } from "../indexes/sagas";
@@ -19,7 +20,19 @@ import { watchSubtraction } from "../subtraction/sagas";
 import { watchUpdates } from "../updates/sagas";
 import { watchUsers } from "../users/sagas";
 import { watchReferences } from "../references/sagas";
-import { all } from "redux-saga/effects";
+import { FIND_FILES, PUSH_STATE, REMOVE_FILE, UPLOAD } from "./actionTypes";
+import { all, put, select, takeEvery, takeLatest } from "redux-saga/effects";
+
+const getLocation = state => state.router.location;
+
+function* pushState(action) {
+    const routerLocation = yield select(getLocation);
+    yield put(push({ ...routerLocation, state: action.state }));
+}
+
+export function* watchRouter() {
+    yield takeLatest(PUSH_STATE, pushState);
+}
 
 /**
  * Yields all of the sagas in the application. Intended for use with the ``react-saga`` middleware.
@@ -37,6 +50,7 @@ function* rootSaga() {
         watchJobs(),
         watchOTUs(),
         watchProcesses(),
+        watchRouter(),
         watchSamples(),
         watchSettings(),
         watchGroups(),
