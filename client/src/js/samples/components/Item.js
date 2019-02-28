204632e9b1252f61dc9fe3da6e8ce74488bd55c2
igboyes <igboyes@gmail.com>
2019-02-28 12:17:30 -0800

    Fix SampleEntryLabel width variation

--- client/src/js/samples/components/Item.js
+++ client/src/js/samples/components/Item.js
@@ -8,18 +8,18 @@ import { Row, Col } from "react-bootstrap";
 import { Icon, Flex, FlexItem, ListGroupItem, RelativeTime, Checkbox, Loader } from "../../base";
 import { selectSample } from "../actions";
 
-const SampleEntryLabel = ({ icon, label, ready }) => (
+export const SampleEntryLabel = ({ icon, label, ready }) => (
     <Flex>
         <FlexItem className={CX("sample-label", { "bg-primary": ready })}>
             <Flex alignItems="center">
-                {ready === "ip" ? (
-                    <Loader size="10px" color="white" verticalAlign="middle" />
-                ) : (
-                    <Icon name={icon} style={{ lineHeight: "inherit" }} />
-                )}
-                <span style={{ paddingLeft: "3px" }} className="hidden-xs hidden-sm">
-                    {label}
+                <span style={{ width: "12px" }}>
+                    {ready === "ip" ? (
+                        <Loader size="10px" color="white" verticalAlign="middle" />
+                    ) : (
+                        <Icon name={icon} style={{ lineHeight: "inherit" }} fixedWidth />
+                    )}
                 </span>
+                <span className="sample-label-text">{label}</span>
             </Flex>
         </FlexItem>
     </Flex>
