5d84db5f8b7b22ef705fb5b3652d60e21da0025d
igboyes <igboyes@gmail.com>
2019-03-25 11:29:39 -0700

    Upgrade react-dropzone and usages
    Update UploadBar to work with latest react-dropzone

--- client/src/js/base/__tests__/UploadBar.test.js
+++ client/src/js/base/__tests__/UploadBar.test.js
@@ -1,38 +1,13 @@
-import Dropzone from "react-dropzone";
 import { UploadBar } from "../UploadBar";
-import { Button } from "../Button";
 
 describe("<UploadBar />", () => {
-    let wrapper;
-
-    it("renders correctly", () => {
-        wrapper = shallow(<UploadBar />);
-
+    it("should render", () => {
+        const wrapper = shallow(<UploadBar />);
         expect(wrapper).toMatchSnapshot();
     });
 
-    it("renders optional message instead", () => {
-        wrapper = shallow(<UploadBar message="tester" />);
-
-        expect(
-            wrapper
-                .find(Dropzone)
-                .children()
-                .text()
-        ).toEqual("tester");
+    it("should render optional message", () => {
+        const wrapper = shallow(<UploadBar message="Foobar" />);
         expect(wrapper).toMatchSnapshot();
-    });
-
-    it("calls Dropzone.open() on button click", () => {
-        const spy = sinon.spy(Dropzone.prototype, "open");
-        wrapper = mount(<UploadBar />);
-
-        expect(spy.called).toBe(false);
-
-        wrapper.find(Button).simulate("click");
-
-        expect(spy.calledOnce).toBe(true);
-
-        spy.restore();
     });
 });
