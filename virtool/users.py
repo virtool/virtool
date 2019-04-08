eab0b9c02fd2e56ca63d4393ca611c4855257d14
igboyes <igboyes@gmail.com>
2019-04-08 14:41:46 -0700

    Add some tests for virtool.users

--- virtool/users.py
+++ virtool/users.py
@@ -15,11 +15,18 @@ PERMISSIONS = [
 ]
 
 
-def calculate_identicon(user_id):
+def calculate_identicon(user_id: str) -> str:
+    """
+    Calculate an identicon hash string based on a user name.
+
+    :param user_id: the user name
+    :return: an identicon string
+
+    """
     return hashlib.sha256(user_id.encode()).hexdigest()
 
 
-def check_api_key(key, hashed):
+def check_api_key(key: str, hashed: str) -> bool:
     """
     Check a API key string against a hashed one from the user database.
 
@@ -33,85 +40,62 @@ def check_api_key(key, hashed):
     return hash_api_key(key) == hashed
 
 
-def check_legacy_password(password, salt, hashed):
+def check_legacy_password(password: str, salt: str, hashed: str) -> bool:
     """
     Check if a unicode ``password`` and ``salt`` match a ``hashed`` password from the database. This is for use only
     with legacy SHA512 hashed passwords. New password hash with :func:`.hash_password` will be hashed using bcrypt.
 
     :param password: the password to check
-    :type password: str
-
     :param salt: a salt
-    :type salt: str
-
     :param hashed: the hashed password from the database
-    :type hashed: str
-
     :return: success of test
-    :rtype: bool
 
     """
     return hashed == hashlib.sha512(salt.encode("utf-8") + password.encode("utf-8")).hexdigest()
 
 
-def check_password(password, hashed):
+def check_password(password: str, hashed: bytes) -> bool:
     """
     Check if a unicode ``password`` matches a ``hashed_password``.
 
     :param password: the password to check.
-    :type password: str
-
     :param hashed: the salted and hashed password from the database
-    :type hashed: str
-
     :return: success of test
-    :rtype: bool
 
     """
     return bcrypt.checkpw(password.encode(), hashed)
 
 
-def hash_api_key(key):
+def hash_api_key(key: str) -> str:
     """
     Hash an API key using SHA256.
 
     :param key: the API key to hash
-    :type key: str
-
     :return: the hashed key
-    :rtype: str
 
     """
     return hashlib.sha256(key.encode("utf-8")).hexdigest()
 
 
-def hash_password(password):
+def hash_password(password: str) -> str:
     """
     Salt and hash a unicode password. Uses bcrypt.
 
     :param password: a password string to salt and hash
-    :type password: str
-
     :return: a salt and hashed password
-    :rtype: str
 
     """
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt(12))
 
 
-def limit_permissions(permissions, limit_filter):
+def limit_permissions(permissions: dict, limit_filter: dict) -> dict:
     """
-    Make sure permission values in `permissions` do not exceed those in `limit_filter`. Returns a filtered set
+    Make sure permission values in `permissions` do not exceed those in `limit_filter`. Returns a filtered set of
     permissions.
 
     :param limit_filter: the limiting permissions that cannot be exceeded
-    :type limit_filter:
-
     :param permissions: a permissions to filter
-    :type permissions: dict
-
     :return: filtered permissions
-    :rtype: dict
 
     """
     return {p: (permissions.get(p, False) and limit_filter[p]) for p in permissions}
