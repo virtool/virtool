df049fd9ff2bf8b817bab37fab199fb7fd3654d9
igboyes <igboyes@gmail.com>
2019-04-18 13:27:29 -0700

    Fix server tests

--- tests/jobs/test_update_sample.py
+++ tests/jobs/test_update_sample.py
@@ -1,6 +1,6 @@
 import pytest
 
-import virtool.jobs.create_sample
+import virtool.jobs.update_sample
 
 
 @pytest.fixture
@@ -16,7 +16,7 @@ def test_update_sample_job(mocker, tmpdir, loop, request, dbi, dbs, test_db_conn
 
     q = mocker.Mock()
 
-    job = virtool.jobs.create_sample.Job(
+    job = virtool.jobs.update_sample.Job(
         test_db_connection_string,
         test_db_name,
         settings,
@@ -73,15 +73,73 @@ def test_check_db(mocker, test_update_sample_job):
     assert test_update_sample_job.params == expected
 
 
-def test_copy_files(mocker, test_update_sample_job):
+@pytest.mark.parametrize("paired", [True, False])
+def test_copy_files(paired, mocker, test_update_sample_job):
+    files = [
+        {
+            "replacement": {
+                "id": "foo_replacement_1.fq.gz"
+            }
+        }
+    ]
+
+    if paired:
+        files.append({
+            "replacement": {
+                "id": "foo_replacement_2.fq.gz"
+            }
+        })
+
+    test_update_sample_job.params = {
+        "sample_id": "baz",
+        "sample_path": "/samples/baz",
+        "paired": paired,
+        "files": files
+    }
+
+    test_update_sample_job.settings["data_path"] = "/data"
+
+    sizes = [1234]
+
+    if paired:
+        sizes.append(1235)
+
+    m_copy_files_to_sample = mocker.patch("virtool.jobs.utils.copy_files_to_sample", return_value=sizes)
+
     test_update_sample_job.copy_files()
 
-    m_copy_or_compress = mocker.patch("virtool.jobs.utils.copy_or_compress")
+    expected_paths = ["/data/files/foo_replacement_1.fq.gz"]
 
-    m_file_stats = mocker.patch("virtool.utils.file_stats", return_value={
-        "size": 12345
+    if paired:
+        expected_paths.append("/data/files/foo_replacement_2.fq.gz")
+
+    m_copy_files_to_sample.assert_called_with(
+        expected_paths,
+        "/samples/baz",
+        test_update_sample_job.proc
+    )
+
+    expected_raw = [{
+        "download_url": "/download/samples/baz/reads_1.fq.gz",
+        "name": "reads_1.fq.gz",
+        "size": 1234,
+        "from": {
+            "replacement": {
+                "id": "foo_replacement_1.fq.gz"
+            }
+        }
+    }]
+
+    if paired:
+        expected_raw.append({
+        "download_url": "/download/samples/baz/reads_2.fq.gz",
+        "name": "reads_2.fq.gz",
+        "size": 1235,
+        "from": {
+            "replacement": {
+                "id": "foo_replacement_2.fq.gz"
+            }
+        }
     })
 
-
-
-
+    assert test_update_sample_job.intermediate["raw"] == expected_raw
