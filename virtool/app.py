8a56a132dbb4ee951b129ade9c2d1a1a9ca69748
igboyes <igboyes@gmail.com>
2019-05-24 15:19:22 -0700

    Remove pprint usage

--- virtool/app.py
+++ virtool/app.py
@@ -381,9 +381,6 @@ def create_app(force_settings=None):
 
     do_setup = virtool.config.should_do_setup(config)
 
-    import pprint
-    pprint.pprint(config)
-
     if not do_setup:
         # Don't use authentication in setup mode.
         middlewares.append(virtool.http.auth.middleware)
