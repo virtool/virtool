cae23b59c5a0c69180d675f097897dde7bb211a0
igboyes <igboyes@gmail.com>
2019-06-03 15:13:38 -0700

    Update base Box component module

--- client/src/js/base/Box.js
+++ client/src/js/base/Box.js
@@ -1,25 +1,49 @@
 import styled from "styled-components";
 
 export const Box = styled.div`
-    border: 1px #dddddd solid;
-    border-radius: 2px;
+    border: 1px ${props => props.theme.color.greyLight} solid;
+    border-radius: 0;
     box-sizing: border-box;
     margin-bottom: 10px;
     padding: 10px 15px;
     cursor: ${props => (props.onClick ? "pointer" : "auto")};
 `;
 
-export const BoxSection = styled.div`
-    margin: -10px -15px;
+export const BoxGroup = styled(Box)`
+    padding: 0;
+`;
+
+export const BoxGroupSection = styled.div`
     padding: 10px 15px;
+
     &:not(:last-child) {
         border-bottom: 1px #dddddd solid;
     }
 `;
 
-export const BoxHeader = styled(BoxSection)`
-    padding: 10px 15px 20px 15px;
+export const BoxGroupHeader = styled(BoxGroupSection)`
+    align-items: stretch;
+    display: flex;
+    flex-direction: column;
+    padding: 10px 15px;
+
+    h2 {
+        align-items: center;
+        display: flex;
+        font-size: ${props => props.theme.fontSize.sm};
+        justify-content: space-between;
+        margin: 0;
+    }
+
+    p {
+        color: ${props => props.theme.color.greyDark};
+        margin: 5px 0 0;
+        font-size: ${props => props.theme.fontSize.sm};
+    }
 `;
+
+BoxGroup.Header = BoxGroupHeader;
+BoxGroup.Section = BoxGroupSection;
 
 export const BoxTitle = styled.h1`
     font-size: 14px;
