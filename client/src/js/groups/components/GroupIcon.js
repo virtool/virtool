b6e7a368188f3df48f6a4b4597baaafa5a883cb2
igboyes <igboyes@gmail.com>
2019-06-03 15:14:31 -0700

    Add GroupIcon component
    Contains group icon in div that matches appearance of user identicon.

---
+++ client/src/js/groups/components/GroupIcon.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+import { Icon } from "../../base";
+
+export const GroupIcon = styled(Icon).attrs(() => ({ name: "users" }))`
+    align-items: center;
+    background-color: #ebf8ff;
+    border-radius: 12px;
+    box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+    display: flex;
+    height: 24px;
+    justify-content: center;
+    width: 24px;
+`;
+
+export default GroupIcon;
