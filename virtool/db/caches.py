61f1f018d5035e770f36e435b0529d8889719226
Ian Boyes <igboyes@gmail.com>
2019-08-13 11:42:19 -0700

    Clean, comment, test, and refactor virtool.db.caches

--- virtool/db/caches.py
+++ virtool/db/caches.py
@@ -1,3 +1,4 @@
+import aiohttp.web
 import hashlib
 import json
 import os
@@ -17,12 +18,32 @@ PROJECTION = [
 ]
 
 
-def calculate_cache_hash(parameters):
+def calculate_cache_hash(parameters: dict) -> str:
+    """
+    Calculate a hash from the parameters `dict` for a cache. The parameters are arguments passed to a trimming program.
+    Caches can be reused when the hash of the trim parameters for a a new analysis matches an existing cache.
+
+    :param parameters: the trimming parameters
+    :return: the cache hash
+
+    """
     string = json.dumps(parameters, sort_keys=True)
     return hashlib.sha1(string.encode()).hexdigest()
 
 
-def create(db, sample_id, parameters, paired, legacy=False, program="skewer-0.2.2"):
+def create(db, sample_id: str, parameters: dict, paired: bool, legacy: bool = False, program: str = "skewer-0.2.2"):
+    """
+    Create and insert a new cache database document. Return the generated unique cache id.
+
+    :param db: the application database client
+    :param sample_id: the id of the esample the cache is derived from
+    :param parameters: the trim parameters
+    :param paired: boolean indicating if the sample contains paired data
+    :param legacy: boolean indicating if the cache is derived from a trimmed legacy sample
+    :param program: the trimming program used
+    :return: the new cache id
+
+    """
     try:
         cache_id = virtool.utils.random_alphanumeric(length=8)
 
@@ -44,33 +65,31 @@ def create(db, sample_id, parameters, paired, legacy=False, program="skewer-0.2.
         return cache_id
 
     except pymongo.errors.DuplicateKeyError:
-        return create(db, sample_id, parameters, paired, legacy=legacy)
+        # Keep trying to add the cache with new ids if the generated id is not unique.
+        return create(db, sample_id, parameters, paired, legacy, program)
 
 
-async def get(db, cache_id):
+async def get(db, cache_id: str) -> dict:
+    """
+    Get the complete representation for the cache with the given `cache_id`.
+
+    :param db: the application database client
+    :param cache_id: the id of the cache to get
+    :return: the cache document
+
+    """
     document = await db.caches.find_one(cache_id)
     return virtool.utils.base_processor(document)
 
 
-async def prune(app):
+async def remove(app: aiohttp.web.Application, cache_id: str):
     """
-    db = app["db"]
+    Remove the cache database document and files with the given `cache_id`.
 
-    seen_hashes = set()
+    :param app: the application object
+    :param cache_id: the id of the cache to remove
 
-    pipeline = [
-
-    ]
-
-    cursor =
-
-    async for document in db.caches.find({"ready": True}, ["hash"]):
-        if document["hash"] in seen_hashes:
     """
-    pass
-
-
-async def remove(app, cache_id):
     db = app["db"]
     settings = app["settings"]
 
