e297eba3e2aafcf9e89dcbdd4606539ca9ae1847
igboyes <igboyes@gmail.com>
2019-08-29 12:05:08 -0700

    Fix Alert positioning in Modal

--- client/src/js/base/index.js
+++ client/src/js/base/index.js
@@ -32,6 +32,7 @@ export * from "./ExternalLink";
 export * from "./Label";
 export * from "./LoadingPlaceholder";
 export * from "./Logo";
+export * from "./Modal";
 export * from "./Table";
 export * from "./TabLink";
 export * from "./Tabs";
