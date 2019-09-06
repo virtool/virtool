e98beb317ed2eb5154892044ca6c819e27a74302
igboyes <igboyes@gmail.com>
2019-09-06 14:41:14 -0700

    Update ExternalLink
    Turn into a styled component. Remove underline on hover and focus.

--- client/src/js/base/ExternalLink.js
+++ client/src/js/base/ExternalLink.js
@@ -1,7 +1,15 @@
 import React from "react";
+import styled from "styled-components";
 
-export const ExternalLink = ({ children, className, href }) => (
+export const UnstyledExternalLink = ({ children, className, href }) => (
     <a className={className} href={href} rel="noopener noreferrer" target="_blank">
         {children}
     </a>
 );
+
+export const ExternalLink = styled(UnstyledExternalLink)`
+    &:hover,
+    &:focus {
+        text-decoration: none;
+    }
+`;
