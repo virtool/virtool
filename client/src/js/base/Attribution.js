372cf583a472f8e25f2a892c976452b507287e2d
igboyes <igboyes@gmail.com>
2019-09-06 14:41:26 -0700

    Update Attribution
    Fix crash when no identicon provided. Make inline.

--- client/src/js/base/Attribution.js
+++ client/src/js/base/Attribution.js
@@ -12,23 +12,23 @@ export const Portrait = ({ gravatar }) => (
     <StyledPortrait alt="" src={`https://s.gravatar.com/avatar/${gravatar}?s=80`} />
 );
 
-const StyledAttribution = styled.div`
+export const UnstyledAttribution = ({ className, time, user, identicon, verb = "created" }) => {
+    return (
+        <span className={className}>
+            {identicon ? <Identicon size={12} hash={identicon} /> : null}
+            <span>{user}</span>
+            <span>{verb}</span>
+            <RelativeTime time={time} />
+        </span>
+    );
+};
+
+export const Attribution = styled(UnstyledAttribution)`
     align-items: center;
-    display: flex;
+    display: inline-flex;
     font-size: inherit;
 
     *:not(:first-child) {
         margin-left: 3px;
     }
 `;
-
-export const Attribution = ({ time, user, identicon, verb = "created" }) => {
-    return (
-        <StyledAttribution>
-            <Identicon size={12} hash={identicon} />
-            <span>{user}</span>
-            <span>{verb}</span>
-            <RelativeTime time={time} />
-        </StyledAttribution>
-    );
-};
