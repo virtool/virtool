5cc76b8f6977284beea3f684b68f0203fb7284dc
igboyes <igboyes@gmail.com>
2019-09-25 11:45:19 -0700

    Update Python tests

--- tests/analyses/test_db.py
+++ tests/analyses/test_db.py
@@ -5,6 +5,98 @@ import virtool.analyses.db
 import virtool.analyses.format
 
 
+@pytest.fixture
+def test_blast_obj(dbi):
+    settings = {
+        "data_path": "/mnt/data"
+    }
+
+    return virtool.analyses.db.BLAST(
+        dbi,
+        settings,
+        "foo",
+        5,
+        "ABC123"
+    )
+
+
+class TestBLAST:
+
+    def test_init(self, dbi, test_blast_obj):
+        assert test_blast_obj.db == dbi
+        assert test_blast_obj.settings == {"data_path": "/mnt/data"}
+        assert test_blast_obj.analysis_id == "foo"
+        assert test_blast_obj.sequence_index == 5
+        assert test_blast_obj.rid == "ABC123"
+        assert test_blast_obj.error is None
+        assert test_blast_obj.interval == 3
+        assert test_blast_obj.ready is False
+        assert test_blast_obj.result is None
+
+    async def test_remove(self, mocker, dbi, test_blast_obj):
+        """
+        Test that the `remove()` method results in a call to `virtool.analyses.db.remove_nuvs_blast()` using the
+        BLAST object attributes.
+
+        """
+        m_remove_nuvs_blast = mocker.patch("virtool.analyses.db.remove_nuvs_blast", make_mocked_coro())
+        await test_blast_obj.remove()
+        m_remove_nuvs_blast.assert_called_with(dbi, "foo", 5)
+
+    async def test_sleep(self, mocker, dbi, test_blast_obj):
+        """
+        Test that calling the `sleep()` method calls `asyncio.sleep` and increments the `interval` attribute by 5.
+
+        """
+        m_sleep = mocker.patch("asyncio.sleep", make_mocked_coro())
+
+        await test_blast_obj.sleep()
+        m_sleep.assert_called_with(3)
+        assert test_blast_obj.interval == 8
+
+        await test_blast_obj.sleep()
+        m_sleep.assert_called_with(8)
+        assert test_blast_obj.interval == 13
+
+    @pytest.mark.parametrize("check", [True, False])
+    @pytest.mark.parametrize("error", [None, "Error"])
+    @pytest.mark.parametrize("ready", [None, True, False])
+    @pytest.mark.parametrize("result", [None, {"foo": "bar"}])
+    async def test_update(self, check, ready, result, error, mocker, dbi, test_blast_obj, static_time):
+        m_check_rid = mocker.patch("virtool.bio.check_rid", make_mocked_coro(check))
+
+        document = {
+            "_id": "foo",
+            "results": [
+                {"index": 2, "blast": "bar"},
+                {"index": 5, "blast": "baz"},
+                {"index": 12, "blast": "baz"}
+            ]
+        }
+
+        await dbi.analyses.insert_one(document)
+        await test_blast_obj.update(ready, result, error)
+
+        if ready is None:
+            m_check_rid.assert_called_with(test_blast_obj.settings, test_blast_obj.rid)
+        else:
+            assert not m_check_rid.called
+
+        import pprint
+        pprint.pprint(await dbi.analyses.find_one())
+
+        document["results"][1]["blast"] = {
+            "error": error,
+            "interval": 3,
+            "last_checked_at": static_time.datetime,
+            "ready": ready,
+            "result": result,
+            "rid": "ABC123"
+        }
+
+        assert await dbi.analyses.find_one() == document
+
+
 @pytest.mark.parametrize("algorithm", [None, "foobar", "nuvs", "pathoscope"])
 async def test_format_analysis(algorithm, mocker):
     """
@@ -52,3 +144,38 @@ async def test_format_analysis(algorithm, mocker):
     elif algorithm == "pathoscope":
         m_format_pathoscope.assert_called_with("db", "settings", document)
         assert not m_format_nuvs.called
+
+    async def test_remove_nuvs_blast(dbi):
+        """
+        Test that the correct BLAST result is removed.
+
+        """
+        documents = [
+            {
+                "_id": "foo",
+                "results": [
+                    {"index": 2, "blast": "bar"},
+                    {"index": 5, "blast": "baz"},
+                ]
+            },
+            {
+
+                "_id": "bar",
+                "results": [
+                    {"index": 3, "blast": "bar"},
+                    {"index": 9, "blast": "baz"},
+                ]
+            }
+        ]
+
+        await dbi.analyses.insert_many(documents)
+
+        await virtool.analyses.db.remove_nuvs_blast(
+            dbi,
+            "foo",
+            5
+        )
+
+        documents[0]["results"][1]["blast"] = None
+
+        assert await dbi.analyses.find().to_list(None) == documents
