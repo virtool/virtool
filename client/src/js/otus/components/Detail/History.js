5f7c8ac81aece10f42684c606ad39f9f3934df86
SEBASTIANCHEN <734637883@QQ.COM>
2019-10-03 12:43:38 -0700

    Fix ESlint errors

--- client/src/js/otus/components/Detail/History.js
+++ client/src/js/otus/components/Detail/History.js
@@ -7,84 +7,14 @@
  *
  */
 import React from "react";
-import styled from "styled-components";
 import PropTypes from "prop-types";
 import { get, groupBy, map, reverse, sortBy } from "lodash-es";
 import { connect } from "react-redux";
+import { ListGroup } from "react-bootstrap";
+import { getOTUHistory, revert } from "../../actions";
+import { LoadingPlaceholder } from "../../../base";
 import { checkRefRight } from "../../../utils/utils";
 import { Change } from "./Change.js";
-import { ListGroup } from "react-bootstrap";
-
-import { getOTUHistory, revert } from "../../actions";
-import { Flex, FlexItem, ListGroupItem, RelativeTime, Icon, Label, LoadingPlaceholder } from "../../../base";
-
-const methodIconProps = {
-    add_isolate: {
-        name: "flask",
-        bsStyle: "primary"
-    },
-    create: {
-        name: "plus-square",
-        bsStyle: "primary"
-    },
-    create_sequence: {
-        name: "dna",
-        bsStyle: "primary"
-    },
-    edit: {
-        name: "pencil-alt",
-        bsStyle: "warning"
-    },
-    edit_isolate: {
-        name: "flask",
-        bsStyle: "warning"
-    },
-    edit_sequence: {
-        name: "dna",
-        bsStyle: "warning"
-    },
-    clone: {
-        name: "clone",
-        bsStyle: "primary"
-    },
-    import: {
-        name: "file-import",
-        bsStyle: "primary"
-    },
-    remote: {
-        name: "link",
-        bsStyle: "primary"
-    },
-    remove: {
-        name: "trash",
-        bsStyle: "danger"
-    },
-    remove_isolate: {
-        name: "flask",
-        bsStyle: "danger"
-    },
-    remove_sequence: {
-        name: "dna",
-        bsStyle: "danger"
-    },
-    set_as_default: {
-        name: "star",
-        bsStyle: "warning"
-    },
-    update: {
-        name: "arrow-alt-circle-up",
-        bsStyle: "warning"
-    }
-};
-
-const getMethodIcon = ({ method_name }) => {
-    const props = get(methodIconProps, method_name, {
-        name: "exclamation-triangle",
-        bsStyle: "danger"
-    });
-
-    return <Icon {...props} />;
-};
 
 export class HistoryList extends React.Component {
     render() {
