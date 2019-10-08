113d494d0d15852f7c605d8528fc8a87a9196e97
igboyes <igboyes@gmail.com>
2019-10-08 10:51:13 -0700

    Drop unused subtraction selectors module

--- client/src/js/subtraction/selectors.js
+++
@@ -1,8 +0,0 @@
-import { createSelector } from "reselect";
-import { map } from "lodash-es";
-
-const getSubtractions = state => state.subtraction.documents;
-export const subtractionsSelector = createSelector(
-    getSubtractions,
-    list => map(list, "id")
-);
