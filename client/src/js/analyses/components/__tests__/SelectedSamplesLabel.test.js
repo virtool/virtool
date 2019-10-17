9b63a00f9179f9d13529536473ab21d10b6007c9
SEBASTIANCHEN <734637883@QQ.COM>
2019-10-17 15:51:07 -0700

    Add tests for SelectedSamplesLabel component

---
+++ client/src/js/analyses/components/__tests__/SelectedSamplesLabel.test.js
@@ -0,0 +1,22 @@
+import { SelectedSamplesLabel } from "../Create/SelectedSamplesLabel";
+
+describe("<SelectedSamplesLabel />", () => {
+    let props;
+
+    beforeEach(() => {
+        props = {
+            count: 1
+        };
+    });
+
+    it("should render", () => {
+        const wrapper = shallow(<SelectedSamplesLabel {...props} />);
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    it("should return when [count>1]", () => {
+        props.count = 2;
+        const wrapper = shallow(<SelectedSamplesLabel {...props} />);
+        expect(wrapper).toMatchSnapshot();
+    });
+});
