a978b1f0ffc6143f174b399280feda7e559a7dae
SEBASTIANCHEN <734637883@QQ.COM>
2019-10-18 12:00:30 -0700

    Add tests for IndexSelectorList component

--- client/src/js/analyses/components/Create/IndexSelector.js
+++ client/src/js/analyses/components/Create/IndexSelector.js
@@ -5,11 +5,7 @@ export const IndexSelector = ({ indexes, onSelect, selected, error }) => {
     return (
         <div style={{ marginBottom: "16px" }}>
             <label className="control-label">References</label>
-            {indexes.length ? (
-                <IndexSelectorList error={error} indexes={indexes} selected={selected} onSelect={onSelect} />
-            ) : (
-                <NoneFound noun="source references" />
-            )}
+            <IndexSelectorList error={error} indexes={indexes} selected={selected} onSelect={onSelect} />
         </div>
     );
 };
