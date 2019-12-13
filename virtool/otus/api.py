d860fb0033b106ea213e4ec2001687906f092a9a
Ian Boyes <igboyes@gmail.com>
2019-12-13 14:34:51 -0800

    Use projection for listing OTU-specific history

--- virtool/otus/api.py
+++ virtool/otus/api.py
@@ -670,6 +670,6 @@ async def list_history(req):
     if not await db.otus.find({"_id": otu_id}).count():
         return not_found()
 
-    cursor = db.history.find({"otu.id": otu_id})
+    cursor = db.history.find({"otu.id": otu_id}, projection=virtool.history.db.LIST_PROJECTION)
 
     return json_response([d async for d in cursor])
