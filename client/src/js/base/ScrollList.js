1ff98f7356d091eb8bd486199506447380383d32
Ian Boyes <igboyes@gmail.com>
2020-01-13 12:18:32 -0800

    Fix non-functional ScrollList issue

--- client/src/js/base/ScrollList.js
+++ client/src/js/base/ScrollList.js
@@ -2,37 +2,20 @@ import React from "react";
 import { map } from "lodash-es";
 import { LoadingPlaceholder } from "./index";
 
-export const calculateScrollRatio = (innerHeight, scrollY, scrollHeight) =>
-    ((innerHeight + scrollY) / scrollHeight).toFixed(1);
+export const getScrollRatio = () =>
+    ((window.innerHeight + window.scrollY) / document.documentElement.scrollHeight).toFixed(1);
 
 export class ScrollList extends React.Component {
-    constructor(props) {
-        super(props);
-        this.scrollList = React.createRef();
-    }
-
     componentDidMount() {
-        this.scrollList.current.addEventListener("scroll", this.onScroll);
+        window.addEventListener("scroll", this.onScroll);
     }
 
     componentWillUnmount() {
-        this.scrollList.current.removeEventListener("scroll", this.onScroll);
+        window.removeEventListener("scroll", this.onScroll);
     }
 
-    getScrollRatio = () => {
-        if (this.props.isElement) {
-            return calculateScrollRatio(
-                this.scrollList.current.clientHeight,
-                this.scrollList.current.scrollTop,
-                this.scrollList.current.scrollHeight
-            );
-        }
-
-        return calculateScrollRatio(window.innerHeight, window.scrollY, document.documentElement.scrollHeight);
-    };
-
     onScroll = () => {
-        if (this.props.documents.length && this.props.page < this.props.pageCount && this.getScrollRatio() > 0.8) {
+        if (this.props.documents.length && this.props.page < this.props.pageCount && getScrollRatio() > 0.8) {
             this.props.onLoadNextPage(this.props.page + 1);
         }
     };
@@ -49,7 +32,7 @@ export class ScrollList extends React.Component {
         }
 
         return (
-            <div ref={this.scrollList} style={this.props.style}>
+            <div style={this.props.style}>
                 {entries}
                 {loading}
             </div>
--- client/src/js/base/__tests__/ScrollList.test.js
+++ client/src/js/base/__tests__/ScrollList.test.js
@@ -2,8 +2,6 @@ import { ScrollList } from "../ScrollList";
 
 describe("<ScrollList />", () => {
     let props;
-    const addEventListener = jest.fn();
-    const removeEventListener = jest.fn();
 
     beforeEach(() => {
         props = {
@@ -18,53 +16,37 @@ describe("<ScrollList />", () => {
     it("should return LoadingPlaceholder when [documents=null] and [page<pageCount]", () => {
         props.documents = null;
         const wrapper = mount(<ScrollList {...props} />);
-        wrapper.instance().scrollList.current = {
-            addEventListener,
-            removeEventListener
-        };
         expect(wrapper).toMatchSnapshot();
     });
 
     it("should return React.Fragment when [noContainer=true]", () => {
         const wrapper = mount(<ScrollList {...props} />);
-        wrapper.instance().scrollList.current = {
-            addEventListener,
-            removeEventListener
-        };
         expect(wrapper).toMatchSnapshot();
     });
 
     it("should render when [noContainer=false]", () => {
         props.noContainer = false;
         const wrapper = mount(<ScrollList {...props} />);
-        wrapper.instance().scrollList.current = {
-            addEventListener,
-            removeEventListener
-        };
         expect(wrapper).toMatchSnapshot();
     });
 
     it("componentDidMount() should call addEventListener()", () => {
+        window.addEventListener = jest.fn();
+        window.removeEventListener = jest.fn();
+
         const wrapper = mount(<ScrollList {...props} />);
-        wrapper.instance().scrollList.current = {
-            addEventListener,
-            removeEventListener
-        };
-        wrapper.instance().componentDidMount();
-        expect(wrapper.instance().scrollList.current.addEventListener).toHaveBeenCalledWith(
-            "scroll",
-            wrapper.instance().onScroll
-        );
+
+        expect(window.addEventListener).toHaveBeenCalledWith("scroll", wrapper.instance().onScroll);
     });
 
     it("componentWillUnmount() should call removeEventListener()", () => {
-        const wrapper = mount(<ScrollList {...props} />);
+        window.addEventListener = jest.fn();
+        window.removeEventListener = jest.fn();
 
-        wrapper.instance().scrollList.current = {
-            addEventListener,
-            removeEventListener
-        };
-        wrapper.instance().componentWillUnmount();
-        expect(removeEventListener).toHaveBeenCalledWith("scroll", wrapper.instance().onScroll);
+        const wrapper = mount(<ScrollList {...props} />);
+        const onScroll = wrapper.instance().onScroll;
+        wrapper.unmount();
+
+        expect(window.removeEventListener).toHaveBeenCalledWith("scroll", onScroll);
     });
 });
