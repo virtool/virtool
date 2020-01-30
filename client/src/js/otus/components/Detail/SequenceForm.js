9cb1da0b88dabc289550d19cf3954af5b86eb564
Ian Boyes <igboyes@gmail.com>
2020-01-30 14:05:32 -0800

    Remove unnecessary React.Fragment

--- client/src/js/otus/components/Detail/SequenceForm.js
+++ client/src/js/otus/components/Detail/SequenceForm.js
@@ -4,46 +4,42 @@ import { Row, Col, Modal } from "react-bootstrap";
 import { SaveButton, InputError } from "../../../base";
 import SequenceField from "./SequenceField";
 
-const SegmentForm = props => {
-    return (
-        <React.Fragment>
-            <form onSubmit={props.handleSubmit}>
-                <Modal.Body>
-                    {props.overlay}
-                    <Row>{props.AccessionSegmentCol}</Row>
-                    <Row>
-                        <Col xs={12}>
-                            <InputError label="Host" name="host" value={props.host} onChange={props.handleChange} />
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col xs={12}>
-                            <InputError
-                                label="Definition"
-                                name="definition"
-                                value={props.definition}
-                                onChange={props.handleChange}
-                                error={props.errorDefinition}
-                            />
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col xs={12}>
-                            <SequenceField
-                                name="sequence"
-                                sequence={props.sequence}
-                                onChange={props.handleChange}
-                                error={props.errorSequence}
-                            />
-                        </Col>
-                    </Row>
-                </Modal.Body>
-                <Modal.Footer>
-                    <SaveButton />
-                </Modal.Footer>
-            </form>
-        </React.Fragment>
-    );
-};
+const SegmentForm = props => (
+    <form onSubmit={props.handleSubmit}>
+        <Modal.Body>
+            {props.overlay}
+            <Row>{props.AccessionSegmentCol}</Row>
+            <Row>
+                <Col xs={12}>
+                    <InputError label="Host" name="host" value={props.host} onChange={props.handleChange} />
+                </Col>
+            </Row>
+            <Row>
+                <Col xs={12}>
+                    <InputError
+                        label="Definition"
+                        name="definition"
+                        value={props.definition}
+                        onChange={props.handleChange}
+                        error={props.errorDefinition}
+                    />
+                </Col>
+            </Row>
+            <Row>
+                <Col xs={12}>
+                    <SequenceField
+                        name="sequence"
+                        sequence={props.sequence}
+                        onChange={props.handleChange}
+                        error={props.errorSequence}
+                    />
+                </Col>
+            </Row>
+        </Modal.Body>
+        <Modal.Footer>
+            <SaveButton />
+        </Modal.Footer>
+    </form>
+);
 
 export default SegmentForm;
