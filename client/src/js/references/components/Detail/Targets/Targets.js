5b9e0a117c3cdcb976d65c4af7d44dd5935c7cfc
Ian Boyes <igboyes@gmail.com>
2020-02-14 09:32:47 -0800

    Update targets list appearance

--- client/src/js/references/components/Detail/Targets/Targets.js
+++ client/src/js/references/components/Detail/Targets/Targets.js
@@ -1,27 +1,25 @@
+import { map, reject } from "lodash-es";
 import React from "react";
-import styled from "styled-components";
 import { connect } from "react-redux";
-import { map, remove } from "lodash-es";
-import { BoxGroupHeader, BoxGroup } from "../../../../base";
+import styled from "styled-components";
+import { BoxGroup, BoxGroupHeader } from "../../../../base";
+import { checkRefRight } from "../../../../utils/utils";
 import { editReference } from "../../../actions";
 import AddTarget from "./Add";
 import EditTarget from "./Edit";
 import { TargetItem } from "./Item";
 
-const StyledAddTargets = styled(BoxGroupHeader)`
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
+const TargetsHeader = styled(BoxGroupHeader)`
+    h2 {
+        display: flex;
+        justify-content: space-between;
+    }
 `;
 
 const getInitialState = () => ({
     showAdd: false,
     showEdit: false
 });
-
-export const StyledAddTargetsButton = styled.a`
-    cursor: pointer;
-`;
 
 export class Targets extends React.Component {
     constructor(props) {
@@ -33,53 +31,73 @@ export class Targets extends React.Component {
         this.setState({ showAdd: false, showEdit: false });
     };
 
-    add = () => {
+    showAdd = () => {
         this.setState({ showAdd: true });
     };
 
-    edit = name => {
+    showEdit = name => {
         this.setState({ showEdit: true, activeName: name });
     };
 
     handleRemove = name => {
-        const targetsRemove = [...this.props.targets];
-        remove(targetsRemove, { name });
-        const update = {
-            targets: targetsRemove
-        };
-
-        this.props.onRemove(this.props.refId, update);
+        this.props.onRemove(this.props.refId, {
+            targets: reject(this.props.targets, { name })
+        });
     };
 
     render() {
-        const addButton = <StyledAddTargetsButton onClick={this.add}>Add target</StyledAddTargetsButton>;
-
         const targetComponents = map(this.props.targets, target => (
             <TargetItem
                 key={target.name}
                 {...target}
-                onEdit={() => this.edit(target.name)}
-                onRemove={() => this.handleRemove(target.name)}
+                canModify={this.props.canModify}
+                onEdit={this.showEdit}
+                onRemove={this.handleRemove}
             />
         ));
 
+        let addButton;
+        let modals;
+
+        if (this.props.canModify) {
+            addButton = (
+                <a href="#" onClick={this.showAdd}>
+                    Add target
+                </a>
+            );
+
+            modals = (
+                <React.Fragment>
+                    <AddTarget show={this.state.showAdd} onHide={this.handleHide} />
+                    <EditTarget
+                        show={this.state.showEdit}
+                        onHide={this.handleHide}
+                        activeName={this.state.activeName}
+                    />
+                </React.Fragment>
+            );
+        }
+
         return (
             <BoxGroup>
-                <StyledAddTargets>
-                    <h2>Targets</h2>
-                    {addButton}
-                </StyledAddTargets>
+                <TargetsHeader>
+                    <h2>
+                        <span>Targets</span>
+                        {addButton}
+                    </h2>
+                    <p>Manage the allowable sequence targets for this barcode reference.</p>
+                </TargetsHeader>
 
                 <div>{targetComponents}</div>
-                <AddTarget show={this.state.showAdd} onHide={this.handleHide} />
-                <EditTarget show={this.state.showEdit} onHide={this.handleHide} activeName={this.state.activeName} />
+
+                {modals}
             </BoxGroup>
         );
     }
 }
 
 export const mapStateToProps = state => ({
-    ...state.references.detail.targets,
+    canModify: checkRefRight(state, "modify"),
     refId: state.references.detail.id,
     targets: state.references.detail.targets
 });
