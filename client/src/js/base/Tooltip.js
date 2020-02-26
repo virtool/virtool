b39160977e4d3a8222a51a4886d230980eb24418
sebastian <734637883@qq.com>
2020-02-26 14:00:42 -0800

    Use tippy tooltip

--- client/src/js/base/Tooltip.js
+++ client/src/js/base/Tooltip.js
@@ -1,35 +1,16 @@
 import React from "react";
-import PropTypes from "prop-types";
+import styled from "styled-components";
+import { Tooltip } from "react-tippy";
+import "react-tippy/dist/tippy.css";
 
-/**
- * A custom implementation of the Bootstrap tooltip component.
- *
- * @func
- * @param children
- * @param header {string} a header for the tooltip
- * @param x {number} x position of the tooltip
- * @param y {number} y position of the tooltip
- */
-export const Tooltip = ({ children, header, x, y }) => {
-    const tooltipStyle = {
-        left: x - 10 + "px",
-        top: y - window.pageYOffset - 10 + "px",
-        zIndex: 10000
-    };
+const StyledTooltip = styled(Tooltip)`
+    display: inline-flex !important;
+`;
 
+export const TippyTooltip = props => {
     return (
-        <div className="tooltip" style={tooltipStyle}>
-            {header ? <div className="tooltip-header">{header}</div> : null}
-            <div className="tooltip-body">{children}</div>
-        </div>
+        <StyledTooltip size="big" title={props.tip} arrow={true}>
+            {props.children}
+        </StyledTooltip>
     );
-};
-
-Tooltip.propTypes = {
-    x: PropTypes.number,
-    y: PropTypes.number,
-
-    header: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
-
-    children: PropTypes.node
 };
