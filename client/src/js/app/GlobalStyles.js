f01c8f9c6c58c0fd41bb10bcfa4b755b009e131b
Ian Boyes <igboyes@gmail.com>
2020-03-17 08:34:00 -0700

    Add style theme values

--- client/src/js/app/GlobalStyles.js
+++ client/src/js/app/GlobalStyles.js
@@ -1,10 +1,32 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle, css } from "styled-components";
 
-export const GlobalStyles = createGlobalStyle`    
+export const tabFocus = css`
+    // WebKit-specific. Other browsers will keep their default outline style.
+    // (Initially tried to also force default via \`outline: initial\`,
+    // but that seems to erroneously remove the outline in Firefox altogether.)
+    outline: 5px auto -webkit-focus-ring-color;
+    outline-offset: -2px;
+`;
+
+export const GlobalStyles = createGlobalStyle`
     body {
         font-family: "Roboto", sans-serif;
         height: calc(100vh - 40px);
         overflow-y: scroll;
         padding-top: 65px;
     }
+
+    a {
+      color: ${props => props.theme.color.blue};
+      text-decoration: none;
+    
+      &:hover,
+      &:focus {
+        color: ${props => props.theme.color.blueDark};
+      }
+    
+      &:focus {
+        ${tabFocus}
+      }
+  }  
 `;
--- client/src/js/app/theme.js
+++ client/src/js/app/theme.js
@@ -1,21 +1,56 @@
 export const theme = {
+    borderRadius: {
+        sm: "2px",
+        lg: "4px"
+    },
+    boxShadow: {
+        xs: "0 1px 2px 0 rgba(0, 0, 0, 0.05)",
+        sm: "0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)",
+        md: "0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)",
+        lg: "0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)"
+    },
     color: {
-        blue: "#2B6CB0",
-        green: "#38A169",
-        greyLight: "#dddddd",
-        greyDark: "#555",
+        black: "#000000",
+        blue: "#3182CE",
+        blueDark: "#2B6CB0",
+        blueDarkest: "#2C5282",
+        blueLight: "#63B3ED",
+        blueLightest: "#BEE3F8",
+        green: "#48BB78",
+        greenDark: "#38A169",
+        greenDarkest: "#2F855A",
+        greenLight: "#68D391",
+        greenLightest: "#C6F6D5",
+        grey: "#A0AEC0",
+        greyDark: "#718096",
+        greyDarkest: "#4A5568",
+        greyLight: "#CBD5E0",
+        greyLightest: "#E2E8F0",
+        orange: "#F6AD55",
+        orangeLight: "#FBD38D",
+        orangeLightest: "#FEEBC8",
+        orangeDark: "#ED8936",
+        orangeDarkest: "#DD6B20",
         primary: "#3c8786",
+        purple: "#9F7AEA",
+        purpleDark: "#805AD5",
+        purpleDarkest: "#553C9A",
+        purpleLight: "#D6BCFA",
+        purpleLightest: "#FAF5FF",
         red: "#E53E3E",
-        white: "#fff",
-        orange: "#DD6B20",
-        purpleLight: "#E9D8FD",
-        purple: "#4C51BF",
-        purpleDark: "#6B46C1"
+        redDark: "#C53030",
+        redDarkest: "#9B2C2C",
+        redLight: "#FC8181",
+        redLightest: "#FED7D7",
+        white: "#fff"
     },
     fontSize: {
-        xs: "12px",
-        sm: "14px",
-        md: "16px",
-        lg: "18px"
+        xs: "10px",
+        sm: "12px",
+        md: "14px",
+        lg: "16px"
+    },
+    gap: {
+        column: "15px"
     }
 };
