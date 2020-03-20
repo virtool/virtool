ab216cb61ebebb6b6945519b7ae49e046b2e4000
Ian Boyes <igboyes@gmail.com>
2020-03-19 17:52:22 -0700

    Fix tooltip

--- client/src/js/base/__tests__/Tooltip.test.js
+++ client/src/js/base/__tests__/Tooltip.test.js
@@ -2,25 +2,22 @@ import { Tooltip } from "../Tooltip";
 
 describe("<Tooltip />", () => {
     const props = {
-        tip: "foo",
-        position: "bar",
-        children: { Foo: "Bar" }
+        tip: "Tip",
+        position: "left"
     };
 
-    it("renders correctly", () => {
-        const wrapper = shallow(<Tooltip />);
-        expect(wrapper).toMatchSnapshot();
-    });
-
-    it("should render when tip prop is provided", () => {
-        props.title = "test-header";
+    it("should render", () => {
         const wrapper = shallow(<Tooltip {...props} />);
         expect(wrapper).toMatchSnapshot();
     });
 
-    it("should render when position tip is provided", () => {
-        props.position = "bottom";
+    it("should render without position prop", () => {
         const wrapper = shallow(<Tooltip {...props} />);
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    it("should render with children", () => {
+        const wrapper = shallow(<Tooltip {...props}>Hello world</Tooltip>);
         expect(wrapper).toMatchSnapshot();
     });
 });
