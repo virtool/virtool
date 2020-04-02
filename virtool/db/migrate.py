43a80bc2e5b39f1a58588816d84e346e475f2429
Ian Boyes <igboyes@gmail.com>
2020-04-02 11:55:26 -0700

    Report Mongo version in software status document

--- virtool/db/migrate.py
+++ virtool/db/migrate.py
@@ -93,10 +93,13 @@ async def migrate_status(db, server_version):
         }
     })
 
+    mongo_version = await virtool.db.utils.determine_mongo_version(db)
+
     try:
         await db.status.insert_one({
             "_id": "software",
             "installed": None,
+            "mongo_version": mongo_version,
             "process": None,
             "releases": list(),
             "updating": False,
@@ -105,6 +108,7 @@ async def migrate_status(db, server_version):
     except pymongo.errors.DuplicateKeyError:
         await db.status.update_one({"_id": "software"}, {
             "$set": {
+                "mongo_version": mongo_version,
                 "process": None,
                 "updating": False,
                 "version": server_version
