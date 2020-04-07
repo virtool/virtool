0628e44984b49fe26894755c90451a8e6f43b780
Ian Boyes <igboyes@gmail.com>
2020-04-07 08:44:46 -0700

    Remove extra OTU detail header

--- client/src/js/otus/components/Detail/Detail.js
+++ client/src/js/otus/components/Detail/Detail.js
@@ -3,7 +3,16 @@ import React from "react";
 import { connect } from "react-redux";
 import { Redirect, Route, Switch } from "react-router-dom";
 import styled from "styled-components";
-import { Icon, LoadingPlaceholder, NotFound, TabLink, Tabs, ViewHeader, ViewHeaderTitle } from "../../../base";
+import {
+    Icon,
+    LoadingPlaceholder,
+    NotFound,
+    TabLink,
+    Tabs,
+    ViewHeader,
+    ViewHeaderIcons,
+    ViewHeaderTitle
+} from "../../../base";
 import { Breadcrumb, BreadcrumbItem } from "../../../base/Breadcrumb";
 import { checkRefRight } from "../../../utils/utils";
 import { getOTU, showEditOTU, showRemoveOTU } from "../../actions";
@@ -23,24 +32,18 @@ const OTUSection = () => (
     </div>
 );
 
-const OTUDetailHeader = styled.h1`
+const OTUDetailTitle = styled(ViewHeaderTitle)`
     align-items: baseline;
     display: flex;
-    margin-bottom: 20px;
-    font-size: 24px;
-    font-weight: bold;
 
     small {
-        font-weight: bold;
-        padding-left: 7px;
+        color: ${props => props.theme.color.greyDark};
+        font-weight: 600;
+        margin-left: 7px;
 
         em {
             font-weight: normal;
         }
-    }
-
-    span:last-child {
-        margin-left: auto;
     }
 `;
 
@@ -61,7 +64,6 @@ class OTUDetail extends React.Component {
         const refId = this.props.detail.reference.id;
         const { id, name, abbreviation } = this.props.detail;
 
-        let iconButtons = [];
         let modifyOTUComponents;
 
         let segmentComponent;
@@ -69,29 +71,28 @@ class OTUDetail extends React.Component {
             segmentComponent = <TabLink to={`/refs/${refId}/otus/${id}/schema`}>Schema</TabLink>;
         }
 
+        let iconButtons;
+
         if (this.props.canModify) {
             iconButtons = (
-                <span>
-                    <small key="edit-icon" style={{ paddingLeft: "5px" }}>
-                        <Icon
-                            color="orange"
-                            name="pencil-alt"
-                            tip="Edit OTU"
-                            tipPlacement="left"
-                            onClick={this.props.showEdit}
-                        />
-                    </small>
-
-                    <small key="remove-icon" style={{ paddingLeft: "5px" }}>
-                        <Icon
-                            color="red"
-                            name="trash"
-                            tip="Remove OTU"
-                            tipPlacement="left"
-                            onClick={this.props.showRemove}
-                        />
-                    </small>
-                </span>
+                <ViewHeaderIcons>
+                    <Icon
+                        key="edit-icon"
+                        color="orange"
+                        name="pencil-alt"
+                        tip="Edit OTU"
+                        tipPlacement="left"
+                        onClick={this.props.showEdit}
+                    />
+                    <Icon
+                        key="remove-icon"
+                        color="red"
+                        name="trash"
+                        tip="Remove OTU"
+                        tipPlacement="left"
+                        onClick={this.props.showRemove}
+                    />
+                </ViewHeaderIcons>
             );
 
             modifyOTUComponents = (
@@ -104,10 +105,6 @@ class OTUDetail extends React.Component {
 
         return (
             <div>
-                <ViewHeader title={name}>
-                    <ViewHeaderTitle>{name}</ViewHeaderTitle>
-                </ViewHeader>
-
                 <Breadcrumb>
                     <BreadcrumbItem to="/refs/">References</BreadcrumbItem>
                     <BreadcrumbItem to={`/refs/${refId}`}>{this.props.refName}</BreadcrumbItem>
@@ -115,11 +112,12 @@ class OTUDetail extends React.Component {
                     <BreadcrumbItem>{name}</BreadcrumbItem>
                 </Breadcrumb>
 
-                <OTUDetailHeader>
-                    <strong>{name}</strong>
-                    <small>{abbreviation || <em>No Abbreviation</em>}</small>
-                    {iconButtons}
-                </OTUDetailHeader>
+                <ViewHeader title={name}>
+                    <OTUDetailTitle>
+                        {name} <small>{abbreviation || <em>No Abbreviation</em>}</small>
+                        {iconButtons}
+                    </OTUDetailTitle>
+                </ViewHeader>
 
                 <Tabs>
                     <TabLink to={`/refs/${refId}/otus/${id}/otu`}>OTU</TabLink>
