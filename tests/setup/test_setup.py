47cd35b4a037537634a236583c83f1ad6aefe714
Ian Boyes <igboyes@gmail.com>
2020-05-24 17:33:15 -0700

    Remove graphical first-time setup

--- tests/setup/test_setup.py
+++
@@ -1,9 +0,0 @@
-import virtool.setup.setup
-
-
-def test_setup_schema(setup_defaults):
-    """
-    Check the `get_defaults()` returns expected default setup `dict`.
-
-    """
-    assert virtool.setup.setup.get_defaults() == setup_defaults
