1ccab52e62b2e69ca1df81cd0f81ef8230db1a1e
Ian Boyes <igboyes@gmail.com>
2020-07-21 14:32:36 -0700

    Fix intermittently failing RelativeTime tests
    Use hooks and React testing library.

--- client/src/js/base/__tests__/RelativeTime.test.js
+++ client/src/js/base/__tests__/RelativeTime.test.js
@@ -1,57 +1,41 @@
+import { screen } from "@testing-library/react";
 import { RelativeTime } from "../RelativeTime";
 
+const fakeTime = "2019-02-10T17:11:00.000000Z";
 const RealDate = Date;
 
 describe("<RelativeTime />", () => {
-    let props;
-
-    beforeEach(() => {
-        props = {
-            time: "2019-02-10T17:11:00.000000Z"
-        };
-    });
-
     beforeAll(() => {
-        Date.now = jest.fn(() => new Date("2019-04-22T10:20:30Z"));
+        Date.now = jest.fn();
+        Date.now.mockReturnValue(new Date("2019-04-22T10:20:30Z"));
     });
 
-    it("should render", () => {
-        // formatDistanceStrict.mockReturnValue("2 days ago");
-        // Date.now = jest.fn(() => new Date("2019-04-22T10:20:30Z"));
-        const wrapper = shallow(<RelativeTime {...props} />);
-        expect(wrapper).toMatchSnapshot();
+    it("should render 2 months ago", async () => {
+        renderWithProviders(<RelativeTime time={fakeTime} />);
+        expect(screen.getByText("2 months ago")).toHaveTextContent("2 months ago");
     });
 
-    it("componentDidMount() should update this.interval", () => {
-        const wrapper = shallow(<RelativeTime {...props} />);
-        expect(wrapper.instance().interval).toBe(9);
+    it("should re-render when time prop changes", async () => {
+        const { rerender } = renderWithProviders(<RelativeTime time={fakeTime} />);
+        expect(screen.getByText("2 months ago")).toBeInTheDocument();
+
+        // Add one month to the time prop.
+        rerender(<RelativeTime time="2019-01-10T12:21:00.000000Z" />);
+
+        expect(await screen.queryByText("2 months ago")).toBeNull();
+        expect(await screen.getByText("3 months ago")).toBeInTheDocument();
     });
 
-    it("componentDidUpdate() should update timeString when [prevProps.time !== this.props.time] and [newTimeString !== this.state.timeString]", () => {
-        const wrapper = shallow(<RelativeTime {...props} />);
-        const time = "2019-01-10T12:21:00.000000Z";
+    it("should re-render when time string changes", async () => {
+        jest.useFakeTimers();
 
-        wrapper.setProps({ ...props, time });
+        // Render with time that is only 10 seconds before the current (mocked) time.
+        renderWithProviders(<RelativeTime time="2019-04-22T10:20:20Z" />);
+        expect(await screen.getByText("10 seconds ago")).toBeInTheDocument();
 
-        expect(wrapper.state()).toEqual({
-            time,
-            timeString: "3 months ago"
-        });
-    });
-
-    it("componentDidUpdate() should not call update when [prevProps.time === this.props.time]", () => {
-        const wrapper = shallow(<RelativeTime {...props} />);
-        wrapper.setProps({ ...props, time: "2020-02-14T17:12:00.000000Z" });
-        wrapper.instance().update = jest.fn();
-        expect(wrapper.instance().update).not.toHaveBeenCalled();
-    });
-
-    it("componentWillUnmount() should update this.interval", () => {
-        const wrapper = shallow(<RelativeTime {...props} />);
-        wrapper.instance().interval = 14;
-        window.clearInterval = jest.fn();
-        wrapper.unmount();
-        expect(window.clearInterval).toHaveBeenCalledWith(14);
+        Date.now.mockReturnValue(new Date("2019-04-22T10:20:40Z"));
+        jest.advanceTimersToNextTimer();
+        expect(await screen.findByText("20 seconds ago")).toBeInTheDocument();
     });
 
     afterAll(() => {
