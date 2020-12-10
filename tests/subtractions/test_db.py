8f1c1126fa1001de13b5ecf7dc0dfd72e9372212
Tiansheng Sui <s7782519728@gmail.com>
2020-12-09 17:00:00 -0800

    Add function to attach subtraction list (#2010)
    * Add attach_subtractions function
    * Add and update test files

--- tests/subtractions/test_db.py
+++ tests/subtractions/test_db.py
@@ -82,3 +82,37 @@ async def test_add_subtraction_files_task(ignore, mocker, tmpdir, spawn_client, 
             }
         ]
     }
+
+
+async def test_attach_subtractions(dbi):
+    await dbi.subtraction.insert_many([
+        {
+            "_id": "foo",
+            "name": "Foo"
+        },
+        {
+            "_id": "bar",
+            "name": "Bar"
+        }
+    ])
+
+    document = {
+        "_id": "foobar",
+        "subtractions": ["foo", "bar"]
+    }
+
+    await virtool.subtractions.db.attach_subtractions(dbi, document)
+
+    assert document == {
+        "_id": "foobar",
+        "subtractions": [
+            {
+                "id": "foo",
+                "name": "Foo"
+            },
+            {
+                "id": "bar",
+                "name": "Bar"
+            }
+        ]
+    }
