91461ba58d1fe85e162e338c921289c7024c610a
Sean <s7782519728@gmail.com>
2021-01-06 09:49:50 -0800

    Add docstring and typing

--- virtool/postgres.py
+++ virtool/postgres.py
@@ -3,12 +3,19 @@ import sys
 
 import sqlalchemy
 from sqlalchemy import text
-from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.ext.asyncio import AsyncConnection, create_async_engine
 
 logger = logging.getLogger(__name__)
 
 
-async def connect(postgres_connection_string: str) -> sqlalchemy.engine:
+async def connect(postgres_connection_string: str) -> AsyncConnection:
+    """
+    Create a connection of Postgres.
+
+    :param postgres_connection_string: the postgres connection string
+    :return: an AsyncConnection object
+
+    """
     if not postgres_connection_string.startswith("postgresql://"):
         logger.fatal("Invalid PostgreSQL connection string")
         sys.exit(1)
@@ -24,7 +31,13 @@ async def connect(postgres_connection_string: str) -> sqlalchemy.engine:
         sys.exit(1)
 
 
-async def check_version(connection):
+async def check_version(connection: AsyncConnection):
+    """
+    Check and log the Postgres sever version.
+
+    :param connection:an AsyncConnection object
+
+    """
     info = await connection.execute(text('SHOW server_version'))
 
     version = info.first()[0].split()[0]
