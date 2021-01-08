be03bb256b7c4bf5d59a160040b641ba8911ab7b
Sean <s7782519728@gmail.com>
2021-01-08 13:47:20 -0800

    Add tables declaration in models.py

---
+++ virtool/models.py
@@ -0,0 +1,22 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String
+
+Base = declarative_base()
+
+
+class Label(Base):
+    __tablename__ = 'labels'
+
+    id = Column(String, primary_key=True)
+    name = Column(String)
+    color = Column(String)
+    description = Column(String)
+
+    def __repr__(self):
+        return "<Label(name='%s', color='%s', description='%s')>" % (
+            self.name, self.color, self.description)
+
+
+async def create_tables(engine):
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
