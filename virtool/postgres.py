2641ce29e1cdd0bab3a215e41bae50b31c803a04
Sean <s7782519728@gmail.com>
2021-01-08 14:17:32 -0800

    Return AsyncEngine insead of AsyncConnection

--- virtool/postgres.py
+++ virtool/postgres.py
@@ -2,18 +2,18 @@ import logging
 import sys
 
 from sqlalchemy import text
-from sqlalchemy.ext.asyncio import AsyncConnection, create_async_engine
+from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine
 
 import virtool.models
 logger = logging.getLogger(__name__)
 
 
-async def connect(postgres_connection_string: str) -> AsyncConnection:
+async def connect(postgres_connection_string: str) -> AsyncEngine:
     """
     Create a connection of Postgres.
 
     :param postgres_connection_string: the postgres connection string
-    :return: an AsyncConnection object
+    :return: an AsyncEngine object
 
     """
     if not postgres_connection_string.startswith("postgresql+asyncpg://"):
@@ -22,24 +22,25 @@ async def connect(postgres_connection_string: str) -> AsyncConnection:
 
     try:
         postgres = create_async_engine(postgres_connection_string)
+
+        await check_version(postgres)
         await virtool.models.create_tables(postgres)
 
-        async with postgres.connect() as connection:
-            await check_version(connection)
-            return connection
+        return postgres
     except ConnectionRefusedError:
         logger.fatal("Could not connect to PostgreSQL: Connection refused")
         sys.exit(1)
 
 
-async def check_version(connection: AsyncConnection):
+async def check_version(engine: AsyncEngine):
     """
     Check and log the Postgres sever version.
 
-    :param connection:an AsyncConnection object
+    :param engine: an AsyncConnection object
 
     """
-    info = await connection.execute(text('SHOW server_version'))
+    async with engine.connect() as conn:
+        info = await conn.execute(text('SHOW server_version'))
 
     version = info.first()[0].split()[0]
     logger.info(f"Found PostgreSQL {version}")
