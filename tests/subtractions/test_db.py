8ef5e8c2274e5171dd4c4f34bb0e9d9dd3f25a11
Ian Boyes <igboyes@gmail.com>
2021-01-18 11:00:06 -0800

    Unlink default subtractions by removing from list
    Previously the subtraction field was to null

--- tests/subtractions/test_db.py
+++ tests/subtractions/test_db.py
@@ -1,4 +1,5 @@
 import virtool.subtractions.db
+from virtool.subtractions.db import unlink_default_subtractions
 
 
 async def test_attach_subtractions(dbi):
@@ -33,3 +34,21 @@ async def test_attach_subtractions(dbi):
             }
         ]
     }
+
+
+async def test_unlink_default_subtractions(dbi):
+    await dbi.samples.insert_many([
+        {"_id": "foo", "subtractions": ["1", "2", "3"]},
+        {"_id": "bar", "subtractions": ["2", "5", "8"]},
+        {"_id": "baz", "subtractions": ["2"]}
+    ])
+
+    await unlink_default_subtractions(dbi, "2")
+
+
+
+    assert await dbi.samples.find().to_list(None) == [
+        {"_id": "foo", "subtractions": ["1", "3"]},
+        {"_id": "bar", "subtractions": ["5", "8"]},
+        {"_id": "baz", "subtractions": []}
+    ]
