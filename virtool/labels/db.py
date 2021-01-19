7795df155dde9bd6ccd58506fe37ab95f9f35eba
Sean <s7782519728@gmail.com>
2021-01-19 15:31:51 -0800

    Add docstring and typing

--- virtool/labels/db.py
+++ virtool/labels/db.py
@@ -1,4 +1,8 @@
-async def attach_sample_count(db, document, label_id):
+async def attach_sample_count(db, document: dict, label_id: int):
+    """
+    Attach the number of samples associated with the given label to the passed document.
+
+    """
     return {
         **document,
         "count": await db.samples.count_documents({"labels": label_id})
