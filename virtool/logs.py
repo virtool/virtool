fd1a066f6167ea450898f37728ff8446890a9132
Ian Boyes <igboyes@gmail.com>
2021-01-19 13:44:42 -0800

    Write logs for server and runner to different files

--- virtool/logs.py
+++ virtool/logs.py
@@ -3,27 +3,47 @@ import logging.handlers
 import coloredlogs
 
 
-def configure(dev, verbose):
+def get_log_format(verbose):
+    if verbose:
+        return "{asctime:<20} {module:<11} {levelname:<8} {message} [{name}:{funcName}:{lineno}]"
+
+    return "{asctime:<20} {module:<11} {levelname:<8} {message}"
+
+
+def configure_base_logger(dev, verbose):
     verbose = dev or verbose
 
     logging_level = logging.DEBUG if verbose else logging.INFO
 
     logging.captureWarnings(True)
 
-    log_format = "{asctime:<20} {module:<11} {levelname:<8} {message}" \
-        if not verbose else \
-        "{asctime:<20} {module:<11} {levelname:<8} {message} [{name}:{funcName}:{lineno}]"
-
     coloredlogs.install(
         level=logging_level,
-        fmt=log_format,
+        fmt=get_log_format(verbose),
         style="{"
     )
 
     logger = logging.getLogger()
 
-    handler = logging.handlers.RotatingFileHandler("virtool.log", maxBytes=1000000, backupCount=5)
-    handler.setFormatter(logging.Formatter(log_format, style="{"))
+    return logger
+
+
+def configure_server(dev, verbose):
+    logger = configure_base_logger(dev, verbose)
+
+    handler = logging.handlers.RotatingFileHandler("server.log", maxBytes=1000000, backupCount=3)
+    handler.setFormatter(logging.Formatter(get_log_format(verbose), style="{"))
+
+    logger.addHandler(handler)
+
+    return logger
+
+
+def configure_runner(dev, verbose):
+    logger = configure_base_logger(dev, verbose)
+
+    handler = logging.handlers.RotatingFileHandler("runner.log", maxBytes=1000000, backupCount=3)
+    handler.setFormatter(logging.Formatter(get_log_format(verbose), style="{"))
 
     logger.addHandler(handler)
 
