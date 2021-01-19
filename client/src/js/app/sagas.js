50d2e13609e49cef10a10cd9717383328c6ed57c
Ian Boyes <igboyes@gmail.com>
2021-01-19 13:04:31 -0800

    Add state and API code for developer commands

--- client/src/js/app/sagas.js
+++ client/src/js/app/sagas.js
@@ -4,6 +4,7 @@ import { watchAccount } from "../account/sagas";
 import { watchSettings } from "../administration/sagas";
 import { watchAnalyses } from "../analyses/sagas";
 import { watchCaches } from "../caches/sagas";
+import { watchDev } from "../dev/sagas";
 import { watchFiles } from "../files/sagas";
 import { watchGroups } from "../groups/sagas";
 import { watchHmms } from "../hmm/sagas";
@@ -39,6 +40,7 @@ function* rootSaga() {
         watchAccount(),
         watchAnalyses(),
         watchCaches(),
+        watchDev(),
         watchFiles(),
         watchSubtraction(),
         watchHmms(),
