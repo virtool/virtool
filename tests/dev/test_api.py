4bf39159954a252f44e33a85308e2ec09c048331
Ian Boyes <igboyes@gmail.com>
2021-01-19 12:46:32 -0800

    Add API endpoint at /api/dev to submit developer commands
    Will allow easier development by adding commands to modify application state instead of running jobs and manipulating the database manually.

---
+++ tests/dev/test_api.py
@@ -0,0 +1,13 @@
+import pytest
+
+
+@pytest.mark.parametrize("dev", [True, False])
+async def test_dev_mode(dev, spawn_client):
+    """
+    Ensure that developer endpoint is not available when not in developer mode.
+    """
+    client = await spawn_client(authorize=True, dev=dev)
+
+    resp = await client.post("/api/dev", {"command": "foo"})
+
+    assert resp.status == (204 if dev else 404)
