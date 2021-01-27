5bb882d3d9c3606aa918b0f29c251cd0bf280500
Jake Alexander <jake.ale@outlook.com>
2021-01-27 12:59:25 -0800

    Model uploads in SQL (#2055)
    * Add SQL model for uploads
    * Use enum for upload file type

---
+++ virtool/uploads/models.py
@@ -0,0 +1,33 @@
+import enum
+
+from sqlalchemy import Column, String, Boolean, Integer, DateTime, Enum
+
+from virtool.postgres import Base
+
+
+class UploadType(enum.Enum):
+    reference = "reference"
+    reads = "reads"
+    subtraction = "subtraction"
+    null = None
+
+
+class Upload(Base):
+    __tablename__ = "uploads"
+
+    id = Column(Integer, primary_key=True)
+    created_at = Column(DateTime)
+    name = Column(String)
+    name_on_disk = Column(String, unique=True)
+    ready = Column(Boolean)
+    removed = Column(Boolean)
+    reserved = Column(Boolean)
+    size = Column(Integer)
+    type = Column(Enum(UploadType))
+    user = Column(String)
+    uploaded_at = Column(DateTime)
+
+    def __repr__(self):
+        return f"<Upload(id={self.id}, created_at={self.created_at}, name={self.name}, " \
+               f"name_on_disk={self.name_on_disk}, ready={self.ready}, reserved={self.reserved}, " \
+               f"size={self.size}, type={self.type}, uploaded_at={self.uploaded_at}, user={self.user}"
