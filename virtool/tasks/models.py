5e356508b8544b2873646dad2aca2eb86ba3c7ba
Sean <s7782519728@gmail.com>
2021-01-28 10:55:05 -0800

    Add Task model

---
+++ virtool/tasks/models.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Boolean, Column, DateTime, Integer, String
+from sqlalchemy.dialects.postgresql import JSONB
+
+from virtool.postgres import Base
+
+
+class Task(Base):
+    __tablename__ = 'tasks'
+
+    id = Column(Integer, primary_key=True)
+    complete = Column(Boolean)
+    context = Column(JSONB)
+    count = Column(Integer)
+    created_at = Column(DateTime)
+    file_size = Column(Integer)
+    progress = Column(Integer)
+    step = Column(String)
+    type = Column(String)
+
+    def __repr__(self):
+        return f"<Task(id={self.id}, complete={self.complete}, context={self.context}, " \
+               f"count={self.count}, created_at={self.created_at}, file_size={self.file_size}, " \
+               f"progress={self.progress}, step={self.step}, type={self.type})>"
