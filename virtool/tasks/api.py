47245ce542067733598f7837a5ffba4d1d0aac70
Sean <s7782519728@gmail.com>
2021-01-29 13:40:08 -0800

    Reimplement tasks API endpoints in SQL

--- virtool/tasks/api.py
+++ virtool/tasks/api.py
@@ -1,4 +1,5 @@
 import virtool.http.routes
+import virtool.tasks.pg
 import virtool.utils
 from virtool.api.response import json_response, not_found
 
@@ -7,22 +8,18 @@ routes = virtool.http.routes.Routes()
 
 @routes.get("/api/tasks")
 async def find(req):
-    db = req.app["db"]
-
-    documents = [virtool.utils.base_processor(d) async for d in db.tasks.find()]
+    documents = await virtool.tasks.pg.find(req.app["postgres"])
 
     return json_response(documents)
 
 
 @routes.get("/api/tasks/{task_id}")
 async def get(req):
-    db = req.app["db"]
-
     task_id = req.match_info["task_id"]
 
-    document = await db.tasks.find_one(task_id)
+    document = await virtool.tasks.pg.get(req.app["postgres"], int(task_id))
 
     if not document:
         return not_found()
 
-    return json_response(virtool.utils.base_processor(document))
+    return json_response(document.to_dict())
