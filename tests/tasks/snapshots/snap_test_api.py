320359ed92972e8bbec0af347f4c77ed4cbe8c9e
Sean <s7782519728@gmail.com>
2021-01-29 13:40:38 -0800

    Add tests for tasks API endpoints

---
+++ tests/tasks/snapshots/snap_test_api.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# snapshottest: v1 - https://goo.gl/zC4yUc
+from __future__ import unicode_literals
+
+from snapshottest import Snapshot
+
+
+snapshots = Snapshot()
+
+snapshots['test_find[uvloop] 1'] = [
+    {
+        'complete': True,
+        'context': {
+            'user_id': 'test_1'
+        },
+        'count': 40,
+        'created_at': '2015-10-06T20:00:00Z',
+        'file_size': 1024,
+        'id': 1,
+        'progress': 100,
+        'step': 'download',
+        'type': 'clone_reference'
+    },
+    {
+        'complete': False,
+        'context': {
+            'user_id': 'test_2'
+        },
+        'count': 30,
+        'created_at': '2015-10-06T20:00:00Z',
+        'file_size': 14754,
+        'id': 2,
+        'progress': 80,
+        'step': 'download',
+        'type': 'import_reference'
+    }
+]
