bc190511bf380e8b572bd5498ba6bb94fe3a1805
Sean <s7782519728@gmail.com>
2021-01-29 16:21:31 -0800

    Migrate virtool.tasks.db to virtool.tasks.pg

--- virtool/tasks/pg.py
+++ virtool/tasks/pg.py
@@ -1,6 +1,11 @@
 from sqlalchemy import select
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
 
+import virtool.db.utils
+import virtool.tasks.steps
+import virtool.tasks.task
+import virtool.utils
 from virtool.tasks.models import Task
 
 
@@ -18,3 +23,67 @@ async def get(pg_engine, task_id):
         result = await session.execute(select(Task).filter_by(id=task_id))
 
     return result.scalar()
+
+
+async def register(pg_engine, task_type, context=None):
+    task = Task(
+        complete=False,
+        context=context or dict(),
+        count=0,
+        created_at=virtool.utils.timestamp(),
+        progress=0,
+        step=virtool.tasks.steps.FIRST_STEPS[task_type],
+        type=task_type
+    )
+
+    async with AsyncSession(pg_engine) as session:
+        session.add(task)
+        await session.flush()
+        document = task.to_dict()
+        await session.commit()
+
+    return virtool.utils.base_processor(document)
+
+
+async def update(pg_engine, task_id, count=None, progress=None, step=None, context_update=None, error=None):
+    async with AsyncSession(pg_engine) as session:
+        result = await session.execute(select(Task).filter_by(id=task_id))
+        task = result.scalar()
+
+        if count is not None:
+            task.count = count
+
+        if progress is not None:
+            task.progress = progress
+
+        if step:
+            task.step = step
+
+        if error is not None:
+            task.error = error
+
+        if context_update:
+            for key, value in context_update.items():
+                task.context[key] = value
+        document = task.to_dict()
+        await session.commit()
+
+    return document
+
+
+async def complete(pg_engine, task_id):
+    async with AsyncSession(pg_engine) as session:
+        result = await session.execute(select(Task).filter_by(id=task_id))
+        task = result.scalar()
+        task.complete = True
+        task.progress = 100
+        await session.commit()
+
+
+async def remove(pg_engine, task_id):
+    async with AsyncSession(pg_engine) as session:
+        result = await session.execute(select(Task).filter_by(id=task_id))
+        task = result.scalar()
+
+        session.delete(task)
+        await session.commit()
