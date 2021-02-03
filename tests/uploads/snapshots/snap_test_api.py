855bc1611dd7450dc96c9db1ed1ec879f8d8fef9
Jake Alexander <jake.ale@outlook.com>
2021-02-03 10:25:08 -0800

    Implement uploads in SQL (#2079)
    * Migrate `uploads.api.upload` to SQL
    * Move upload document creation to `uploads.db.create`
    * Add hmm to UploadType and update `__repr__()`
    * Add error checking to `uploads.api`

---
+++ tests/uploads/snapshots/snap_test_api.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+# snapshottest: v1 - https://goo.gl/zC4yUc
+from __future__ import unicode_literals
+
+from snapshottest import Snapshot
+
+
+snapshots = Snapshot()
+
+snapshots['TestUpload.test[uvloop-reference] 1'] = {
+    'created_at': '2015-10-06T20:00:00Z',
+    'id': 1,
+    'name': 'Test.fq.gz',
+    'name_on_disk': '1-Test.fq.gz',
+    'ready': False,
+    'removed': False,
+    'reserved': False,
+    'size': 9081,
+    'type': 'reference',
+    'uploaded_at': '2015-10-06T20:00:00Z',
+    'user': 'test'
+}
+
+snapshots['TestUpload.test[uvloop-reads] 1'] = {
+    'created_at': '2015-10-06T20:00:00Z',
+    'id': 1,
+    'name': 'Test.fq.gz',
+    'name_on_disk': '1-Test.fq.gz',
+    'ready': False,
+    'removed': False,
+    'reserved': False,
+    'size': 9081,
+    'type': 'reads',
+    'uploaded_at': '2015-10-06T20:00:00Z',
+    'user': 'test'
+}
+
+snapshots['TestUpload.test[uvloop-hmm] 1'] = {
+    'created_at': '2015-10-06T20:00:00Z',
+    'id': 1,
+    'name': 'Test.fq.gz',
+    'name_on_disk': '1-Test.fq.gz',
+    'ready': False,
+    'removed': False,
+    'reserved': False,
+    'size': 9081,
+    'type': 'hmm',
+    'uploaded_at': '2015-10-06T20:00:00Z',
+    'user': 'test'
+}
+
+snapshots['TestUpload.test[uvloop-subtraction] 1'] = {
+    'created_at': '2015-10-06T20:00:00Z',
+    'id': 1,
+    'name': 'Test.fq.gz',
+    'name_on_disk': '1-Test.fq.gz',
+    'ready': False,
+    'removed': False,
+    'reserved': False,
+    'size': 9081,
+    'type': 'subtraction',
+    'uploaded_at': '2015-10-06T20:00:00Z',
+    'user': 'test'
+}
--- tests/uploads/test_api.py
+++ tests/uploads/test_api.py
@@ -1,12 +1,13 @@
 import os
-import pytest
 import sys
+
+import pytest
 
 
 class TestUpload:
 
     @pytest.mark.parametrize("file_type", ["reference", "reads", "hmm", "subtraction"])
-    async def test(self, file_type, tmpdir, spawn_client, static_time, test_random_alphanumeric):
+    async def test(self, file_type, tmpdir, snapshot, spawn_client, static_time, test_random_alphanumeric):
         client = await spawn_client(authorize=True, permissions=["upload_file"])
 
         client.app["settings"]["data_path"] = str(tmpdir)
@@ -21,23 +22,13 @@ class TestUpload:
             "file": open(path, "rb")
         }
 
-        resp = await client.post_form("/upload/{}?name=Test.fq.gz".format(file_type), data=files)
+        resp = await client.post_form(f"/upload/{file_type}?name=Test.fq.gz", data=files)
 
         assert resp.status == 201
 
-        assert os.listdir(str(files_dir)) == ["{}-Test.fq.gz".format(test_random_alphanumeric.last_choice)]
+        assert os.listdir(str(files_dir)) == ["1-Test.fq.gz"]
 
-        assert await resp.json() == {
-            "name": "Test.fq.gz",
-            "type": file_type,
-            "ready": False,
-            "reserved": False,
-            "uploaded_at": static_time.iso,
-            "id": "{}-Test.fq.gz".format(test_random_alphanumeric.last_choice),
-            "user": {
-                "id": "test"
-            }
-        }
+        snapshot.assert_match(await resp.json())
 
     async def test_invalid_query(self, spawn_client, resp_is):
         client = await spawn_client(authorize=True, permissions=["upload_file"])
@@ -54,7 +45,7 @@ class TestUpload:
             "name": ["required field"]
         })
 
-    async def test_not_found(self, spawn_client, resp_is):
+    async def test_bad_type(self, spawn_client, resp_is):
         client = await spawn_client(authorize=True, permissions=["upload_file"])
 
         path = os.path.join(sys.path[0], "tests", "test_files", "test.fq.gz")
@@ -63,6 +54,6 @@ class TestUpload:
             "file": open(path, "rb")
         }
 
-        resp = await client.post_form("/uploads/foobar", data=files)
+        resp = await client.post_form("/upload/foobar?name=Test.fq.gz", data=files)
 
-        assert resp.status == 404
+        assert await resp_is.bad_request(resp, message="Unsupported file type")
