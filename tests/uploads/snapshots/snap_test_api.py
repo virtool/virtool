f799939f0edde140e38a7b6a7702cc66c3771dc8
Jake Alexander <jake.ale@outlook.com>
2021-02-03 16:38:33 -0800

    Add `GET /api/uploads` endpoint

--- tests/uploads/snapshots/snap_test_api.py
+++ tests/uploads/snapshots/snap_test_api.py
@@ -7,6 +7,20 @@ from snapshottest import Snapshot
 
 snapshots = Snapshot()
 
+snapshots['TestUpload.test[uvloop-hmm] 1'] = {
+    'created_at': '2015-10-06T20:00:00Z',
+    'id': 1,
+    'name': 'Test.fq.gz',
+    'name_on_disk': '1-Test.fq.gz',
+    'ready': False,
+    'removed': False,
+    'reserved': False,
+    'size': 9081,
+    'type': 'hmm',
+    'uploaded_at': '2015-10-06T20:00:00Z',
+    'user': 'test'
+}
+
 snapshots['TestUpload.test[uvloop-reference] 1'] = {
     'created_at': '2015-10-06T20:00:00Z',
     'id': 1,
@@ -31,20 +45,6 @@ snapshots['TestUpload.test[uvloop-reads] 1'] = {
     'reserved': False,
     'size': 9081,
     'type': 'reads',
-    'uploaded_at': '2015-10-06T20:00:00Z',
-    'user': 'test'
-}
-
-snapshots['TestUpload.test[uvloop-hmm] 1'] = {
-    'created_at': '2015-10-06T20:00:00Z',
-    'id': 1,
-    'name': 'Test.fq.gz',
-    'name_on_disk': '1-Test.fq.gz',
-    'ready': False,
-    'removed': False,
-    'reserved': False,
-    'size': 9081,
-    'type': 'hmm',
     'uploaded_at': '2015-10-06T20:00:00Z',
     'user': 'test'
 }
@@ -76,3 +76,82 @@ snapshots['TestUpload.test[uvloop-None] 1'] = {
     'uploaded_at': '2015-10-06T20:00:00Z',
     'user': 'test'
 }
+
+snapshots['TestFind.test[uvloop-danny-reads] 1'] = [
+    {
+        'created_at': None,
+        'id': 1,
+        'name': 'test.fq.gz',
+        'name_on_disk': None,
+        'ready': None,
+        'removed': None,
+        'reserved': None,
+        'size': None,
+        'type': 'reads',
+        'uploaded_at': None,
+        'user': 'danny'
+    }
+]
+
+snapshots['TestFind.test[uvloop-danny-reference] 1'] = [
+]
+
+snapshots['TestFind.test[uvloop-danny-None] 1'] = [
+    {
+        'created_at': None,
+        'id': 1,
+        'name': 'test.fq.gz',
+        'name_on_disk': None,
+        'ready': None,
+        'removed': None,
+        'reserved': None,
+        'size': None,
+        'type': 'reads',
+        'uploaded_at': None,
+        'user': 'danny'
+    }
+]
+
+snapshots['TestFind.test[uvloop-lester-reads] 1'] = [
+]
+
+snapshots['TestFind.test[uvloop-lester-reference] 1'] = [
+]
+
+snapshots['TestFind.test[uvloop-lester-None] 1'] = [
+    {
+        'created_at': None,
+        'id': 2,
+        'name': 'test.fq.gz',
+        'name_on_disk': None,
+        'ready': None,
+        'removed': None,
+        'reserved': None,
+        'size': None,
+        'type': 'subtraction',
+        'uploaded_at': None,
+        'user': 'lester'
+    }
+]
+
+snapshots['TestFind.test[uvloop-jake-reads] 1'] = [
+]
+
+snapshots['TestFind.test[uvloop-jake-reference] 1'] = [
+]
+
+snapshots['TestFind.test[uvloop-jake-None] 1'] = [
+    {
+        'created_at': None,
+        'id': 3,
+        'name': 'test.fq.gz',
+        'name_on_disk': None,
+        'ready': None,
+        'removed': None,
+        'reserved': None,
+        'size': None,
+        'type': None,
+        'uploaded_at': None,
+        'user': 'jake'
+    }
+]
