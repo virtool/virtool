5ac05705742b7278a93bfe0e3902f1a601e867ea
Sean <s7782519728@gmail.com>
2021-02-08 13:29:52 -0800

    Remove unnecessary label_id argument on attach_sample_count

--- virtool/labels/db.py
+++ virtool/labels/db.py
@@ -1,9 +1,9 @@
-async def attach_sample_count(db, document: dict, label_id: int):
+async def attach_sample_count(db, document: dict):
     """
     Attach the number of samples associated with the given label to the passed document.
 
     """
     return {
         **document,
-        "count": await db.samples.count_documents({"labels": label_id})
+        "count": await db.samples.count_documents({"labels": document["id"]})
     }
