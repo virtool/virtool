36a505ad21459a0a2eb730beb8f75e4ea144e41f
Ian Boyes <igboyes@gmail.com>
2021-02-12 16:48:28 -0800

    Improve job fixture
    Make a MutableMapping. Access id and fetch from DB via methods.

--- tests/fixtures/jobs.py
+++ tests/fixtures/jobs.py
@@ -1,9 +1,43 @@
+from typing import MutableMapping
+
 import pytest
 
 
+class TestJob(MutableMapping):
+
+    def __init__(self, db, data: dict):
+        self._data = data
+        self._db = db
+
+    @property
+    def id(self):
+        return self._data["_id"]
+
+    async def fetch(self):
+        return self._db.jobs.find_one({"_id": self.id})
+
+    def __getitem__(self, key):
+        return self._data[key]
+
+    def __setitem__(self, key, value):
+        self._data[key] = value
+
+    def __delitem__(self, key):
+        del self._data[key]
+
+    def __iter__(self):
+        return iter(self._data)
+
+    def __len__(self):
+        return len(self._data)
+
+    def __repr__(self):
+        return repr(self._data)
+
+
 @pytest.fixture
-def test_job(static_time):
-    return {
+def test_job(dbi, static_time):
+    return TestJob(dbi, {
         "_id": "4c530449",
         "acquired": False,
         "args": {
@@ -51,4 +85,4 @@ def test_job(static_time):
         "user": {
             "id": "igboyes"
         }
-    }
+    })
