00786966b3ef2365116b6fe0463b8b35ebbad8ca
Ian Boyes <igboyes@gmail.com>
2021-02-12 16:36:37 -0800

    Add jobs_only endpoint flag
    Flags the endpoint as only being available to clients authenticated as jobs.

--- virtool/http/routes.py
+++ virtool/http/routes.py
@@ -14,36 +14,42 @@ class Routes(aiohttp.web.RouteTableDef):
     def __init__(self):
         super().__init__()
 
-    def get(self, *args, admin=False, allow_jobs=False, permission=None, public=False, schema=None,
+    def get(self, *args, admin=False, allow_jobs=False, jobs_only=False, permission=None,
+            public=False, schema=None,
             **kwargs):
         route_decorator = super().get(*args, **kwargs)
-        return protect(route_decorator, admin, allow_jobs, permission, public, schema)
+        return protect(route_decorator, admin, allow_jobs, jobs_only, permission, public, schema)
 
-    def post(self, *args, admin=False, allow_jobs=False, permission=None, public=False,
+    def post(self, *args, admin=False, allow_jobs=False, jobs_only=False, permission=None,
+             public=False,
              schema=None, **kwargs):
         route_decorator = super().post(*args, **kwargs)
-        return protect(route_decorator, admin, allow_jobs, permission, public, schema)
+        return protect(route_decorator, admin, allow_jobs, jobs_only, permission, public, schema)
 
-    def patch(self, *args, admin=False, allow_jobs=False, permission=None, public=False,
+    def patch(self, *args, admin=False, allow_jobs=False, jobs_only=False, permission=None,
+              public=False,
               schema=None, **kwargs):
         route_decorator = super().patch(*args, **kwargs)
-        return protect(route_decorator, admin, allow_jobs, permission, public, schema)
+        return protect(route_decorator, admin, allow_jobs, jobs_only, permission, public, schema)
 
-    def put(self, *args, admin=False, allow_jobs=False, permission=None, public=False, schema=None,
+    def put(self, *args, admin=False, allow_jobs=False, jobs_only=False, permission=None,
+            public=False, schema=None,
             **kwargs):
         route_decorator = super().put(*args, **kwargs)
-        return protect(route_decorator, admin, allow_jobs, permission, public, schema)
+        return protect(route_decorator, admin, allow_jobs, jobs_only, permission, public, schema)
 
-    def delete(self, *args, admin=False, allow_jobs=False, permission=None, public=False,
+    def delete(self, *args, admin=False, allow_jobs=False, jobs_only=False, permission=None,
+               public=False,
                schema=None, **kwargs):
         route_decorator = super().delete(*args, **kwargs)
-        return protect(route_decorator, admin, allow_jobs, permission, public, schema)
+        return protect(route_decorator, admin, allow_jobs, jobs_only, permission, public, schema)
 
 
 def protect(
         route_decorator: Callable,
         admin: bool,
         allow_jobs: bool,
+        jobs_only: bool,
         permission: str,
         public: bool,
         schema: Dict[str, Any]
@@ -58,10 +64,18 @@ def protect(
             if not public and client is None:
                 return unauthorized("Requires authorization")
 
-            if isinstance(client, JobClient) and not allow_jobs:
+            is_job_client = isinstance(client, JobClient)
+
+            if is_job_client and not allow_jobs and not jobs_only:
                 return json_response({
                     "id": "no_jobs",
                     "message": "Job access is forbidden"
+                }, status=403)
+
+            if not is_job_client and jobs_only:
+                return json_response({
+                    "id": "jobs_only",
+                    "message": "Only job access is allowed at this endpoint"
                 }, status=403)
 
             if client is None or not client.administrator:
