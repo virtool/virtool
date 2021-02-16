5783d1c2b79fcaa357ca11332d5351645953dcc3
Sean <s7782519728@gmail.com>
2021-02-16 14:33:41 -0800

    Add test for prepare_files_field()

--- tests/subtractions/test_utils.py
+++ tests/subtractions/test_utils.py
@@ -1,3 +1,5 @@
+import os
+
 import virtool.subtractions.utils
 
 
@@ -9,3 +11,25 @@ def test_join_subtraction_path():
     path = virtool.subtractions.utils.join_subtraction_path(settings, "bar")
 
     assert path == "/foo/subtractions/bar"
+
+
+def test_prepare_files_field(tmpdir):
+    test_dir = tmpdir.mkdir("subtractions")
+    test_dir.join("subtraction.fa.gz").write("FASTA file")
+    test_dir.join("subtraction.1.bt2").write("Bowtie2 file")
+    path = os.path.join(tmpdir, "subtractions")
+
+    files = virtool.subtractions.utils.prepare_files_field(path)
+
+    assert files == [
+        {
+            'size': os.stat(os.path.join(path, "subtraction.1.bt2")).st_size,
+            'name': 'subtraction.1.bt2',
+            'type': 'bowtie2'
+        },
+        {
+            'size': os.stat(os.path.join(path, "subtraction.fa.gz")).st_size,
+            'name': 'subtraction.fa.gz',
+            'type': 'fasta'
+        }
+    ]
