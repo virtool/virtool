c8a6a6475820f59d07af32f69547600b49729cff
Blake <blakeinvictoria@gmail.com>
2021-02-17 16:08:33 -0800

    Perform initializations on startup.

--- jobs_api_process.py
+++ jobs_api_process.py
@@ -7,13 +7,14 @@ import virtool.http.csp
 import virtool.http.errors
 import virtool.http.proxy
 import virtool.http.query
+import virtool.startup
 
 
 async def start_aiohttp_server():
     middlewares = [
-        virtool.http.csp.middleware,
-        virtool.http.auth.middleware,
         virtool.http.accept.middleware,
+        virtool.http.auth.middleware,
+        virtool.http.csp.middleware,
         virtool.http.errors.middleware,
         virtool.http.proxy.middleware,
         virtool.http.query.middleware
@@ -22,6 +23,15 @@ async def start_aiohttp_server():
     app = aiohttp.web.Application(middlewares=middlewares)
 
     aiojobs.aiohttp.setup(app)
+
+    app.on_startup += [
+        virtool.startup.init_check_db,
+        virtool.startup.init_db,
+        virtool.startup.init_paths,
+        virtool.startup.init_postgres,
+        virtool.startup.init_settings,
+        virtool.startup.init_version,
+    ]
 
 
 async def run(**kwargs):
