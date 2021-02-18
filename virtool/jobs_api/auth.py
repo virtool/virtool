5cdbbc739e22412f489478efc8054418df9562ab
Blake <blakeinvictoria@gmail.com>
2021-02-18 13:34:04 -0800

    Decode Authorization header.

--- virtool/jobs_api/auth.py
+++ virtool/jobs_api/auth.py
@@ -2,6 +2,8 @@ from typing import Callable, Awaitable
 
 import aiohttp.web
 
+from virtool.api.response import unauthorized
+
 RouteHandler = Callable[[aiohttp.web.Request], Awaitable[aiohttp.web.Response]]
 
 PUBLIC_ROUTES = [("PATCH", "/api/jobs")]
@@ -11,3 +13,10 @@ PUBLIC_ROUTES = [("PATCH", "/api/jobs")]
 def job_authentication(request: aiohttp.web.Request, handler: RouteHandler):
     if (request.method, request.path) in PUBLIC_ROUTES:
         return await handler(request)
+
+    try:
+        auth_header = request.headers["AUTHORIZATION"]
+        job_id, key = aiohttp.BasicAuth.decode(auth_header)
+    except KeyError:
+        return unauthorized("No authorization header.")
+   
