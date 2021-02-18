f11d4d37f6a33a49015e247fc9b2c1c96e5d9529
Ian Boyes <igboyes@gmail.com>
2021-02-18 14:13:46 -0800

    Rename Message class to Change
    * Changes are processed by the dispatcher. Messages are the data that is sent to clients.
    * Add `target` property to get dot notated interface and operation
    * Add pretty `__repr__` method
    * Add Operation type hint to operation argument
    * Add docstring

---
+++ tests/dispatcher/test_change.py
@@ -0,0 +1,24 @@
+from virtool.dispatcher.change import Change
+from virtool.dispatcher.operations import UPDATE
+
+
+def test_change():
+    """
+    Make sure a change object has the required attributes and string representation.
+
+    """
+    change = Change(
+        "jobs",
+        UPDATE,
+        ["foo", "bar"]
+    )
+
+    assert change.interface == "jobs"
+    assert change.operation == UPDATE
+    assert change.id_list == ["foo", "bar"]
+
+    assert str(change) == (
+        '<Change interface="jobs" operation="update id_list="[\'foo\', \'bar\']">'
+    )
+
+    assert change.target == "jobs.update"
