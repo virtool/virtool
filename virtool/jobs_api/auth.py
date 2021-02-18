c699e46333daaa4cacf3cf6f3e97f9304ea183ab
Blake <blakeinvictoria@gmail.com>
2021-02-18 13:22:35 -0800

    Whitelist public routes.

--- virtool/jobs_api/auth.py
+++ virtool/jobs_api/auth.py
@@ -4,7 +4,10 @@ import aiohttp.web
 
 RouteHandler = Callable[[aiohttp.web.Request], Awaitable[aiohttp.web.Response]]
 
+PUBLIC_ROUTES = [("PATCH", "/api/jobs")]
+
 
 @aiohttp.web.middleware
 def job_authentication(request: aiohttp.web.Request, handler: RouteHandler):
-    ...
+    if (request.method, request.path) in PUBLIC_ROUTES:
+        return await handler(request)
