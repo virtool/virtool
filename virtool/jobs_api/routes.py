2c92705211c92c06ab635d92569d608388b04dab
Blake <blakeinvictoria@gmail.com>
2021-02-18 16:48:19 -0800

    Aiojobs startup functions must be coroutines.

--- virtool/jobs_api/routes.py
+++ virtool/jobs_api/routes.py
@@ -4,10 +4,8 @@ import virtool.analyses.api
 import virtool.indexes.api
 
 
-def init_routes(app: aiohttp.web.Application):
+async def init_routes(app: aiohttp.web.Application):
     """Add routes to jobs API."""
     for routes in (virtool.analyses.api.job_routes,
                    virtool.indexes.api.job_routes):
         app.add_routes(routes)
-
-    return app
