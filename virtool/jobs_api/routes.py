dc8ebcb703aaf408257a88d887e60ddc2d572b25
Blake <blakeinvictoria@gmail.com>
2021-02-19 12:36:40 -0800

    Add job routes from all virtool API modules.
    Re-use `virtool.routes.ROUTES`.

--- virtool/jobs_api/routes.py
+++ virtool/jobs_api/routes.py
@@ -2,10 +2,10 @@ import aiohttp.web
 
 import virtool.analyses.api
 import virtool.indexes.api
+import virtool.routes
 
 
 async def init_routes(app: aiohttp.web.Application):
     """Add routes to jobs API."""
-    for routes in (virtool.analyses.api.job_routes,
-                   virtool.indexes.api.job_routes):
-        app.add_routes(routes)
+    for routes in virtool.routes.ROUTES:
+        app.add_routes(routes.job_routes)
