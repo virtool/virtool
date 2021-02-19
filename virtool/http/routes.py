b15489fd29958e1094c240e3feedb1cd086ea12f
Blake <blakeinvictoria@gmail.com>
2021-02-19 10:00:14 -0800

    Use `@_protected` to wrap aiohttp `RouteTableDef` methods.

--- virtool/http/routes.py
+++ virtool/http/routes.py
@@ -22,34 +22,11 @@ class Routes(aiohttp.web.RouteTableDef):
 
     def __init__(self):
         super().__init__()
-
-    def get(self, *args, admin=False, allow_jobs=False, permission=None,
-            public=False, **kwargs):
-        route_decorator = super().get(*args, **kwargs)
-        return protect(route_decorator, admin, permission, public)
-
-    def post(self, *args, admin=False, permission=None,
-             public=False, **kwargs):
-        route_decorator = super().post(*args, **kwargs)
-        return protect(route_decorator, admin, permission, public)
-
-    def patch(self, *args, admin=False, allow_jobs=False, jobs_only=False, permission=None,
-              public=False,
-              schema=None, **kwargs):
-        route_decorator = super().patch(*args, **kwargs)
-        return protect(route_decorator, admin, allow_jobs, jobs_only, permission, public, schema)
-
-    def put(self, *args, admin=False, allow_jobs=False, jobs_only=False, permission=None,
-            public=False, schema=None,
-            **kwargs):
-        route_decorator = super().put(*args, **kwargs)
-        return protect(route_decorator, admin, allow_jobs, jobs_only, permission, public, schema)
-
-    def delete(self, *args, admin=False, allow_jobs=False, jobs_only=False, permission=None,
-               public=False,
-               schema=None, **kwargs):
-        route_decorator = super().delete(*args, **kwargs)
-        return protect(route_decorator, admin, allow_jobs, jobs_only, permission, public, schema)
+        self.get = self._protected(self.get)
+        self.post = self._protected(self.post)
+        self.delete = self._protected(self.delete)
+        self.put = self._protected(self.put)
+        self.patch = self._protected(self.patch)
 
 
 def protect(
