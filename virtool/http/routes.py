1b7787637a56d3f269fba2f07920d400cd6b9705
Blake <blakeinvictoria@gmail.com>
2021-02-24 13:27:13 -0800

    Remove `routes` method.

--- virtool/http/routes.py
+++ virtool/http/routes.py
@@ -1,12 +1,10 @@
 from functools import wraps
-from typing import Callable, Any
+from typing import Callable
 
 import aiohttp.web
-from aiohttp.web_routedef import RouteDef
 
 import virtool.users.utils
 from virtool.api.response import json_response, unauthorized
-from virtool.types import RouteHandler
 
 
 class Routes(aiohttp.web.RouteTableDef):
@@ -28,42 +26,6 @@ class Routes(aiohttp.web.RouteTableDef):
         self.patch = self._protected(self.patch)
 
         self.job_routes = []
-
-    def route(self,
-              method: str,
-              path: str,
-              jobs_only: bool = False,
-              allow_jobs: bool = True,
-              **kwargs: Any) -> Callable[[RouteHandler], RouteHandler]:
-        """
-        Create a decorator which registers an aiohttp route.
-
-        This function is called by :class:`RouteTableDef`'s method functions, such as :func:`.get`. Any keyword
-        arguments passed to those functions will be forwarded here, allowing the `jobs_only` and `allow_jobs`
-        flags to be handled.
-
-        :param method: The name of the method type. Constants for these names are contained in `aiohttp.hdrs` and
-            are prefixed with `METH_`.
-        :param path: The path of the route.
-        :param jobs_only: If True, the route will be added to the :obj:`.job_routes` attribute only.
-        :param allow_jobs: If True,  the route will be added to the route table, and to :obj:`.job_routes`.
-        :param kwargs: Any keyword arguments are passed to the `RouteDef` object.
-        :return: A decorator which adds the :class:`RouteHandler` to the route table, and/or :obj:`.job_routes`
-        """
-
-        if jobs_only:
-            def _route_decorator(handler: RouteHandler):
-                self.job_routes.append(RouteDef(method, path, handler, {}))
-                return handler
-        elif allow_jobs:
-            def _route_decorator(handler: RouteHandler):
-                self.job_routes.append(RouteDef(method, path, handler, {}))
-                super(Routes, self).route(method, path, **kwargs)(handler)
-                return handler
-        else:
-            _route_decorator = super(Routes, self).route(method, path, **kwargs)
-
-        return _route_decorator
 
 
 def protect(
