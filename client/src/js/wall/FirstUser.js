882cf84821e753fa31501a83b4012912d3791a59
Joshua Moreira <joshuamoreira3@gmail.com>
2021-02-26 14:56:14 -0600

    Updated formatting

--- client/src/js/wall/FirstUser.js
+++ client/src/js/wall/FirstUser.js
@@ -2,13 +2,20 @@ import { noop } from "lodash-es";
 import React from "react";
 import { connect } from "react-redux";
 import { Formik, Form, Field, ErrorMessage } from "formik";
-import { BoxGroupHeader, BoxGroupSection, Button, Input, InputGroup, InputLabel, InputError, PasswordInput } from "../base";
+import {
+    BoxGroupHeader,
+    BoxGroupSection,
+    Button,
+    Input,
+    InputGroup,
+    InputLabel,
+    InputError,
+    PasswordInput
+} from "../base";
 import { createFirstUser } from "../users/actions";
 import { WallContainer, WallDialog, WallDialogFooter } from "./Container";
 
-
-export const FirstUser = ({onSubmit}) => {
-
+export const FirstUser = ({ onSubmit }) => {
     const initialValues = {
         username: "",
         password: ""
@@ -23,46 +30,44 @@ export const FirstUser = ({onSubmit}) => {
 
         const minimum_password_length = 8;
 
-        if(values.password.length < minimum_password_length)
-            errors.password = "password should be at least 8 characters long"
-        
+        if (values.password.length < minimum_password_length)
+            errors.password = "password should be at least 8 characters long";
+
         return errors;
     };
 
     return (
-            <WallContainer>
-                <WallDialog size="lg">
-                    <BoxGroupHeader>
-                        <h2>Setup User</h2>
-                        <p>Create an initial administrative user to start using Virtool.</p>
-                    </BoxGroupHeader>
-                    <Formik initialValues={initialValues} validate={validate} onSubmit={handleSubmit}>
-                        <Form>
-                            <BoxGroupSection>
-                                <InputGroup>
-                                    <InputLabel>Username</InputLabel>
-                                    <Field type="text" name="username" as={Input}/>
-                                </InputGroup>
-                                <InputGroup>
-                                    <InputLabel>Password</InputLabel>
-                                    <Field name="password" as={PasswordInput}/>
-                                </InputGroup>
-                            </BoxGroupSection>
-                            <WallDialogFooter>
-                                <Button type="submit" icon="user-plus" color="blue">
-                                    Create User
-                                </Button>
-                                <ErrorMessage name="password">
-                                    {
-                                        errorMessage => { return <InputError>{errorMessage}</InputError> }
-                                    }
-                                </ErrorMessage>
-                            </WallDialogFooter>
-                        </Form>
-                    </Formik>
-                </WallDialog>
-            </WallContainer>
-        );
+        <WallContainer>
+            <WallDialog size="lg">
+                <BoxGroupHeader>
+                    <h2>Setup User</h2>
+                    <p>Create an initial administrative user to start using Virtool.</p>
+                </BoxGroupHeader>
+                <Formik initialValues={initialValues} validate={validate} onSubmit={handleSubmit}>
+                    <Form>
+                        <BoxGroupSection>
+                            <InputGroup>
+                                <InputLabel>Username</InputLabel>
+                                <Field type="text" name="username" as={Input} />
+                            </InputGroup>
+                            <InputGroup>
+                                <InputLabel>Password</InputLabel>
+                                <Field name="password" as={PasswordInput} />
+                            </InputGroup>
+                        </BoxGroupSection>
+                        <WallDialogFooter>
+                            <Button type="submit" icon="user-plus" color="blue">
+                                Create User
+                            </Button>
+                            <ErrorMessage name="password">
+                                {errorMessage => <InputError>{errorMessage}</InputError>}
+                            </ErrorMessage>
+                        </WallDialogFooter>
+                    </Form>
+                </Formik>
+            </WallDialog>
+        </WallContainer>
+    );
 };
 
 export const mapDispatchToProps = dispatch => ({
