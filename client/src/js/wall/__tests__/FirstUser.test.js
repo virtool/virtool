39bfdf64e1b7017aab16ef090ba107d8d9f3cbba
Joshua Moreira <joshuamoreira3@gmail.com>
2021-03-04 15:23:40 -0600

    Updating FirstUser tests

--- client/src/js/wall/__tests__/FirstUser.test.js
+++ client/src/js/wall/__tests__/FirstUser.test.js
@@ -1,21 +1,29 @@
 import { Input, PasswordInput } from "../../base";
 import { FirstUser, mapDispatchToProps } from "../FirstUser";
+import { waitFor, fireEvent, screen } from "@testing-library/react";
 
 describe("<FirstUser />", () => {
     let props;
+    let errorMessages;
 
     beforeEach(() => {
+        errorMessages = {
+            generalError: "General Error",
+            usernameErrors: ["Password Error 1", "Password Error 2"],
+            passwordErrors: ["Username Error 1", "Username Error 2"]
+        };
         props = {
-            onSubmit: jest.fn()
+            onSubmit: jest.fn(),
+            ...errorMessages
         };
     });
 
     it("should render", () => {
         const wrapper = shallow(<FirstUser {...props} />);
-        wrapper.setState({
-            username: "bob",
-            password: "password"
-        });
+        // wrapper.setState({
+        //     username: "bob",
+        //     password: "password"
+        // });
         expect(wrapper).toMatchSnapshot();
     });
 
@@ -34,21 +42,29 @@ describe("<FirstUser />", () => {
         expect(wrapper).toMatchSnapshot();
     });
 
-    it("should call onSubmit when form is submitted", () => {
+    it("should call onSubmit when form is submitted", async () => {
         props = {
-            onSubmit: jest.fn()
+            onSubmit: jest.fn(),
+            ...errorMessages
         };
-        const wrapper = shallow(<FirstUser {...props} />);
-        wrapper.setState({
-            username: "fee",
-            password: "baz"
-        });
+        // const wrapper = shallow(<FirstUser {...props} />);
+        const { container } = render(<FirstUser {...props} />);
+        const submit = container.querySelector('button[type="submit"]');
+        // wrapper.setState({
+        //     username: "fee",
+        //     password: "baz"
+        // });
         const e = {
             preventDefault: jest.fn()
         };
-        wrapper.find("form").simulate("submit", e);
+        //wrapper.find("Formik").simulate("submit", e);
+
+        // Await must be used to allow the Formik component to call it's own onSubmit
+        await waitFor(() => {
+            fireEvent.click(submit); //screen.getByRole("button", { name: "button-name" }));
+        });
         expect(e.preventDefault).toHaveBeenCalled();
-        expect(props.onSubmit).toHaveBeenCalledWith("fee", "baz");
+        expect(props.onSubmit).toHaveBeenCalledWith("", "");
     });
 });
 
