fe6e28c4765a0f79b108635555a1a8845fb21440
Jake Alexander <jake.ale@outlook.com>
2021-03-05 10:48:03 -0800

    Add `SampleArtifacts` model

--- virtool/samples/models.py
+++ virtool/samples/models.py
@@ -1,6 +1,39 @@
-from sqlalchemy import Column, Integer, String, DateTime
+import enum
+
+from sqlalchemy import Column, Integer, String, DateTime, Enum
 
 from virtool.postgres import Base
+
+
+class ArtifactType(str, enum.Enum):
+    """
+    Enumerated type for possible artifact types
+
+    """
+
+    sam = "sam"
+    bam = "bam"
+    fasta = "fasta"
+    fastq = "fastq"
+    csv = "csv"
+    tsv = "tsv"
+    json = "json"
+
+
+class SampleArtifacts(Base):
+    """
+    SQL model to store sample artifacts
+
+    """
+    __tablename__ = "sample_artifacts"
+
+    id = Column(Integer, primary_key=True)
+    sample = Column(String)
+    name = Column(String)
+    name_on_disk = Column(String)
+    size = Column(Integer)
+    type = Column(Enum(ArtifactType))
+    uploaded_at = Column(DateTime)
 
 
 class SampleReadsFile(Base):
