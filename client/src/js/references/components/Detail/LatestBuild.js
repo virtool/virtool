c6f4e77897c3b90dd74138aef1618867bdf65b36
Joshua Moreira <joshuamoreira3@gmail.com>
2021-03-10 11:44:40 -0600

    Added download link and updated styling of LatestBuild

--- client/src/js/references/components/Detail/LatestBuild.js
+++ client/src/js/references/components/Detail/LatestBuild.js
@@ -2,23 +2,37 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import { BoxGroupSection, Icon, NoneFoundSection, RelativeTime } from "../../../base";
+import styled from "styled-components";
+
+const StyledLatestBuild = styled(BoxGroupSection)`
+    align-items: center;
+    display: flex;
+
+    a {
+        margin-left: auto;
+    }
+`;
 
 export const LatestBuild = ({ id, latestBuild }) => {
+    console.log("LatestBuild = ", latestBuild);
     if (latestBuild) {
         return (
-            <BoxGroupSection>
-                <strong>
-                    <Link to={`/refs/${id}/indexes/${latestBuild.id}`}>Index {latestBuild.version}</Link>
-                </strong>
-                <span>
-                    &nbsp;/ Created <RelativeTime time={latestBuild.created_at} /> by {latestBuild.user.id}
-                </span>
-                <span style={{ float: "right" }}>
-                    <a href="https://google.ca">
-                        <Icon color="blue" faStyle="fas" fixedWidth={false} name="download" tip="Download" />
+            <StyledLatestBuild>
+                <div>
+                    <strong>
+                        <Link to={`/refs/${id}/indexes/${latestBuild.id}`}>Index {latestBuild.version}</Link>
+                    </strong>
+                    <span>
+                        &nbsp;/ Created <RelativeTime time={latestBuild.created_at} /> by {latestBuild.user.id}
+                    </span>
+                </div>
+                {
+                    //Uncomment line below once
+                    /*latestBuild.has_json && */ <a href={`/download/indexes/:${latestBuild.id}`} download>
+                        Download Index
                     </a>
-                </span>
-            </BoxGroupSection>
+                }
+            </StyledLatestBuild>
         );
     }
 
