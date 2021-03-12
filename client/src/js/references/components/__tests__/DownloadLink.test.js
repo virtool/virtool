d2725db24656c3ccc0021d136a5edb2aab59bcd9
Joshua Moreira <joshuamoreira3@gmail.com>
2021-03-12 12:55:14 -0600

    Removed redundant test

--- client/src/js/references/components/__tests__/DownloadLink.test.js
+++ client/src/js/references/components/__tests__/DownloadLink.test.js
@@ -3,16 +3,10 @@ import { screen } from "@testing-library/react";
 
 describe("<DownloadLink />", () => {
     const props = {
-        onSelect: jest.fn(),
         id: 1
     };
 
     it("should render", () => {
-        const wrapper = shallow(<DownloadLink {...props} />);
-        expect(wrapper).toMatchSnapshot();
-    });
-
-    it("should download the correct link when clicked", () => {
         renderWithProviders(<DownloadLink {...props} />);
         const link = screen.getByText("Download Index").closest("a");
         expect(link).toHaveAttribute("href", `/download/indexes/${props.id}`);
