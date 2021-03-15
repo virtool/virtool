77cd81423c750c31727cc123d2650420a90e5dc1
Jake Alexander <jake.ale@outlook.com>
2021-03-15 11:32:46 -0700

    Add `SampleArtifactCache` and `SampleReadsFileCache` SQL models

--- virtool/samples/models.py
+++ virtool/samples/models.py
@@ -20,10 +20,10 @@ class ArtifactType(str, SQLEnum):
 
 class SampleArtifact(Base):
     """
-    SQL model to store sample artifacts
+    SQL model to store a sample artifact
 
     """
-    __tablename__ = "sample_artifacts"
+    __tablename__ = "sample_artifact"
 
     id = Column(Integer, primary_key=True)
     sample = Column(String, nullable=False)
@@ -32,6 +32,11 @@ class SampleArtifact(Base):
     size = Column(Integer)
     type = Column(Enum(ArtifactType), nullable=False)
     uploaded_at = Column(DateTime)
+
+    def __repr__(self):
+        return f"<SampleArtifact(id={self.id}, sample={self.sample}, name={self.name}, " \
+               f"name_on_disk={self.name_on_disk}, size={self.size}, type={self.type}, " \
+               f"uploaded_at={self.uploaded_at}"
 
 
 class SampleReadsFile(Base):
@@ -50,4 +55,44 @@ class SampleReadsFile(Base):
 
     def __repr__(self):
         return f"<SampleReadsFile(id={self.id}, sample={self.sample}, name={self.name}, " \
+               f"name_on_disk={self.name_on_disk}, size={self.size}, uploaded_at={self.uploaded_at})>"
+
+
+class SampleArtifactCache(Base):
+    """
+    SQL model to store a cached sample artifact
+
+    """
+    __tablename__ = "sample_artifact_cache"
+
+    id = Column(Integer, primary_key=True)
+    sample = Column(String, nullable=False)
+    name = Column(String, nullable=False)
+    name_on_disk = Column(String)
+    size = Column(Integer)
+    type = Column(Enum(ArtifactType), nullable=False)
+    uploaded_at = Column(DateTime)
+
+    def __repr__(self):
+        return f"<SampleArtifactCache(id={self.id}, sample={self.sample}, name={self.name}, " \
+               f"name_on_disk={self.name_on_disk}, size={self.size}, type={self.type}, " \
+               f"uploaded_at={self.uploaded_at}"
+
+    
+class SampleReadsFileCache(Base):
+    """
+    SQL model to store cached sample reads files
+
+    """
+    __tablename__ = "sample_reads_files_cache"
+
+    id = Column(Integer, primary_key=True)
+    sample = Column(String, nullable=False)
+    name = Column(String(length=13), nullable=False)
+    name_on_disk = Column(String, nullable=False)
+    size = Column(Integer)
+    uploaded_at = Column(DateTime)
+
+    def __repr__(self):
+        return f"<SampleReadsFileCache(id={self.id}, sample={self.sample}, name={self.name}, " \
                f"name_on_disk={self.name_on_disk}, size={self.size}, uploaded_at={self.uploaded_at})>"
