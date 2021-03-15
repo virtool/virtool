5161f07240e9ed991b269c29de88e989dea88012
Jake Alexander <jake.ale@outlook.com>
2021-03-15 13:17:51 -0700

    Add `POST /api/samples/:id/caches/:key/artifacts`

--- virtool/samples/files.py
+++ virtool/samples/files.py
@@ -2,46 +2,55 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession
 
 import virtool.utils
-from virtool.samples.models import SampleArtifact, SampleReadsFile
+from virtool.samples.models import SampleArtifact, SampleReadsFile, SampleArtifactCache
 
 
 async def get_existing_reads(pg: AsyncEngine, sample_id: str):
     async with AsyncSession(pg) as session:
-        query = await session.execute(select(SampleReadsFile).filter_by(sample=sample_id))
+        query = await session.execute(
+            select(SampleReadsFile).filter_by(sample=sample_id)
+        )
 
     return [result.name for result in query.scalars().all()]
 
 
-async def create_artifact_file(pg: AsyncEngine, name: str, sample_id: str, artifact_type: str):
+async def create_artifact_file(
+    pg: AsyncEngine, name: str, sample: str, artifact_type: str, cache: bool = False
+):
     """
-    Create a row in the `sample_artifact` SQL table that represents uploaded sample artifact files.
+    Create a row in an SQL table that represents uploaded sample artifact files. A row is created in either the
+    `sample_artifact` or `sample_artifact_cache` table depending on the condition of `cached`.
 
     :param pg: PostgreSQL AsyncEngine object
     :param name: Name of the sample artifact file
-    :param sample_id: ID that corresponds to a parent sample
+    :param sample: ID that corresponds to a parent sample
     :param artifact_type: Type of artifact to be uploaded
+    :param cache: Whether the row should be created in the `sample_artifact` or `sample_artifact_cache` table
     :return: A dictionary representation of the newly created row
     """
     async with AsyncSession(pg) as session:
-        artifact_file = SampleArtifact(
-            sample=sample_id,
-            name=name,
-            type=artifact_type
-        )
+        if not cache:
+            artifact = SampleArtifact()
+        else:
+            artifact = SampleArtifactCache()
+        
+        artifact.sample, artifact.name, artifact.type = sample, name, artifact_type
 
-        session.add(artifact_file)
+        session.add(artifact)
         await session.flush()
 
-        artifact_file.name_on_disk = f"{artifact_file.id}-{artifact_file.name}"
+        artifact.name_on_disk = f"{artifact.id}-{artifact.name}"
 
-        artifact_file = artifact_file.to_dict()
+        artifact = artifact.to_dict()
 
         await session.commit()
 
-        return artifact_file
+        return artifact
 
 
-async def create_reads_file(pg: AsyncEngine, size: int, name: str, name_on_disk: str, sample_id: str):
+async def create_reads_file(
+    pg: AsyncEngine, size: int, name: str, name_on_disk: str, sample_id: str
+):
     """
     Create one or two rows in the `samples_reads_files` SQL table that represents uploaded sample reads files.
 
@@ -59,7 +68,7 @@ async def create_reads_file(pg: AsyncEngine, size: int, name: str, name_on_disk:
             name=name,
             name_on_disk=name_on_disk,
             size=size,
-            uploaded_at=virtool.utils.timestamp()
+            uploaded_at=virtool.utils.timestamp(),
         )
 
         session.add(reads_file)
