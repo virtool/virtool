44bb0c8f49c3c0bdf33f0ea3ac817d7bb612e94c
Sean <s7782519728@gmail.com>
2021-03-17 14:19:34 -0700

    Add Jobs API for downloading sample artfacts

--- virtool/samples/api.py
+++ virtool/samples/api.py
@@ -8,6 +8,8 @@ from pathlib import Path
 import aiohttp.web
 from aiohttp.web_fileresponse import FileResponse
 from cerberus import Validator
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 
 import virtool.analyses.db
 import virtool.analyses.utils
@@ -832,7 +834,7 @@ async def upload_artifact(req):
 
 
 @routes.jobs_api.get("/api/samples/{sample_id}/reads/{suffix}")
-async def download_reads(req):
+async def download_reads(req: aiohttp.web.Request):
     """
     Download the sample reads file.
 
@@ -854,6 +856,44 @@ async def download_reads(req):
         return not_found()
 
     file_path = os.path.join(req.app["settings"]["data_path"], "samples", sample_id, file_name)
+
+    if not os.path.isfile(file_path):
+        return virtool.api.response.not_found()
+
+    file_stats = virtool.utils.file_stats(file_path)
+
+    headers = {
+        "Content-Length": file_stats["size"],
+        "Content-Type": "application/gzip"
+    }
+
+    return FileResponse(file_path, chunk_size=1024 * 1024, headers=headers)
+
+
+@routes.jobs_api.get("/api/samples/{sample_id}/artifacts/{filename}")
+async def download_artifact(req: aiohttp.web.Request):
+    """
+    Download the sample artifact.
+
+    """
+    db = req.app["db"]
+    pg = req.app["pg"]
+
+    sample_id = req.match_info["sample_id"]
+    filename = req.match_info["filename"]
+
+    if not await db.samples.find_one(sample_id):
+        return not_found()
+
+    async with AsyncSession(pg) as session:
+        result = (await session.execute(select(SampleArtifact).filter_by(sample=sample_id, name=filename))).scalar()
+
+    if not result:
+        return not_found()
+
+    artifact = result.to_dict()
+
+    file_path = os.path.join(req.app["settings"]["data_path"], "samples", sample_id, artifact["name_on_disk"])
 
     if not os.path.isfile(file_path):
         return virtool.api.response.not_found()
