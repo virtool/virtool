a64ed9e5e20c2a42030d5c02cf2162cf767fe4f3
Sean <s7782519728@gmail.com>
2021-03-30 13:12:14 -0700

    Add tests for attach_files()

--- tests/indexes/test_db.py
+++ tests/indexes/test_db.py
@@ -5,6 +5,7 @@ import virtool.indexes
 import virtool.indexes.db
 import virtool.errors
 import virtool.jobs.build_index
+from virtool.indexes.models import IndexFile
 
 
 @pytest.mark.parametrize("exists", [True, False])
@@ -183,3 +184,49 @@ async def test_update_last_indexed_versions(dbi, test_otu, spawn_client):
     document = await client.db.otus.find_one({"reference.id": "hxn167"})
 
     assert document["last_indexed_version"] == document["version"]
+
+
+async def test_attach_files(pg, pg_session):
+    index_1 = IndexFile(id=1, name="reference.1.bt2", index="foo", type="bowtie2", size=1234567)
+    index_2 = IndexFile(id=2, name="reference.2.bt2", index="foo", type="bowtie2", size=1234567)
+
+    async with pg_session as session:
+        session.add_all([index_1, index_2])
+        await session.commit()
+
+    document = {
+        "_id": "foo",
+        "reference": {
+            "id": "bar"
+        },
+        "user": {
+            "id": "test"
+        }
+    }
+
+    document = await virtool.indexes.db.attach_files(pg, document)
+    assert document == {
+        '_id': 'foo',
+        'reference': {
+            'id': 'bar'
+        },
+        'user': {
+            'id': 'test'
+        },
+        'files': [
+            {
+                'id': 1,
+                'name': 'reference.1.bt2',
+                'index': 'foo',
+                'type': 'bowtie2',
+                'size': 1234567
+            },
+            {
+                'id': 2,
+                'name': 'reference.2.bt2',
+                'index': 'foo',
+                'type': 'bowtie2',
+                'size': 1234567
+            }
+        ]
+    }
