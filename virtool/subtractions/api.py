3ec5172ddd43db96dd5cf299e7d5000dea85cbf0
Jake Alexander <jake.ale@outlook.com>
2021-04-07 10:28:55 -0700

    Stop writing `subtraction.files` when subtraction files are uploaded (#2213)
    * Change`prepare_files_field` to `get_subtractions_files`. Using `get` instead of `attach` since the function only returns a list of files instead of returning an updated dictionary.
    * Stop updating `subtraction.files` when a subtraction file is uploaded

--- virtool/subtractions/api.py
+++ virtool/subtractions/api.py
@@ -99,7 +99,7 @@ async def get(req):
         return not_found()
 
     document["linked_samples"] = await virtool.subtractions.db.get_linked_samples(db, subtraction_id)
-    document["files"] = await virtool.subtractions.utils.prepare_files_field(pg, subtraction_id)
+    document["files"] = await virtool.subtractions.utils.get_subtraction_files(pg, subtraction_id)
 
     return json_response(virtool.utils.base_processor(document))
 
@@ -239,12 +239,6 @@ async def upload(req):
 
     subtraction_file = await virtool.uploads.db.finalize(pg, size, upload_id, SubtractionFile)
 
-    await db.subtraction.find_one_and_update({"_id": subtraction_id}, {
-        "$push": {
-            "files": upload_id
-        }
-    })
-
     headers = {
         "Location": f"/api/subtractions/{subtraction_id}/files/{file_name}"
     }
@@ -295,8 +289,13 @@ async def edit(req):
     if document is None:
         return not_found()
 
-    document["linked_samples"] = await virtool.subtractions.db.get_linked_samples(db, subtraction_id)
-    document["files"] = await virtool.subtractions.utils.prepare_files_field(pg, subtraction_id)
+    files, linked_samples = await asyncio.gather(*[virtool.subtractions.utils.get_subtraction_files(pg, subtraction_id),
+                                                   virtool.subtractions.db.get_linked_samples(db, subtraction_id)])
+
+    document.update({
+        "files": files,
+        "linked_samples": linked_samples
+    })
 
     return json_response(virtool.utils.base_processor(document))
 
@@ -340,10 +339,11 @@ async def finalize_subtraction(req: aiohttp.web.Request):
     updated_document = await db.subtraction.find_one_and_update({"_id": subtraction_id}, {
         "$set": {
             "gc": data["gc"],
-            "ready": True,
-            "files": await virtool.subtractions.utils.prepare_files_field(pg, subtraction_id)
+            "ready": True
         }
     })
+
+    updated_document["files"] = await virtool.subtractions.utils.get_subtraction_files(pg, subtraction_id)
 
     return json_response(virtool.utils.base_processor(updated_document))
 
