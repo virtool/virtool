d82089b3f9bf7520c50793c87961762e34c18be7
Tiansheng Sui <s7782519728@gmail.com>
2021-04-07 15:55:17 -0700

    Create fake reference when running in fake (#2218)

--- tests/dev/test_fake.py
+++ tests/dev/test_fake.py
@@ -2,12 +2,22 @@ from os.path import isdir
 
 import pytest
 
-from virtool.dev.fake import create_fake_data_path, create_fake_user, create_fake_jobs, create_fake_analysis
+from virtool.dev.fake import create_fake_data_path, create_fake_user, create_fake_jobs, create_fake_analysis, \
+    create_fake_references
 
 
 @pytest.fixture
 def app(dbi, pg):
-    return {"db": dbi, "pg": pg}
+    return {
+        "db": dbi,
+        "pg": pg,
+        "settings": {
+            "default_source_types": [
+                "isolate",
+                "strain"
+            ]
+        }
+    }
 
 
 def test_create_fake_data_path():
@@ -32,3 +42,11 @@ async def test_create_fake_job(snapshot, app, dbi):
     document["status"][0]["timestamp"] = None
     snapshot.assert_match(document)
 
+
+async def test_create_fake_references(snapshot, app, dbi, static_time):
+    await create_fake_references(app)
+
+    document = await dbi.references.find_one({"_id": "reference_1"})
+    document["created_at"] = static_time.datetime
+
+    snapshot.assert_match(document)
