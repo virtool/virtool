47faf0a2690edce5d303540d6f38ad1c4a79ff33
Sean <s7782519728@gmail.com>
2021-04-09 10:56:50 -0700

    Add optional 'index_id' argument for virtool.indexes.db.create

--- virtool/indexes/db.py
+++ virtool/indexes/db.py
@@ -5,7 +5,7 @@ Work with indexes in the database.
 import asyncio
 import asyncio.tasks
 import typing
-from typing import Union
+from typing import Optional, Union
 
 import pymongo
 from sqlalchemy.ext.asyncio import AsyncEngine
@@ -45,16 +45,18 @@ FILES = (
 )
 
 
-async def create(db, ref_id: str, user_id: str) -> dict:
+async def create(db, ref_id: str, user_id: str, index_id: Optional[str] = None) -> dict:
     """
     Create a new index and update history to show the version and id of the new index.
 
     :param db: the application database client
     :param ref_id: the ID of the reference to create index for
     :param user_id: the ID of the current user
+    :param index_id: the ID of the index
+
     :return: the new index document
     """
-    index_id = await virtool.db.utils.get_new_id(db.indexes)
+    index_id = index_id or await virtool.db.utils.get_new_id(db.indexes)
 
     index_version = await get_next_version(db, ref_id)
 
