7b1a6b3d436a94649fe2a7f890e0cc365ef795f5
Jake Alexander <jake.ale@outlook.com>
2021-04-09 12:06:02 -0700

    Don't subclass Faker
    Co-authored-by: Ian Boyes <igboyes@gmail.com>

--- virtool/faker/wrapper.py
+++ virtool/faker/wrapper.py
@@ -1,10 +1,12 @@
 from faker import Faker
 from faker.providers import misc
 
-class FakerWrapper(Faker):
-    fake = Faker()
-    Faker.seed(0)
-    fake.add_provider(misc)
+class FakerWrapper:
+
+    def __init__(self):
+        self.fake = Faker()
+        self.fake.seed_instance(0)
+        self.fake.add_provider(misc)
 
     def get_mongo_id(self):
         return self.fake.password(length=8, special_chars=False)
