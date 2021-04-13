12aa2abc2669a3e69778cf36cf46796726d1d7cd
Blake Smith <blakeinvictoria@gmail.com>
2021-04-13 13:40:48 -0700

    Use snapshot to confirm faked data is identical.

--- tests/samples/test_fake.py
+++ tests/samples/test_fake.py
@@ -6,7 +6,7 @@ from virtool.samples.db import LIST_PROJECTION
 
 
 @pytest.mark.parametrize("paired", [True, False])
-async def test_create_fake_unpaired(paired, dbi, pg):
+async def test_create_fake_unpaired(paired, dbi, pg, snapshot):
     app = {"db": dbi, "fake": FakerWrapper(), "pg": pg}
 
     fake_sample = await create_fake_sample(app, paired)
@@ -15,3 +15,9 @@ async def test_create_fake_unpaired(paired, dbi, pg):
         assert key in fake_sample
 
     assert len(fake_sample["files"]) == (2 if paired else 1)
+
+    del fake_sample["created_at"]
+    for f in fake_sample["files"]:
+        del f["uploaded_at"]
+
+    snapshot.assert_match(fake_sample)
