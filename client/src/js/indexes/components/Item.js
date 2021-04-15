bef2e78ca9be4bb3067c1722c0234675cf3cfe8c
Ian Boyes <igboyes@gmail.com>
2021-04-15 14:56:02 -0700

    Fixed appearance of index list items

--- client/src/js/indexes/components/Item.js
+++ client/src/js/indexes/components/Item.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { connect } from "react-redux";
 import styled from "styled-components";
-import { getFontSize, getFontWeight } from "../../app/theme";
+import { fontWeight, getFontSize } from "../../app/theme";
 import { Attribution, Icon, LinkBox, Loader } from "../../base";
 import { getActiveIndexId } from "../selectors";
 
-export const IndexItemChangeDescription = ({ changeCount, modifiedCount }) => {
+const StyledIndexItemDescription = styled.span`
+    font-weight: ${fontWeight.normal};
+`;
+
+export const IndexItemDescription = ({ changeCount, modifiedCount }) => {
     if (changeCount === null) {
         return null;
     }
@@ -15,36 +19,24 @@ export const IndexItemChangeDescription = ({ changeCount, modifiedCount }) => {
     }
 
     return (
-        <span>
+        <StyledIndexItemDescription>
             {changeCount} change{changeCount === 1 ? "" : "s"} made in {modifiedCount} OTU
             {modifiedCount === 1 ? "" : "s"}
-        </span>
+        </StyledIndexItemDescription>
     );
 };
 
 const IndexItemTop = styled.h3`
     display: flex;
     font-size: ${getFontSize("lg")};
-    margin: 0;
-
-    > strong {
-        flex: 1 0 120px;
-        font-weight: ${getFontWeight("thick")};
-    }
-
-    > span {
-        flex: 1 0 auto;
-        font-weight: ${getFontWeight("normal")};
-    }
-
-    > div {
-        flex: 1 0 auto;
-    }
+    font-weight: ${fontWeight.thick};
+    margin: 0 0 5px;
 `;
 
 const StyledIndexItemIcon = styled.div`
     margin-left: auto;
     text-align: right;
+    width: 85px;
 `;
 
 export const IndexItemIcon = ({ activeId, id, ready }) => {
@@ -52,7 +44,7 @@ export const IndexItemIcon = ({ activeId, id, ready }) => {
         if (id === activeId) {
             return (
                 <StyledIndexItemIcon>
-                    <Icon name="check" color="green" /> <strong>Active</strong>
+                    <Icon name="check" color="green" /> <span>Active</span>
                 </StyledIndexItemIcon>
             );
         }
@@ -63,19 +55,22 @@ export const IndexItemIcon = ({ activeId, id, ready }) => {
     return (
         <StyledIndexItemIcon>
             <Loader size="14px" color="primary" />
-            <strong> Building</strong>
+            <span> Building</span>
         </StyledIndexItemIcon>
     );
 };
 
+const IndexItemVersion = styled.strong`
+    flex: 1 0 auto;
+    font-weight: ${fontWeight.thick};
+    max-width: 40%;
+`;
+
 export const IndexItem = ({ activeId, document, refId }) => (
     <LinkBox to={`/refs/${refId}/indexes/${document.id}`}>
         <IndexItemTop>
-            <strong>Version {document.version}</strong>
-            <IndexItemChangeDescription
-                changeCount={document.change_count}
-                modifiedCount={document.modified_otu_count}
-            />
+            <IndexItemVersion>Version {document.version}</IndexItemVersion>
+            <IndexItemDescription changeCount={document.change_count} modifiedCount={document.modified_otu_count} />
             <IndexItemIcon activeId={activeId} id={document.id} ready={document.ready} />
         </IndexItemTop>
         <Attribution time={document.created_at} user={document.user.id} />
