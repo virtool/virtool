1de458c317275535d7f90cffcea1363fdbb54e06
Ian Boyes <igboyes@gmail.com>
2021-04-23 15:13:14 -0700

    Add sample sidebar with label selector
    * Use popover for selector dropdown

--- client/src/js/samples/components/Detail/General.js
+++ client/src/js/samples/components/Detail/General.js
@@ -3,12 +3,20 @@ import React from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-import { BoxGroup, BoxGroupHeader, Table, Markdown, NarrowContainer } from "../../../base";
+import { BoxGroup, BoxGroupHeader, Markdown, NarrowContainer, SideContainer, Table } from "../../../base";
 import { getLibraryTypeDisplayName } from "../../utils";
 import EditSample from "../Edit/Edit";
 import SampleFileSizeWarning from "./FileSizeWarning.js";
+import { Sidebar } from "./Sidebar/Sidebar";
 
-const StyledSampleDetailGeneral = styled(NarrowContainer)`
+const SampleDetailSidebarContainer = styled(SideContainer)`
+    padding-left: 15px;
+`;
+
+const StyledSampleDetailGeneral = styled.div`
+    align-items: stretch;
+    display: flex;
+
     th {
         width: 220px;
     }
@@ -29,97 +37,103 @@ export const SampleDetailGeneral = ({
     subtraction
 }) => (
     <StyledSampleDetailGeneral>
-        <SampleFileSizeWarning />
-        <BoxGroup>
-            <BoxGroupHeader>
-                <h2>General</h2>
-                <p>User-defined information about the sample.</p>
-            </BoxGroupHeader>
-            <Table>
-                <tbody>
-                    <tr>
-                        <th>Name</th>
-                        <td>{name}</td>
-                    </tr>
-                    <tr>
-                        <th>Host</th>
-                        <td>{host}</td>
-                    </tr>
-                    <tr>
-                        <th>Isolate</th>
-                        <td>{isolate}</td>
-                    </tr>
-                    <tr>
-                        <th>Locale</th>
-                        <td>{locale}</td>
-                    </tr>
-                </tbody>
-            </Table>
-        </BoxGroup>
+        <NarrowContainer>
+            <SampleFileSizeWarning />
+            <BoxGroup>
+                <BoxGroupHeader>
+                    <h2>General</h2>
+                    <p>User-defined information about the sample.</p>
+                </BoxGroupHeader>
+                <Table>
+                    <tbody>
+                        <tr>
+                            <th>Name</th>
+                            <td>{name}</td>
+                        </tr>
+                        <tr>
+                            <th>Host</th>
+                            <td>{host}</td>
+                        </tr>
+                        <tr>
+                            <th>Isolate</th>
+                            <td>{isolate}</td>
+                        </tr>
+                        <tr>
+                            <th>Locale</th>
+                            <td>{locale}</td>
+                        </tr>
+                    </tbody>
+                </Table>
+            </BoxGroup>
 
-        <BoxGroup>
-            <BoxGroupHeader>
-                <h2>Library</h2>
-                <p>Information about the sequencing reads in this sample.</p>
-            </BoxGroupHeader>
-            <Table>
-                <tbody>
-                    <tr>
-                        <th>Encoding</th>
-                        <td>{encoding}</td>
-                    </tr>
-                    <tr>
-                        <th>Read Count</th>
-                        <td>{count}</td>
-                    </tr>
-                    <tr>
-                        <th>Library Type</th>
-                        <td>{libraryType}</td>
-                    </tr>
-                    <tr>
-                        <th>Length Range</th>
-                        <td>{lengthRange}</td>
-                    </tr>
-                    <tr>
-                        <th>GC Content</th>
-                        <td>{gc}</td>
-                    </tr>
-                    <tr>
-                        <th>Paired</th>
-                        <td>{paired ? "Yes" : "No"}</td>
-                    </tr>
-                </tbody>
-            </Table>
-        </BoxGroup>
+            <BoxGroup>
+                <BoxGroupHeader>
+                    <h2>Library</h2>
+                    <p>Information about the sequencing reads in this sample.</p>
+                </BoxGroupHeader>
+                <Table>
+                    <tbody>
+                        <tr>
+                            <th>Encoding</th>
+                            <td>{encoding}</td>
+                        </tr>
+                        <tr>
+                            <th>Read Count</th>
+                            <td>{count}</td>
+                        </tr>
+                        <tr>
+                            <th>Library Type</th>
+                            <td>{libraryType}</td>
+                        </tr>
+                        <tr>
+                            <th>Length Range</th>
+                            <td>{lengthRange}</td>
+                        </tr>
+                        <tr>
+                            <th>GC Content</th>
+                            <td>{gc}</td>
+                        </tr>
+                        <tr>
+                            <th>Paired</th>
+                            <td>{paired ? "Yes" : "No"}</td>
+                        </tr>
+                    </tbody>
+                </Table>
+            </BoxGroup>
 
-        <BoxGroup>
-            <BoxGroupHeader>
-                <h2>Default Subtraction</h2>
-                <p>This subtraction will be the default selection when creating an analysis.</p>
-            </BoxGroupHeader>
-            <Table>
-                <tbody>
-                    <tr>
-                        <th>Subtraction</th>
-                        <td>
-                            {subtraction ? (
-                                <Link to={`/subtraction/${subtraction.id}`}>{subtraction.name}</Link>
-                            ) : (
-                                "None"
-                            )}
-                        </td>
-                    </tr>
-                </tbody>
-            </Table>
-        </BoxGroup>
+            <BoxGroup>
+                <BoxGroupHeader>
+                    <h2>Default Subtraction</h2>
+                    <p>This subtraction will be the default selection when creating an analysis.</p>
+                </BoxGroupHeader>
+                <Table>
+                    <tbody>
+                        <tr>
+                            <th>Subtraction</th>
+                            <td>
+                                {subtraction ? (
+                                    <Link to={`/subtraction/${subtraction.id}`}>{subtraction.name}</Link>
+                                ) : (
+                                    "None"
+                                )}
+                            </td>
+                        </tr>
+                    </tbody>
+                </Table>
+            </BoxGroup>
 
-        <BoxGroup>
-            <BoxGroupHeader>
-                <h2>Notes</h2>
-                <p>Additional notes about the sample.</p>
-            </BoxGroupHeader>
-            <Markdown markdown={notes} />
-        </BoxGroup>
+            <BoxGroup>
+                <BoxGroupHeader>
+                    <h2>Notes</h2>
+                    <p>Additional notes about the sample.</p>
+                </BoxGroupHeader>
+                <Markdown markdown={notes} />
+            </BoxGroup>
+        </NarrowContainer>
+
+        <SampleDetailSidebarContainer>
+            <Sidebar />
+        </SampleDetailSidebarContainer>
 
         <EditSample />
     </StyledSampleDetailGeneral>
