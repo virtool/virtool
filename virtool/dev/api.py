9fd241c4fe02d964dc81af2286dd788f3bddf9ae
Ian Boyes <igboyes@gmail.com>
2021-05-12 17:11:15 -0700

    Fix handling of create_sample command on dev API endpoint
    This was completely broken.

--- virtool/dev/api.py
+++ virtool/dev/api.py
@@ -3,7 +3,7 @@ from logging import getLogger
 from virtool.api.response import no_content
 from virtool.fake.wrapper import FakerWrapper
 from virtool.http.routes import Routes
-from virtool.samples.fake import create_fake_samples
+from virtool.samples.fake import create_fake_sample
 from virtool.subtractions.fake import create_fake_fasta_upload, create_fake_finalized_subtraction
 from virtool.utils import random_alphanumeric
 
@@ -42,6 +42,12 @@ async def dev(req):
         )
 
     if command == "create_sample":
-        await create_fake_samples(req.app)
+        await create_fake_sample(
+            req.app,
+            random_alphanumeric(8),
+            req["client"].user_id,
+            False,
+            True
+        )
 
     return no_content()
