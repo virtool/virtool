1c74eb3aaba1069ea421b043259b50ba630f3119
Elizabeth <eroberts9789@gmail.com>
2021-07-12 16:09:39 -0700

    Remove create_subtraction.py job module

--- virtool/jobs/classes.py
+++ virtool/jobs/classes.py
@@ -1,14 +1,12 @@
 import virtool.jobs.aodp
 import virtool.jobs.build_index
 import virtool.jobs.create_sample
-import virtool.jobs.create_subtraction
 import virtool.jobs.nuvs
 import virtool.jobs.pathoscope
 
 TASK_CREATORS = {
     "aodp": virtool.jobs.aodp.create,
     "build_index": virtool.jobs.build_index.create,
-    "create_subtraction": virtool.jobs.create_subtraction.create,
     "create_sample": virtool.jobs.create_sample.create,
     "nuvs": virtool.jobs.nuvs.create,
     "pathoscope_bowtie": virtool.jobs.pathoscope.create,
