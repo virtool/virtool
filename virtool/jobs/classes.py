8a853335393f19d186941736302bc5b0e780c951
Elizabeth <eroberts9789@gmail.com>
2021-07-13 09:29:24 -0700

    Remove create_sample.py job module * Remove related tests

--- virtool/jobs/classes.py
+++ virtool/jobs/classes.py
@@ -1,6 +1,5 @@
 import virtool.jobs.aodp
 import virtool.jobs.build_index
-import virtool.jobs.create_sample
 import virtool.jobs.create_subtraction
 import virtool.jobs.nuvs
 import virtool.jobs.pathoscope
@@ -9,7 +8,6 @@ TASK_CREATORS = {
     "aodp": virtool.jobs.aodp.create,
     "build_index": virtool.jobs.build_index.create,
     "create_subtraction": virtool.jobs.create_subtraction.create,
-    "create_sample": virtool.jobs.create_sample.create,
     "nuvs": virtool.jobs.nuvs.create,
     "pathoscope_bowtie": virtool.jobs.pathoscope.create,
 }
