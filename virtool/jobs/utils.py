69f4b09f15718cc3655cbc847e61fe2b9d7f305f
Elizabeth <eroberts9789@gmail.com>
2021-07-14 09:36:48 -0700

    Remove imports only used in create_sample.py * Remove get_sample_params() * Remove copy_or_compress() * Remove copy_files_to_sample() * Remove related tests

--- virtool/jobs/utils.py
+++ virtool/jobs/utils.py
@@ -146,75 +146,8 @@ class JobRights:
         return rights_dict
 
 
-def copy_files_to_sample(paths: list, sample_path: str, proc: int) -> list:
-    sizes = list()
-
-    for index, path in enumerate(paths):
-        suffix = index + 1
-        target = virtool.samples.utils.join_read_path(sample_path, suffix)
-
-        virtool.jobs.utils.copy_or_compress(path, target, proc)
-
-        stats = virtool.utils.file_stats(target)
-
-        sizes.append(stats["size"])
-
-    return sizes
-
-
-def copy_or_compress(path: str, target: str, proc: int):
-    """
-    Copy the file at `path` to `target`. Compress the file on-the-fly if it is not already compressed.
-
-    This function will make use of `pigz` for compression. Pass a `proc` number to assign workers to the `pigz` process.
-
-    :param path: the path to copy from
-    :param target: the path to copy to
-    :param proc: the number of worker processes to allow for pigz
-
-    """
-    if virtool.utils.is_gzipped(path):
-        shutil.copyfile(path, target)
-    else:
-        virtool.utils.compress_file(path, target, processes=proc)
-
-
 def copy_or_decompress(path: Path, target: str, proc: int):
     if virtool.utils.is_gzipped(path):
         virtool.utils.decompress_file(path, target, proc)
     else:
         shutil.copyfile(path, target)
-
-
-async def get_sample_params(db, settings: dict, task_args: dict) -> dict:
-    """
-    Return a `dict` of parameters that can be assigned to `self.params` in the `create_sample` job.
-
-    This function should be called in :method:`~virtool.job.Job.check_db`.
-
-    :param db: the job database client
-    :param settings: the application settings
-    :param task_args: the job's `task_args`
-    :return: a dict of job params
-
-    """
-    params = dict(task_args)
-
-    sample_id = params["sample_id"]
-
-    sample_path = os.path.join(
-        settings["data_path"],
-        "samples",
-        sample_id
-    )
-
-    document = await db.samples.find_one(sample_id)
-
-    params.update({
-        "sample_path": sample_path,
-        "document": document,
-        "files": document["files"],
-        "paired": document["paired"]
-    })
-
-    return params
