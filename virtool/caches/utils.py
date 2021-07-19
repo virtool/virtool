5ff4938b519b5e1ea935a6d1655690cda1bc2860
Elizabeth <eroberts9789@gmail.com>
2021-07-19 09:36:23 -0700

    Remove unused code from modules imported to analysis.py jobs module * Remove caches.utils.join_cache_read_paths()

--- virtool/caches/utils.py
+++ virtool/caches/utils.py
@@ -3,9 +3,7 @@ Utilities used for working with cache files within analysis workflows.
 
 """
 from pathlib import Path
-from typing import Any, Dict, List, Optional
-
-import virtool.samples.utils
+from typing import Any, Dict
 
 
 def join_cache_path(settings: Dict[str, Any], cache_id: str) -> Path:
@@ -18,22 +16,3 @@ def join_cache_path(settings: Dict[str, Any], cache_id: str) -> Path:
 
     """
     return settings["data_path"] / "caches" / cache_id
-
-
-def join_cache_read_paths(settings: Dict[str, Any], cache: dict) -> Optional[List[Path]]:
-    """
-    Return a list of read paths for a cache given the application settings and the cache document.
-
-    The path list will contain two paths if paired, and one if not.
-
-    :param settings: the application settings
-    :param cache: a cache document
-    :return: a list of read paths
-
-    """
-    if not cache:
-        return None
-
-    cache_path = join_cache_path(settings, cache["id"])
-
-    return virtool.samples.utils.join_read_paths(cache_path, cache["paired"])
