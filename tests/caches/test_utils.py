26810cb55e1eb0cc00b9fdc529426edc735ab25b
eroberts9789 <82687748+eroberts9789@users.noreply.github.com>
2021-07-19 10:23:57 -0700

    Remove analysis.py jobs module (#2326)
    * Remove analysis.py job module
    * Remove immediate imports only used in this module
    * Remove related tests
    * Remove unused code from modules imported to analysis.py jobs module
    * Remove caches.utils.join_cache_read_paths()

--- tests/caches/test_utils.py
+++ tests/caches/test_utils.py
@@ -11,36 +11,3 @@ def test_join_cache_path(tmp_path):
     cache_id = "bar"
 
     assert virtool.caches.utils.join_cache_path(settings, cache_id) == tmp_path / "caches" / "bar"
-
-
-@pytest.mark.parametrize("paired,paths", [
-    (True, ["caches/bar/reads_1.fq.gz", "caches/bar/reads_2.fq.gz"]),
-    (False, ["caches/bar/reads_1.fq.gz"])
-])
-@pytest.mark.parametrize("exists", [True, False])
-def test_join_cache_read_paths(paired, paths, exists, tmp_path):
-    """
-    Test that the correct read paths are returned when the data is paired or unpaired and when the cache is not defined.
-
-    """
-    settings = {
-        "data_path": tmp_path
-    }
-
-    paths = [tmp_path / path for path in paths]
-
-    cache = None
-
-    if exists:
-        cache = {
-            "id": "bar",
-            "paired": paired
-        }
-
-    result = virtool.caches.utils.join_cache_read_paths(settings, cache)
-
-    if exists:
-        assert result == paths
-        return
-
-    assert result is None
