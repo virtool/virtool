3ae3ce56fccc27e5e678199515b822d08056048d
Joshua Moreira <joshuamoreira3@gmail.com>
2021-08-06 11:50:22 -0500

    Removed unnecessary code and added onUpdate function

--- client/src/js/samples/components/Detail/Sidebar/Subtractions.js
+++ client/src/js/samples/components/Detail/Sidebar/Subtractions.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import styled from "styled-components";
-import { routerLocationHasState } from "../../../../utils/utils";
 import { getDefaultSubtractions, getSampleDetailId } from "../../../selectors";
 import { SampleLabel } from "../../Label";
 import { SidebarHeader } from "./Header";
@@ -9,20 +8,18 @@ import { SampleLabelsSelector } from "./Selector";
 
 // TODO: add a add/remove default subtraction function (If necessary)
 
-const SampleLabelsList = styled.div`
-    display: flex;
-    flex-flow: wrap;
-`;
-
 const InlineSampleLabel = styled(SampleLabel)`
     background-color: ${props => props.theme.color.white};
     display: inline;
     margin: 0 5px 5px 0;
 `;
 
-export const DefaultSubtractions = ({ defaultSubtractions, sampleId, subtractionOptions }) => {
-    // useEffect(onListLabels, [sampleId]);
+const SampleLabelsList = styled.div`
+    display: flex;
+    flex-flow: wrap;
+`;
 
+export const DefaultSubtractions = ({ defaultSubtractions, sampleId, subtractionOptions, onUpdate }) => {
     const sampleLabelComponents = defaultSubtractions.map(subtraction => (
         <InlineSampleLabel key={subtraction.id} name={subtraction.name} />
     ));
@@ -34,16 +31,12 @@ export const DefaultSubtractions = ({ defaultSubtractions, sampleId, subtraction
                 allLabels={subtractionOptions}
                 sampleLabels={defaultSubtractions}
                 sampleId={sampleId}
+                onUpdate={onUpdate}
             />
             <SampleLabelsList>{sampleLabelComponents}</SampleLabelsList>
         </React.Fragment>
     );
 };
-
-// export const mapStateToProps = state => ({
-//     sampleId: getSampleDetailId(state),
-//     sampleLabels: getSampleLabels(state)
-// });
 
 export const mapStateToProps = state => ({
     defaultSubtractions: getDefaultSubtractions(state),
@@ -51,10 +44,10 @@ export const mapStateToProps = state => ({
     subtractionOptions: state.subtraction.shortlist
 });
 
-// export const mapDispatchToProps = dispatch => ({
-//     onListLabels: () => {
-//         dispatch(listLabels());
-//     }
-// });
+export const mapDispatchToProps = dispatch => ({
+    onUpdate: (sampleId, subtractions) => {
+        dispatch(editSample(sampleId, { subtractions }));
+    }
+});
 
 export default connect(mapStateToProps)(DefaultSubtractions);
