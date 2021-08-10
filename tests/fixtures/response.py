4731a4f9d67ac08d8ce368df9465deed726fa560
Lilly <eroberts9789@gmail.com>
2021-08-10 09:21:27 -0700

    Remove no_content() from AssertRespIs * No longer used since 204 error codes are now checked directly in tests

--- tests/fixtures/response.py
+++ tests/fixtures/response.py
@@ -116,14 +116,6 @@ class AssertRespIs:
         }
 
     @staticmethod
-    async def no_content(resp):
-        """
-        Check whether a response object is a valid Virtool ``no_content``.
-
-        """
-        assert resp.status == 204
-
-    @staticmethod
     async def bad_request(resp, message="Bad request"):
         """
         Check whether a response object is a valid Virtool ``bad_request``.
