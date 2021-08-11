52156e13cb672cb69ed1d440d1072f6ad36a338c
Lilly <eroberts9789@gmail.com>
2021-08-11 13:54:30 -0700

    Update virtool.http.errors.middleware to return a web.Response object with a JSON body

--- virtool/http/errors.py
+++ virtool/http/errors.py
@@ -1,38 +1,33 @@
 from typing import Callable
-
 from aiohttp import web
+from aiohttp.web_exceptions import HTTPNotFound
 
-import virtool.templates
-import virtool.utils
-from virtool.api.response import not_found
+from virtool.api.response import json_response
+from virtool.templates import setup_template_env
+from virtool.utils import get_static_hash
 
 
 @web.middleware
 async def middleware(req: web.Request, handler: Callable):
-    is_api_call = req.path.startswith("/api")
-
     try:
-        response = await handler(req)
+        resp = await handler(req)
+        return resp
 
-        if not is_api_call and response.status == 404:
-            return handle_404(req)
-
-        return response
+    except HTTPNotFound:
+        return handle_404(req)
 
     except web.HTTPException as ex:
-        if is_api_call:
-            return not_found()
-
-        if ex.status == 404:
-            return handle_404(req)
-
-        raise
+        data = {
+            "id": "_".join(ex.reason.lower().split(" ")),
+            "message": ex.text
+        }
+        return json_response(data, ex.status)
 
 
 def handle_404(req: web.Request):
-    template = virtool.templates.setup_template_env.get_template("error_404.html")
+    template = setup_template_env.get_template("error_404.html")
 
-    static_hash = virtool.utils.get_static_hash(req)
+    static_hash = get_static_hash(req)
 
     html = template.render(hash=static_hash)
 
