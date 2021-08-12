b750e727a164f01cf791e0769f2a482f52d65468
Lilly <eroberts9789@gmail.com>
2021-08-12 11:33:38 -0700

    Determine whether 404 exception is an API call or not in the except block of errors.middleware * Now html template is used for cases where the request path doesn't start with "/api" * Write test to ensure correct behaviour for handling 404 exceptions

--- virtool/http/errors.py
+++ virtool/http/errors.py
@@ -9,15 +9,15 @@ from virtool.utils import get_static_hash
 
 @web.middleware
 async def middleware(req: web.Request, handler: Callable):
+
     try:
         resp = await handler(req)
-
-        if not req.path.startswith("/api") and resp.status == 404:
-            return handle_404(req)
-
         return resp
 
     except web.HTTPException as ex:
+        if not req.path.startswith("/api") and ex.status == 404:
+            return handle_404(req)
+
         data = {
             "id": "_".join(ex.reason.lower().split(" ")),
             "message": ex.text
