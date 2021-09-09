085ede4ba18c5920a5a20e6fa1943205ba192e05
Ian Boyes <igboyes@gmail.com>
2021-09-09 11:47:48 -0700

    Remove unused import from Sidebar.js

--- client/src/js/samples/components/Detail/Sidebar/Sidebar.js
+++ client/src/js/samples/components/Detail/Sidebar/Sidebar.js
@@ -1,6 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-import { Box } from "../../../../base";
 import SampleLabels from "./Labels";
 import DefaultSubtractions from "./Subtractions";
 
