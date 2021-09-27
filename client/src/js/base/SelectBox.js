8a9b0dee4dd7f3296a561706b01d544a724b5527
reecehoffmann <reecehoffmann@outlook.com>
2021-09-27 14:27:25 -0700

    Possible fix for VIR-376

--- client/src/js/base/SelectBox.js
+++ client/src/js/base/SelectBox.js
@@ -1,10 +1,15 @@
 import styled from "styled-components";
 import { Box } from "./Box";
+import {Button} from "./Button";
 
-export const SelectBox = styled(Box)`
+export const SelectBox = styled(Button)`
     border: 1px ${props => (props.active ? props.theme.color.blue : props.theme.color.greyLight)} solid;
     border-radius: ${props => props.theme.borderRadius.sm};
     box-shadow: ${props => (props.active ? props.theme.boxShadow.inset : "none")};
+    background: white;
+    width: 100%;
+    text-align: center;
+    white-space: normal;
 
     div {
         font-weight: ${props => props.theme.fontWeight.thick};
