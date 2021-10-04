b7c1103878fb8988818dee895619eb29e95a224d
ReeceHoffmann <59776400+ReeceHoffmann@users.noreply.github.com>
2021-10-04 09:25:14 -0700

    Make base SelectBox component accessible
    * Enables tabbing by using `button` element instead of `div`.

--- client/src/js/base/SelectBox.js
+++ client/src/js/base/SelectBox.js
@@ -1,11 +1,15 @@
 import styled from "styled-components";
 import { Box } from "./Box";
 
-export const SelectBox = styled(Box)`
+export const SelectBox = styled(Box).attrs(() => ({
+    type: "button",
+    as: "button"
+}))`
     border: 1px ${props => (props.active ? props.theme.color.blue : props.theme.color.greyLight)} solid;
     border-radius: ${props => props.theme.borderRadius.sm};
     box-shadow: ${props => (props.active ? props.theme.boxShadow.inset : "none")};
-
+    background-color: ${props => props.theme.color.white};
+    text-align: left;
     div {
         font-weight: ${props => props.theme.fontWeight.thick};
         padding-bottom: 5px;
