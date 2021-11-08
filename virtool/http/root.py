9a70bec466923548997759fb79646f7f6d3e93aa
Lilly <eroberts9789@gmail.com>
2021-11-08 13:19:06 -0800

    feat: return dev mode value in root api response
    Update documentation links in virtool.http.root.
    
    Add test to ensure that dev is included in the response from the GET /api request handler.

--- virtool/http/root.py
+++ virtool/http/root.py
@@ -16,64 +16,66 @@ async def get(req):
     app_data = {"endpoints": {
             "account": {
                 "url": "/api/account",
-                "doc": f"{API_URL_ROOT}_account.html"
+                "doc": f"{API_URL_ROOT}/account"
             },
             "analyses": {
                 "url": "/api/analyses",
-                "doc": f"{API_URL_ROOT}_analyses.html"
+                "doc": f"{API_URL_ROOT}/analyses"
             },
             "genbank": {
                 "url": "/api/genbank",
-                "doc": f"{API_URL_ROOT}_genbank.html"
+                "doc": f"{API_URL_ROOT}/genbank"
             },
             "groups": {
                 "url": "/api/groups",
-                "doc": f"{API_URL_ROOT}_groups.html"
+                "doc": f"{API_URL_ROOT}/groups"
             },
             "history": {
                 "url": "/api/history",
-                "doc": f"{API_URL_ROOT}_history.html"
+                "doc": f"{API_URL_ROOT}/history"
             },
             "hmm": {
                 "url": "/api/hmm",
-                "doc": f"{API_URL_ROOT}_hmm.html"
+                "doc": f"{API_URL_ROOT}/hmm"
             },
             "indexes": {
                 "url": "/api/indexes",
-                "doc": f"{API_URL_ROOT}_indexes.html"
+                "doc": f"{API_URL_ROOT}/indexes"
             },
             "jobs": {
                 "url": "/api/jobs",
-                "doc": f"{API_URL_ROOT}_jobs.html"
+                "doc": f"{API_URL_ROOT}/jobs"
             },
             "otus": {
                 "url": "/api/otus",
-                "doc": f"{API_URL_ROOT}_otus.html"
+                "doc": f"{API_URL_ROOT}/otus"
             },
             "references": {
                 "url": "/api/references",
-                "doc": f"{API_URL_ROOT}_references.html"
+                "doc": f"{API_URL_ROOT}/refs"
             },
             "samples": {
                 "url": "/api/samples",
-                "doc": f"{API_URL_ROOT}_samples.html"
+                "doc": f"{API_URL_ROOT}/samples"
             },
             "settings": {
                 "url": "/api/settings",
-                "doc": f"{API_URL_ROOT}_settings.html"
+                "doc": f"{API_URL_ROOT}/settings"
             },
             "subtraction": {
                 "url": "/api/subtraction",
-                "doc": f"{API_URL_ROOT}_subtraction.html"
+                "doc": f"{API_URL_ROOT}/subtraction"
             },
             "tasks": {
                 "url": "/api/tasks",
-                "doc": f"{API_URL_ROOT}_tasks.html"
+                "doc": f"{API_URL_ROOT}/tasks"
             },
             "users": {
                 "url": "/api/users",
-                "doc": f"{API_URL_ROOT}_users.html"
-            }}}
+                "doc": f"{API_URL_ROOT}/users"
+            }},
+        "dev": req.app["config"].dev
+    }
 
     try:
         app_data["version"] = req.app["version"]
