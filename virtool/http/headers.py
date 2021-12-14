d5648a31b84e9190fcbc2d1829e156ced91e5308
Lilly <eroberts9789@gmail.com>
2021-12-14 14:26:08 -0800

    refactor: prefix Content-Location headers with base url
    Add tests.

--- virtool/http/headers.py
+++ virtool/http/headers.py
@@ -18,10 +18,15 @@ async def headers_middleware(req, handler):
 
 async def on_prepare_location(req: Request, resp: Response):
     """
-    Signal handler that adds base URL to Location header if possible
+    Signal handler that adds base URL to Location header or Content-Location if possible
     """
     location = resp.headers.get("Location")
     base_url = req.app["config"].base_url
 
     if location and base_url not in location:
         resp.headers["Location"] = base_url + location
+
+    content_location = resp.headers.get("Content-Location")
+
+    if content_location and base_url not in content_location:
+        resp.headers["Content-Location"] = base_url + content_location
