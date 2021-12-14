99ef181e9ce415c8928309ac8dfd40a5c95feda6
Lilly <eroberts9789@gmail.com>
2021-12-14 14:26:08 -0800

    refactor: prefix Content-Location headers with base url
    Add tests.

---
+++ tests/http/test_headers.py
@@ -0,0 +1,20 @@
+from aiohttp.web_request import Request
+from aiohttp.web_response import Response
+from aiohttp.web_routedef import RouteTableDef
+
+test_routes = RouteTableDef()
+
+
+@test_routes.get("/foo")
+def public_test_route(request: Request):
+    headers = {"Location": "/foo", "Content-Location": "/bar"}
+    return Response(status=200, headers=headers)
+
+
+async def test_on_prepare_location_location(spawn_client):
+    client = await spawn_client(base_url="foobar", authorize=True, addon_route_table=test_routes)
+
+    resp = await client.get("/foo")
+
+    assert resp.headers["Location"] == "foobar/foo"
+    assert resp.headers["Content-Location"] == "foobar/bar"
