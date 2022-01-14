970968171b8104fa6619f2e805881a5b0bdb9158
Ian Boyes <igboyes@gmail.com>
2022-01-13 16:58:09 -0800

    refactor: use Type[Base] instead of Base for type hints
    * This is the correct way to implement this.
    * Prevents warnings in IDE.

--- virtool/labels/db.py
+++ virtool/labels/db.py
@@ -1,7 +1,9 @@
 from typing import Dict, Any
 
+from virtool.types import Document
 
-async def attach_sample_count(db, document: dict) -> Dict[str, Any]:
+
+async def attach_sample_count(db, document: dict) -> Document:
     """
     Attach the number of samples associated with the given label to the passed document.
 
