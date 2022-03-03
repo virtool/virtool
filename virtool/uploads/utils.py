c32c78d6ef674f99071d74426212a8e647163040
Ian Boyes <igboyes@gmail.com>
2022-03-03 14:26:01 -0800

    fix: introduce potential fix for slow uploads
    Increase file upload chunk size.
    It was previously 1 KB. Increasing it should increase upload performance.

--- virtool/uploads/utils.py
+++ virtool/uploads/utils.py
@@ -1,7 +1,7 @@
 import os
 import pathlib
 from logging import getLogger
-from typing import Callable, Optional, Any
+from typing import Any, Callable, Optional
 
 import aiofiles
 from aiohttp.web_request import Request
@@ -9,13 +9,15 @@ from cerberus import Validator
 
 logger = getLogger(__name__)
 
-CHUNK_SIZE = 4096
-WRITE_BUFFER_LENGTH = 10000
+CHUNK_SIZE = 1024 * 1000 * 50
 
 
 def is_gzip_compressed(chunk: bytes):
     """
-    Check if a file is gzip compressed by checking the first two bytes for the gzip magic number.
+    Check if a file is gzip compressed.
+
+    Peek at the first two bytes for the gzip magic number and raise and exception if it
+    is not present.
 
     :param chunk: First byte chunk from a file being uploaded
     :raises OSError: An OSError is raised when the file is not gzip-compressed
@@ -27,7 +29,7 @@ def is_gzip_compressed(chunk: bytes):
 
 def naive_validator(req) -> Validator.errors:
     """
-    Validate `name` given in a HTTP request using cerberus
+    Validate `name` given in an HTTP request using cerberus
 
     """
     v = Validator({"name": {"type": "string", "required": True}}, allow_unknown=True)
@@ -45,8 +47,8 @@ async def naive_writer(
     Write a new file from a HTTP multipart request.
 
     :param req: aiohttp request object
-    :param path: A complete path (including filename) to where the file should be written
-    :param on_first_chunk: A function to call with the first chunk of the file stream
+    :param path: the file path to write the data to
+    :param on_first_chunk: a function to call with the first chunk of the file stream
     :return: size of the new file in bytes
     """
     reader = await req.multipart()
@@ -59,9 +61,7 @@ async def naive_writer(
     except FileExistsError:
         pass
 
-    buffer = list()
-
-    async with aiofiles.open(path, "wb") as handle:
+    async with aiofiles.open(path, "wb") as f:
         while True:
             chunk = await file.read_chunk(CHUNK_SIZE)
 
@@ -71,14 +71,8 @@ async def naive_writer(
             if size == 0 and on_first_chunk:
                 on_first_chunk(chunk)
 
-            buffer.append(chunk)
+            await f.write(chunk)
 
             size += len(chunk)
-
-            if len(buffer) > WRITE_BUFFER_LENGTH:
-                continue
-
-            await handle.write(b"".join(buffer))
-            buffer = list()
 
     return size
