b120bd78408591b9da59659eb762ad828effc89a
Ryan <ryanfang11@gmail.com>
2022-07-15 16:33:00 -0700

    feat: generate oas using customized version

---
+++ virtool/custom_oas/oas/__main__.py
@@ -0,0 +1,10 @@
+import argparse
+
+from virtool.custom_oas.oas.cmd import setup
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Generate Open API Specification")
+    setup(parser)
+    args = parser.parse_args()
+    args.func(args)
