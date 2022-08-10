c404b2245c098e95f94afc1519cc37bdc2e22c4b
Ian Boyes <igboyes@gmail.com>
2022-08-10 16:17:50 -0700

    chore: remove unused data_model module

--- virtool/data_model/subtraction.py
+++
@@ -1,7 +0,0 @@
-from virtool_core.models.subtraction import Subtraction, SubtractionMinimal
-
-VirtoolSubtraction = Subtraction
-"""Internal model for Subtraction used in Virtool."""
-
-VirtoolSubtractionMinimal = SubtractionMinimal
-"""Internal minimal model for Subtraction used in Virtool."""
