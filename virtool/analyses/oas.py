b6d11c8597bd9063ab21ca842709bc85f37cecc6
Aman monga <aman260@outlook.com>
2022-09-28 12:45:45 -0700

    chore: fix modelling for finding analyses

--- virtool/analyses/oas.py
+++ virtool/analyses/oas.py
@@ -1,28 +1,35 @@
-from virtool_core.models.analysis import AnalysisMinimal, Analysis
+from virtool_core.models.analysis import Analysis, AnalysisSearchResult
 
 
-class GetAnalysisResponse(AnalysisMinimal):
+class FindAnalysesResponse(AnalysisSearchResult):
     class Config:
         schema_extra = {
-            "example": [
-                {
-                    "created_at": "2022-08-15T17:42:35.979000Z",
-                    "id": "ofv7rp4v",
-                    "index": {"id": "u3lm1rk8", "version": 14},
-                    "job": {"id": "us3toy8j"},
-                    "ready": True,
-                    "reference": {"id": "d19exr83", "name": "New Plant Viruses"},
-                    "sample": {"id": "7tu8c5m5"},
-                    "subtractions": [{"id": "1sk885at", "name": "Vitis vinifera"}],
-                    "updated_at": "2022-08-15T17:42:35.979000Z",
-                    "user": {
-                        "administrator": True,
-                        "handle": "mrott",
-                        "id": "ihvze2u9",
-                    },
-                    "workflow": "pathoscope_bowtie",
-                }
-            ]
+            "example": {
+                "documents": [
+                    {
+                        "created_at": "2022-08-15T17:42:35.979000Z",
+                        "id": "ofv7rp4v",
+                        "index": {"id": "u3lm1rk8", "version": 14},
+                        "job": {"id": "us3toy8j"},
+                        "ready": True,
+                        "reference": {"id": "d19exr83", "name": "New Plant Viruses"},
+                        "sample": {"id": "7tu8c5m5"},
+                        "subtractions": [{"id": "1sk885at", "name": "Vitis vinifera"}],
+                        "updated_at": "2022-08-15T17:42:35.979000Z",
+                        "user": {
+                            "administrator": True,
+                            "handle": "mrott",
+                            "id": "ihvze2u9",
+                        },
+                        "workflow": "pathoscope_bowtie",
+                    }
+                ],
+                "found_count": 2621,
+                "page": 1,
+                "page_count": 105,
+                "per_page": 25,
+                "total_count": 2621,
+            }
         }
 
 
