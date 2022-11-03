d84d54d22816afa18f80597095d442df0ad8232a
Ryan Fang <104466198+ryanfang5@users.noreply.github.com>
2022-11-03 09:51:51 -0700

    feat!: remove hmm purge endpoint

--- tests/hmm/test_data.py
+++ tests/hmm/test_data.py
@@ -1,6 +1,3 @@
-from aiohttp.test_utils import make_mocked_coro
-
-
 async def test_get_status(config, data_layer, fake2, mongo, snapshot, static_time):
     """
     Test that function works when the HMM data are being updated and when they are not.
@@ -45,54 +42,3 @@ async def test_get_status(config, data_layer, fake2, mongo, snapshot, static_tim
     )
 
     assert await data_layer.hmms.get_status() == snapshot
-
-
-async def test_purge(mongo, data_layer, mocker, tmp_path, config, static_time):
-    """
-    Test that the function calls `delete_unreferenced_hmms()` and hides all remaining
-    HMM documents.
-
-    """
-    mocker.patch(
-        "virtool.hmm.db.get_referenced_hmm_ids", make_mocked_coro(["b", "e", "f"])
-    )
-
-    mocker.patch(
-        "virtool.hmm.db.fetch_and_update_release",
-        make_mocked_coro(
-            {
-                "body": "- remove some annotations that didn't have corresponding profiles",
-                "content_type": "application/gzip",
-                "download_url": "https://github.com/virtool/virtool-hmm/releases/download/v0.2.1/vthmm.tar.gz",
-                "etag": 'W/"7bd9cdef79c82ab4d7e5cfff394cf81eaddc6f681b8202f2a7bdc65cbcc4aaea"',
-                "filename": "vthmm.tar.gz",
-                "html_url": "https://github.com/virtool/virtool-hmm/releases/tag/v0.2.1",
-                "id": 1230982,
-                "name": "v0.2.1",
-                "newer": False,
-                "published_at": static_time.datetime,
-                "retrieved_at": static_time.datetime,
-                "size": 85904451,
-            }
-        ),
-    )
-
-    await mongo.hmm.insert_many(
-        [
-            {"_id": "a"},
-            {"_id": "b"},
-            {"_id": "c"},
-            {"_id": "d"},
-            {"_id": "e"},
-            {"_id": "f"},
-        ]
-    )
-
-    await data_layer.settings.ensure()
-    await data_layer.hmms.purge()
-
-    assert await mongo.hmm.find().sort("_id").to_list(None) == [
-        {"_id": "b", "hidden": True},
-        {"_id": "e", "hidden": True},
-        {"_id": "f", "hidden": True},
-    ]
