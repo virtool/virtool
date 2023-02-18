5bb7a2a60e498393b53eaa38195fc373f7d2e2c3
SnickerDoodlePop <markus@swoveland.com>
2023-02-17 16:48:51 -0800

    fix: remove deprecated sqlalchemy feature

--- virtool/pg/base.py
+++ virtool/pg/base.py
@@ -1,24 +1,19 @@
 from enum import Enum
-
+from sqlalchemy.orm import registry
 from sqlalchemy.ext.declarative import as_declarative
 
 
-@as_declarative()
+@registry().as_declarative_base()
 class Base:
     def __repr__(self):
         params = ", ".join(
             f"{column}={value}" for column, value in self.to_dict().items()
         )
-
         return f"<{self.__class__.__name__}({params})>"
-
     def to_dict(self):
         row = {}
-
         for column in self.__table__.columns:
             value = getattr(self, column.name, None)
-
             # Enums cannot serialize to JSON
             row[column.name] = value if not isinstance(value, Enum) else value.value
-
         return row
