652cc54318d12fa09e0c769be49b2951af9877ae
SnickerDoodlePop <markus@swoveland.com>
2023-05-29 13:23:21 -0700

    feat: add BLAST search timeout

--- virtool/blast/task.py
+++ virtool/blast/task.py
@@ -53,7 +53,7 @@ class BLASTTask(BaseTask):
 
         self.rid = blast.rid
 
-    async def wait(self):
+    async def wait(self, retries: int = 3):
         """
         Wait until the BLAST search completes.
 
@@ -64,34 +64,56 @@ class BLASTTask(BaseTask):
         the search completes. The task completes when either an error or
 
         """
-        try:
-            while True:
-                await asyncio.sleep(self.interval)
 
-                blast = await self.data.blast.check_nuvs_blast(
+        for _ in range(0, retries):
+            try:
+                while True:
+                    await asyncio.sleep(self.interval)
+
+                    blast_coroutine = self.data.blast.check_nuvs_blast(
+                        self.analysis_id, self.sequence_index
+                    )
+
+                    blast = await asyncio.wait_for(blast_coroutine, 600)
+
+                    if blast.ready:
+                        logger.info("Retrieved result for BLAST %s", blast.rid)
+                        break
+
+                    if blast.error:
+                        logger.info(
+                            "Encountered error for BLAST %s: %s", blast.rid, blast.error
+                        )
+                        await self._set_error(blast.error)
+                        break
+
+                    self.interval += 5
+
+                    logger.debug(
+                        "Checked BLAST %s. Waiting %s seconds", blast.rid, self.interval
+                    )
+
+                    break
+
+            except asyncio.TimeoutError:
+                await self.data.blast.delete_nuvs_blast(
                     self.analysis_id, self.sequence_index
                 )
 
-                if blast.ready:
-                    logger.info("Retrieved result for BLAST %s", blast.rid)
-                    break
+                logger.info("BLAST timed out: %s", self.rid)
 
-                if blast.error:
-                    logger.info(
-                        "Encountered error for BLAST %s: %s", blast.rid, blast.error
-                    )
-                    await self._set_error(blast.error)
-                    break
+                break
 
-                self.interval += 5
-
-                logger.debug(
-                    "Checked BLAST %s. Waiting %s seconds", blast.rid, self.interval
+            except asyncio.CancelledError:
+                await self.data.blast.delete_nuvs_blast(
+                    self.analysis_id, self.sequence_index
                 )
 
-        except asyncio.CancelledError:
-            await self.data.blast.delete_nuvs_blast(
-                self.analysis_id, self.sequence_index
-            )
+                logger.info("Deleted BLAST due to cancellation: %s", self.rid)
 
-            logger.info("Deleted BLAST due to cancellation: %s", self.rid)
+                break
+
+            except Exception as err:
+                logger.info("Blast failed due to error: %s", err)
+
+                continue
