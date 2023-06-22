918716f61ea6480d88f71be773b21a07c5ff7919
SnickerDoodlePop <markus@swoveland.com>
2023-06-22 13:44:57 -0700

    refactor: access tasks pg table through data layer
    also added docstring and made logging more consistent

--- virtool/tasks/runner.py
+++ virtool/tasks/runner.py
@@ -2,6 +2,7 @@ import asyncio
 import logging
 
 from aiohttp.abc import Application
+from virtool.data.layer import DataLayer
 
 from virtool.pg.utils import get_row_by_id
 from virtool.tasks.client import AbstractTasksClient
@@ -10,23 +11,34 @@ from virtool.tasks.task import BaseTask
 
 from sentry_sdk import capture_exception
 
+SHUTDOWN_TIMEOUT: int = 600
+"""
+Maximum time in seconds to wait
+for a running task to complete
+when a kill signal is recieved.
+
+If this time is exceeded,
+the task will be forcibly shut down.
+"""
+
 
 async def shutdown_task_runner(app: Application):
     """
-    Raise an `asyncio.CancelledError` in the running task runner.
+    Raise an `asyncio.CancelledError`
+    in the running task runner.
 
     :param app: Current running application.
     """
     await app["task_runner"].close(timeout=float("inf"))
 
 
-SHUTDOWN_TIMEOUT: int = 600
-
-
 class TaskRunner:
-    def __init__(self, tasks_client: AbstractTasksClient, app: Application):
+    def __init__(
+        self, data: DataLayer, tasks_client: AbstractTasksClient, app: Application
+    ):
         self._tasks_client = tasks_client
-        self.app = app
+        self._app = app
+        self._data = data
         self._current_task: BaseTask = None
         self._current_task_id: int = None
 
@@ -45,7 +57,7 @@ class TaskRunner:
             raise
 
         except Exception as err:
-            logging.fatal("Task Runner shutting down due to exception: %s", err)
+            logging.fatal("Shutting down due to exception: %s", err)
 
             capture_exception(err)
 
@@ -53,15 +65,13 @@ class TaskRunner:
         """
         Run task given by `self._current_task_id`.
         """
-        task: Task = await get_row_by_id(self.app["pg"], Task, self._current_task_id)
+        task: Task = await self._data.tasks.get(self._current_task_id)
 
         logging.info("Starting task id=%s name=%s", self._current_task_id, task.type)
 
         for cls in BaseTask.__subclasses__():
             if task.type == cls.name:
-                current_task = await cls.from_task_id(
-                    self.app["data"], self._current_task_id
-                )
+                current_task = await cls.from_task_id(self._data, self._current_task_id)
 
                 self._current_task = asyncio.create_task(current_task.run())
 
@@ -77,13 +87,15 @@ class TaskRunner:
         """
         Gracefully shutdown the task runner.
 
-        Current task is awaited for SHUTDOWN_TIMEOUT seconds;
-        if it exceeds this time or recieves a second kill signal,
+        Current task is awaited for SHUTDOWN_TIMEOUT seconds.
+        if it exceeds this time or
+        recieves a second kill signal,
         the task is forcefully shut down.
         """
-        if self._current_task:
-            logging.info("Received stop signal")
 
+        logging.info("Received stop signal")
+
+        if self._current_task:
             try:
                 logging.info("Waiting for task to finish: %s", self._current_task_id)
 
@@ -93,8 +105,8 @@ class TaskRunner:
 
             except asyncio.TimeoutError:
                 logging.critical(
-                    "Task Runner timed out while waiting for task to finish: %s",
+                    "Timed out while waiting for task to finish: %s",
                     self._current_task_id,
                 )
 
-        logging.info("Task Runner shutting down")
+        logging.info("Shutting down")
--- virtool/tasks/utils.py
+++ virtool/tasks/utils.py
@@ -1,6 +1,6 @@
 import asyncio
 
-from virtool_core.redis import connect, periodically_ping_redis
+from virtool_core.redis import connect_redis, periodically_ping_redis
 
 from virtool.pg.utils import connect_pg
 from virtool.config import get_config_from_app
@@ -22,7 +22,7 @@ async def startup_databases_for_spawner(app: App):
 
     pg, redis = await asyncio.gather(
         connect_pg(config.postgres_connection_string),
-        connect(config.redis_connection_string),
+        connect_redis(config.redis_connection_string),
     )
 
     dispatcher_interface = DispatcherClient(redis)
