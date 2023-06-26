9eab631cb71c0b18290b79cbebed0265fb4fbace
Ian Boyes <igboyes@gmail.com>
2023-06-26 09:37:56 -0700

    fix: fix typing and remove unused import

--- virtool/tasks/runner.py
+++ virtool/tasks/runner.py
@@ -2,10 +2,10 @@ import asyncio
 import logging
 
 from aiohttp.abc import Application
+from virtool_core.models.task import Task
 
 from virtool.data.layer import DataLayer
 from virtool.tasks.client import AbstractTasksClient
-from virtool.tasks.models import SQLTask
 from virtool.tasks.task import BaseTask
 
 from sentry_sdk import capture_exception
@@ -32,13 +32,11 @@ async def shutdown_task_runner(app: Application):
 
 
 class TaskRunner:
-    def __init__(
-        self, data: DataLayer, tasks_client: AbstractTasksClient
-    ):
+    def __init__(self, data: DataLayer, tasks_client: AbstractTasksClient):
         self._tasks_client = tasks_client
         self._data = data
-        self._current_task: BaseTask = None
-        self._current_task_id: int = None
+        self._current_task: BaseTask | None = None
+        self._current_task_id: int | None = None
 
     async def run(self):
         try:
@@ -51,12 +49,10 @@ class TaskRunner:
 
         except asyncio.CancelledError:
             await self._shutdown()
-
             raise
 
         except Exception as err:
             logging.fatal("Shutting down due to exception: %s", err)
-
             capture_exception(err)
 
     async def _run_task(self):
@@ -68,7 +64,7 @@ class TaskRunner:
         logging.info("Starting task id=%s name=%s", self._current_task_id, task.type)
 
         for cls in BaseTask.__subclasses__():
-            if task.type == cls.name:
+            if task.type == cls.name and self._current_task_id is not None:
                 current_task = await cls.from_task_id(self._data, self._current_task_id)
 
                 self._current_task = asyncio.create_task(current_task.run())
@@ -78,17 +74,14 @@ class TaskRunner:
                 logging.info("Finished task: %s", self._current_task_id)
 
                 self._current_task = None
-
                 self._current_task_id = None
 
     async def _shutdown(self):
         """
         Gracefully shutdown the task runner.
 
-        Current task is awaited for SHUTDOWN_TIMEOUT seconds.
-        if it exceeds this time or
-        recieves a second kill signal,
-        the task is forcefully shut down.
+        Any running task is given a grace period to finish its work. If a kill signal
+        is received during the grace period, the task will be forcibly stopped.
         """
 
         logging.info("Received stop signal")
@@ -96,11 +89,8 @@ class TaskRunner:
         if self._current_task:
             try:
                 logging.info("Waiting for task to finish: %s", self._current_task_id)
-
                 await asyncio.wait_for(self._current_task, SHUTDOWN_TIMEOUT)
-
                 logging.info("Finished task: %s", self._current_task_id)
-
             except asyncio.TimeoutError:
                 logging.critical(
                     "Timed out while waiting for task to finish: %s",
