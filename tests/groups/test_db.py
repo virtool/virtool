8d1fcf410cd449422e3526b019c3823c9ab19d81
SnickerDoodlePop <markus@swoveland.com>
2023-06-28 15:51:26 -0700

    feat: impliment `AttachGroupsTransform`

--- tests/groups/test_db.py
+++ tests/groups/test_db.py
@@ -1,6 +1,6 @@
 import pytest
 
-from virtool.groups.transforms import AttachGroupTransform
+from virtool.groups.transforms import AttachGroupTransform, AttachGroupsTransform
 from virtool.mongo.transforms import apply_transforms
 
 
@@ -30,3 +30,45 @@ async def test_attach_group_transform(snapshot, fake2, mongo, quantity):
             assert (
                 await apply_transforms(docs, [AttachGroupTransform(mongo)]) == snapshot
             )
+
+
+@pytest.mark.parametrize("quantity", ["one", "many"])
+async def test_attach_groups_transform(snapshot, fake2, mongo, quantity):
+    match quantity:
+        case "one":
+            groups = [
+                await fake2.groups.create(),
+                await fake2.groups.create(),
+                await fake2.groups.create(),
+            ]
+
+            doc = {"_id": "doc", "groups": [groups[0].id, groups[1].id, groups[2].id]}
+
+            transformed_doc = await apply_transforms(
+                doc, [AttachGroupsTransform(mongo)]
+            )
+
+        case "many":
+            groups = [
+                await fake2.groups.create(),
+                await fake2.groups.create(),
+                await fake2.groups.create(),
+                await fake2.groups.create(),
+                await fake2.groups.create(),
+                await fake2.groups.create(),
+                await fake2.groups.create(),
+                await fake2.groups.create(),
+                await fake2.groups.create(),
+            ]
+
+            docs = [
+                {"_id": "doc_0", "groups": [groups[0].id, groups[1].id, groups[2].id]},
+                {"_id": "doc_1", "groups": [groups[3].id, groups[4].id, groups[5].id]},
+                {"_id": "doc_2", "groups": [groups[6].id, groups[7].id, groups[8].id]},
+            ]
+
+            transformed_docs = await apply_transforms(
+                docs, [AttachGroupsTransform(mongo)]
+            )
+
+            pass
--- virtool/groups/transforms.py
+++ virtool/groups/transforms.py
@@ -26,8 +26,8 @@ class AttachGroupTransform(AbstractTransform):
 
         return group_doc
 
-    async def attach_one(self, group_doc: Document, prepared: Any):
-        return {**group_doc, "group": prepared}
+    async def attach_one(self, document: Document, prepared: Any):
+        return {**document, "group": prepared}
 
     async def prepare_many(self, documents: List[Document]):
         user_ids = [extract_group_id(document) for document in documents]
@@ -37,8 +37,46 @@ class AttachGroupTransform(AbstractTransform):
             async for group_doc in self._mongo.groups.find({"_id": {"$in": user_ids}})
         }
 
-    async def attach_many(self, group_docs: List[Document], prepared: Dict[str, Any]):
+    async def attach_many(self, documents: List[Document], prepared: Dict[str, Any]):
         return [
-            await self.attach_one(group_doc, prepared[extract_group_id(group_doc)])
-            for group_doc in group_docs
+            await self.attach_one(document, prepared[extract_group_id(document)])
+            for document in documents
+        ]
+
+
+def extract_group_ids(document: Document) -> List[str]:
+    try:
+        return document["groups"]
+
+    except KeyError:
+        raise ValueError("Document must contain a list of group ids")
+
+
+class AttachGroupsTransform(AbstractTransform):
+    def __init__(self, mongo: Mongo):
+        self._mongo = mongo
+
+    async def prepare_one(self, document: Document) -> Dict[str, Dict]:
+        group_ids = extract_group_ids(document)
+
+        return {
+            group_doc["_id"]: base_processor(group_doc)
+            async for group_doc in self._mongo.groups.find({"_id": {"$in": group_ids}})
+        }
+
+    async def attach_one(self, document: Document, prepared: Any):
+        return {**document, "groups": prepared}
+
+    async def prepare_many(self, documents: List[Document]):
+        prepared = dict()
+
+        for document in documents:
+            prepared[document["_id"]] = await self.prepare_one(document)
+
+        return prepared
+
+    async def attach_many(self, documents: List[Document], prepared: Dict[str, Any]):
+        return [
+            await self.attach_one(document, prepared[document["_id"]])
+            for document in documents
         ]
