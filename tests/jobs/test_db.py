24c8579416a85d2a1386c455a78a6b1072867747
christinewc <crw.wong@gmail.com>
2023-07-11 12:00:48 -0700

    fix: refactor assertion to use string representation

--- tests/jobs/test_db.py
+++ tests/jobs/test_db.py
@@ -1,9 +1,8 @@
-from virtool_core.models.job import Job
-
 from virtool.jobs.db import fetch_complete_job
 
-async def test_fetch_complete_job(snapshot, mongo, fake2, static_time, test_job):
+
+async def test_fetch_complete_job(snapshot, fake2, mongo, static_time, test_job):
     user = await fake2.users.create()
     test_job["user"] = {"id": user.id}
 
-    assert await fetch_complete_job(mongo, test_job) == snapshot
+    assert repr(await fetch_complete_job(mongo, test_job)) == snapshot
