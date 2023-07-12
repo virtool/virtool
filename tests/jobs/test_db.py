6dd297f2bb759ac52e5b8428db3f30da264676c5
christinewc <crw.wong@gmail.com>
2023-07-12 12:37:54 -0700

    fix: make test consistent with test suite

--- tests/jobs/test_db.py
+++ tests/jobs/test_db.py
@@ -5,4 +5,6 @@ async def test_fetch_complete_job(snapshot, fake2, mongo, static_time, test_job)
     user = await fake2.users.create()
     test_job["user"] = {"id": user.id}
 
-    assert repr(await fetch_complete_job(mongo, test_job)) == snapshot
+    result = dict(await fetch_complete_job(mongo, test_job))
+
+    assert result == snapshot
