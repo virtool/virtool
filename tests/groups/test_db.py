009fd8fda0aa3a03f2a3d9a051d6675386340b56
SnickerDoodlePop <markus@swoveland.com>
2023-07-21 13:23:40 -0700

    test: add testing for `AttachGroupsTransform`

--- tests/groups/test_db.py
+++ tests/groups/test_db.py
@@ -44,6 +44,11 @@ async def test_attach_groups_transform(snapshot, fake2, mongo, quantity):
 
             doc = {"_id": "doc", "groups": [groups[0].id, groups[1].id, groups[2].id]}
 
+            complete_doc = await apply_transforms(doc, [AttachGroupsTransform(mongo)])
+
+            assert complete_doc == snapshot
+
+
             assert (
                 await apply_transforms(doc, [AttachGroupsTransform(mongo)]) == snapshot
             )
@@ -67,6 +72,6 @@ async def test_attach_groups_transform(snapshot, fake2, mongo, quantity):
                 {"_id": "doc_2", "groups": [groups[6].id, groups[7].id, groups[8].id]},
             ]
 
-            assert (
-                await apply_transforms(docs, [AttachGroupsTransform(mongo)]) == snapshot
-            )
+            complete_docs = await apply_transforms(docs, [AttachGroupsTransform(mongo)])
+
+            assert complete_docs == snapshot
