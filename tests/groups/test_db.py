3fccd8912d71552ed78499b3dd05c2ffb703c1bb
SnickerDoodlePop <markus@swoveland.com>
2023-08-23 13:36:38 -0700

    test: use `fake2` to create fake users

--- tests/groups/test_db.py
+++ tests/groups/test_db.py
@@ -1,85 +1,106 @@
-from virtool.fake.next import DataFaker
-from virtool.groups.transforms import AttachPrimaryGroupTransform, AttachGroupsTransform
 from virtool.data.transforms import apply_transforms
+from virtool.groups.transforms import AttachGroupsTransform, AttachPrimaryGroupTransform
+from virtool.utils import base_processor
 
 
-async def create_fake_users_with_groups(
-    faker: DataFaker = None,
-    quantity: int = 1,
-    use_primary_group: bool = False,
-    use_groups: bool = False,
-) -> list[dict]:
-    """
-    Create a list of unique fake users
+class TestAttachPrimaryGroup:
+    async def test_no_primary_group(self, snapshot, fake2, mongo):
+        user = await fake2.users.create()
 
-    :param faker: data faker used to create fake data
-    :quantity: number of users to create
-    :use_primary_group: attach a primary group to each user
-    :use_groups: attach a list of groups to each user
-    :return: a list of fake users
-    """
+        incomplete_user = base_processor(await mongo.users.find_one({"_id": user.id}))
 
-    fake_users = []
-
-    for i in range(0, quantity):
-        fake_user = {"id": f"doc_{i}"}
-
-        if use_primary_group:
-            fake_user.update({"primary_group": (await faker.groups.create()).id})
-
-        if use_groups:
-            fake_user.update(
-                {"groups": [(await faker.groups.create()).id for i in range(0, 2)]}
+        assert (
+            await apply_transforms(
+                incomplete_user,
+                [AttachGroupsTransform(mongo), AttachPrimaryGroupTransform(mongo)],
             )
-
-        fake_users.append(fake_user)
-
-    return fake_users
-
-
-class TestAttachPrimaryGroupTransform:
-    async def test_no_primary_group(self, snapshot, mongo):
-        doc = (await create_fake_users_with_groups(None, 1, False, False))[0]
-
-        complete_doc = await apply_transforms(doc, [AttachPrimaryGroupTransform(mongo)])
-
-        assert complete_doc == snapshot
-
-    async def test_single_document(self, snapshot, fake2, mongo):
-        doc = (await create_fake_users_with_groups(fake2, 1, True, False))[0]
-
-        complete_doc = await apply_transforms(doc, [AttachPrimaryGroupTransform(mongo)])
-
-        assert complete_doc == snapshot
-
-    async def test_multiple_documents(self, snapshot, fake2, mongo):
-        docs = await create_fake_users_with_groups(fake2, 3, True, False)
-
-        complete_docs = await apply_transforms(
-            docs, [AttachPrimaryGroupTransform(mongo)]
+            == snapshot
         )
 
-        assert complete_docs == snapshot
-
-
-class TestAttachGroupsTransform:
-    async def test_no_groups(self, snapshot, mongo):
-        doc = (await create_fake_users_with_groups(None, 1, False, False))[0]
-
-        complete_doc = await apply_transforms(doc, [AttachGroupsTransform(mongo)])
-
-        assert complete_doc == snapshot
-
     async def test_single_document(self, snapshot, fake2, mongo):
-        doc = (await create_fake_users_with_groups(fake2, 1, False, True))[0]
+        group = await fake2.groups.create()
 
-        complete_doc = await apply_transforms(doc, [AttachGroupsTransform(mongo)])
+        user = await fake2.users.create(groups=[group], primary_group=group)
 
-        assert complete_doc == snapshot
+        incomplete_user = base_processor(await mongo.users.find_one({"_id": user.id}))
+        assert (
+            await apply_transforms(
+                incomplete_user,
+                [AttachGroupsTransform(mongo), AttachPrimaryGroupTransform(mongo)],
+            )
+            == snapshot
+        )
 
     async def test_multiple_documents(self, snapshot, fake2, mongo):
-        docs = await create_fake_users_with_groups(fake2, 3, False, True)
+        group_0 = await fake2.groups.create()
+        group_1 = await fake2.groups.create()
 
-        complete_docs = await apply_transforms(docs, [AttachGroupsTransform(mongo)])
+        user_0 = await fake2.users.create(groups=[group_0], primary_group=group_0)
+        user_1 = await fake2.users.create(groups=[group_1], primary_group=group_1)
 
-        assert complete_docs == snapshot
+        incomplete_users = [
+            base_processor(await mongo.users.find_one({"_id": user_0.id})),
+            base_processor(await mongo.users.find_one({"_id": user_1.id})),
+        ]
+
+        assert (
+            await apply_transforms(
+                incomplete_users,
+                [AttachGroupsTransform(mongo), AttachPrimaryGroupTransform(mongo)],
+            )
+            == snapshot
+        )
+
+
+class TestAttachGroups:
+    async def test_no_groups(self, snapshot, fake2, mongo):
+        user = await fake2.users.create()
+
+        incomplete_user = base_processor(await mongo.users.find_one({"_id": user.id}))
+
+        assert (
+            await apply_transforms(incomplete_user, [AttachGroupsTransform(mongo)])
+            == snapshot
+        )
+
+    async def test_single_document(self, snapshot, fake2, mongo):
+        user = await fake2.users.create(
+            groups=[
+                await fake2.groups.create(),
+                await fake2.groups.create(),
+                await fake2.groups.create(),
+            ]
+        )
+
+        incomplete_user = base_processor(await mongo.users.find_one({"_id": user.id}))
+
+        assert (
+            await apply_transforms(incomplete_user, [AttachGroupsTransform(mongo)])
+            == snapshot
+        )
+
+    async def test_multiple_documents(self, snapshot, fake2, mongo):
+        user_0 = await fake2.users.create(
+            groups=[
+                await fake2.groups.create(),
+                await fake2.groups.create(),
+                await fake2.groups.create(),
+            ]
+        )
+        user_1 = await fake2.users.create(
+            groups=[
+                await fake2.groups.create(),
+                await fake2.groups.create(),
+                await fake2.groups.create(),
+            ]
+        )
+
+        incomplete_users = [
+            base_processor(await mongo.users.find_one({"_id": user_0.id})),
+            base_processor(await mongo.users.find_one({"_id": user_1.id})),
+        ]
+
+        assert (
+            await apply_transforms(incomplete_users, [AttachGroupsTransform(mongo)])
+            == snapshot
+        )
