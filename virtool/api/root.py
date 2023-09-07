5407ec996a516cad09da95bc8c33933598f8dac2
Matthew <matthewcurtis1999@gmail.com>
2023-09-07 14:02:44 -0700

    fix: update root handler with correct documentation links

--- virtool/api/root.py
+++ virtool/api/root.py
@@ -4,7 +4,7 @@ from virtool.http.policy import policy, PublicRoutePolicy
 from virtool.http.routes import Routes
 from virtool.data.utils import get_data_from_req
 
-API_URL_ROOT = "https://www.virtool.ca/docs/developer/api"
+API_URL_ROOT = "https://www.virtool.ca/docs/api"
 
 routes = Routes()
 
@@ -24,22 +24,26 @@ async def get(req):
         "first_user": first_user,
         "endpoints": {
             "account": {"url": "/account", "doc": f"{API_URL_ROOT}/account"},
+            "admin": {"url": "/admin", "doc": f"{API_URL_ROOT}/admin"},
             "analyses": {"url": "/analyses", "doc": f"{API_URL_ROOT}/analyses"},
-            "genbank": {"url": "/genbank", "doc": f"{API_URL_ROOT}/genbank"},
             "groups": {"url": "/groups", "doc": f"{API_URL_ROOT}/groups"},
             "history": {"url": "/history", "doc": f"{API_URL_ROOT}/history"},
-            "hmm": {"url": "/hmm", "doc": f"{API_URL_ROOT}/hmm"},
+            "hmms": {"url": "/hmms", "doc": f"{API_URL_ROOT}/hmms"},
             "indexes": {"url": "/indexes", "doc": f"{API_URL_ROOT}/indexes"},
             "jobs": {"url": "/jobs", "doc": f"{API_URL_ROOT}/jobs"},
+            "labels": {"url": "/labels", "doc": f"{API_URL_ROOT}/labels"},
+            "ml": {"url": "/ml", "doc": f"{API_URL_ROOT}/ml"},
             "otus": {"url": "/otus", "doc": f"{API_URL_ROOT}/otus"},
             "references": {"url": "/references", "doc": f"{API_URL_ROOT}/refs"},
             "samples": {"url": "/samples", "doc": f"{API_URL_ROOT}/samples"},
             "settings": {"url": "/settings", "doc": f"{API_URL_ROOT}/settings"},
+            "spaces": {"url": "/spaces", "doc": f"{API_URL_ROOT}/spaces"},
             "subtraction": {
                 "url": "/subtraction",
                 "doc": f"{API_URL_ROOT}/subtraction",
             },
             "tasks": {"url": "/tasks", "doc": f"{API_URL_ROOT}/tasks"},
+            "uploads": {"url": "/uploads", "doc": f"{API_URL_ROOT}/uploads"},
             "users": {"url": "/users", "doc": f"{API_URL_ROOT}/users"},
         },
     }
