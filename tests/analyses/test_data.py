1f62a3886753dbbcca01c89df7012fc880cc5a5a
Matthew <matthewcurtis1999@gmail.com>
2023-09-14 08:39:42 -0700

    test: add test to ensure task_args always receives analysis ID even if not passed in

--- tests/analyses/test_data.py
+++ tests/analyses/test_data.py
@@ -66,3 +66,66 @@ async def test_create(
     assert await mongo.analyses.find_one({"_id": analysis.id}) == snapshot(
         name="mongo", exclude=props("created_at", "updated_at")
     )
+
+
+async def test_create_analysis_id(
+    data_layer,
+    fake2,
+    mongo,
+    snapshot,
+    users_data,
+):
+    """
+    Tests that an analysis is created with the expected fields.
+    """
+    subtractions = ["subtraction_1", "subtraction_2"]
+
+    user = await fake2.users.create()
+
+    async with mongo.create_session() as session:
+        await mongo.samples.insert_one({"_id": "test_sample", "name": "Test Sample"})
+
+        await mongo.indexes.insert_one(
+            {
+                "_id": "test_index",
+                "version": 11,
+                "ready": True,
+                "reference": {"id": "test_ref"},
+            }
+        )
+
+        await mongo.references.insert_one(
+            {
+                "_id": "test_ref",
+                "name": "Test Reference",
+                "data_type": "genome",
+            }
+        )
+
+        await mongo.subtraction.insert_many(
+            [
+                {
+                    "_id": "subtraction_1",
+                    "name": "Subtraction 1",
+                },
+                {
+                    "_id": "subtraction_2",
+                    "name": "Subtraction 2",
+                },
+            ],
+            session=session,
+        )
+
+    analysis = await data_layer.analyses.create(
+        "test_sample",
+        "test_ref",
+        subtractions,
+        user.id,
+        QuickAnalyzeWorkflow.nuvs,
+        0,
+    )
+
+    assert analysis == snapshot(name="obj", exclude=props("created_at", "updated_at"))
+    assert await mongo.analyses.find_one({"_id": analysis.id}) == snapshot(
+        name="mongo", exclude=props("created_at", "updated_at")
+    )
