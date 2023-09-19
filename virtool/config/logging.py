702467d08ba9e9a6dbc103a6ce95b2ef49bb0fe0
Matthew <matthewcurtis1999@gmail.com>
2023-09-19 10:12:15 -0700

    fix: remove dependency on coloredlogs

--- virtool/config/logging.py
+++ virtool/config/logging.py
@@ -1,8 +1,5 @@
 import logging
-import sys
-from logging import DEBUG, INFO
-
-import coloredlogs
+from logging import INFO, DEBUG
 import structlog
 
 
@@ -16,36 +13,23 @@ def configure_logs(debug: bool):
     :param debug: log debug messages
 
     """
+    logging.basicConfig(level=DEBUG if debug else INFO, format="%(message)s")
 
     structlog.configure(
-         processors=[
-        #     # If log level is too low, abort pipeline and throw away log entry.
-              structlog.stdlib.filter_by_level,
-        #     # Add the name of the logger to event dict.
-             structlog.stdlib.add_logger_name,
-             structlog.stdlib.add_log_level,
-        #     # Perform %-style formatting.
-             structlog.stdlib.PositionalArgumentsFormatter(),
-        #     # Add a timestamp in ISO 8601 format.
-             structlog.processors.TimeStamper(fmt="%Y-%m-%d %H:%M.%S"),
-        #     # Add callsite parameters.
-             structlog.processors.CallsiteParameterAdder(
-                 {
-                     structlog.processors.CallsiteParameter.PATHNAME,
-                 }
-             ),
-            structlog.dev.ConsoleRenderer(colors=True)
-        #
-        ],
-        # # `wrapper_class` is the bound logger that you get back from
-        # # get_logger(). This one imitates the API of `logging.Logger`.
         wrapper_class=structlog.make_filtering_bound_logger(DEBUG if debug else INFO),
-        # # `logger_factory` is used to create wrapped loggers that are used for
-        # # OUTPUT. This one returns a `logging.Logger`. The final value from
-        # # the final processor will be passed to the method of the same name as
-        # # that you've called on the bound logger.
         logger_factory=structlog.stdlib.LoggerFactory(),
-        # # Effectively freeze configuration after creating the first bound
-        # # logger.
         cache_logger_on_first_use=True,
+        processors=[
+            structlog.stdlib.filter_by_level,
+            structlog.stdlib.add_logger_name,
+            structlog.stdlib.add_log_level,
+            structlog.stdlib.PositionalArgumentsFormatter(),
+            structlog.processors.TimeStamper(fmt="%Y-%m-%d %H:%M.%S"),
+            structlog.processors.CallsiteParameterAdder(
+                {
+                    structlog.processors.CallsiteParameter.PATHNAME,
+                }
+            ),
+            structlog.dev.ConsoleRenderer(colors=True, sort_keys=True),
+        ],
     )
