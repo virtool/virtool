fd5850683ec816885a107741cf87971e643912c5
Matthew <matthewcurtis1999@gmail.com>
2023-09-20 10:02:36 -0700

    test: refactor test and update snapshot

--- tests/analyses/test_data.py
+++ tests/analyses/test_data.py
@@ -2,30 +2,30 @@ import os
 
 import pytest
 from syrupy import snapshot
+from syrupy.filters import props
 
 from virtool.analyses.models import SQLAnalysisFile
+from virtool.config import get_config_from_app
 from virtool.pg.utils import get_row_by_id
 from tests.analyses.test_api import files
 
 
 @pytest.mark.apitest
-@pytest.mark.parametrize("error", [None, 400, 404, 422])
-async def test_upload_file(error, files, spawn_job_client, tmp_path, data_layer):
+async def test_upload_file(files, spawn_job_client, tmp_path, data_layer, snapshot):
     """
     Test that an analysis result file is properly uploaded and a row is inserted into the `analysis_files` SQL table.
 
     """
     client = await spawn_job_client(authorize=True)
-
-    if error is None:
-        format_ = "fasta"
-        await client.db.analyses.insert_one(
-            {"_id": "foobar", "ready": True, "job": {"id": "hello"}}
-        )
-        resp = await client.put(
-            f"/analyses/foobar/files?name=reference.fa&format={format_}", data=files
-        )
-        assert resp.status == 201
-        assert await resp.json() == snapshot
-        assert os.listdir(tmp_path / "analyses") == ["1-reference.fa"]
-        assert await get_row_by_id(data_layer.analysis.pg, SQLAnalysisFile, 1)
+    get_config_from_app(client.app).data_path = tmp_path
+    format_ = "fasta"
+    await client.db.analyses.insert_one(
+        {"_id": "foobar", "ready": True, "job": {"id": "hello"}}
+    )
+    resp = await client.put(
+        f"/analyses/foobar/files?name=reference.fa&format={format_}", data=files
+    )
+    assert resp.status == 201
+    assert await resp.json() == snapshot(exclude=props("uploaded_at"))
+    assert os.listdir(tmp_path / "analyses") == ["1-reference.fa"]
+    assert await get_row_by_id(data_layer.analyses._pg, SQLAnalysisFile, 1)
