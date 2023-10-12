1b129f630dc7f2745fc5fcdcba1a7e0fc22e5dcf
Ian Boyes <igboyes@gmail.com>
2023-10-12 11:36:25 -0700

    fix: fix hmm related `404` during workflow runs
    The profiles file was being installed at `/data/hmm/profiles.hmm/profiles.hmm`.

--- virtool/hmm/utils.py
+++ virtool/hmm/utils.py
@@ -1,14 +1,11 @@
-from pathlib import Path
-from typing import Optional
-
 from semver import VersionInfo
 
 import virtool.github
 
 
 def format_hmm_release(
-    updated: Optional[dict], release: dict, installed: dict
-) -> Optional[dict]:
+    updated: dict | None, release: dict, installed: dict
+) -> dict | None:
     # The release dict will only be replaced if there is a 200 response from GitHub. A 304 indicates the release
     # has not changed and `None` is returned from `get_release()`.
     if updated is None:
@@ -27,13 +24,3 @@ def format_hmm_release(
     )
 
     return formatted
-
-
-def hmm_data_exists(file_path: Path) -> bool:
-    """
-    Checks if HMM data exists in the local data path.
-
-    :param file_path: Path to where `profiles.hmm` should be
-    :return: True if both the `hmm` directory and `profiles.hmm` exist, else False
-    """
-    return file_path.parent.is_dir() and file_path.is_file()
