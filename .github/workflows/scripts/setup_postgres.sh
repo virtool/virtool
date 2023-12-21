392648f21243a958865fc555836f59b893bcf484
Ian Boyes <igboyes@gmail.com>
2023-12-20 20:14:55 -0800

    ci: check if one pg db can service vt and ofga

---
+++ .github/workflows/scripts/setup_postgres.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+	CREATE DATABASE openfga;
+	GRANT ALL PRIVILEGES ON DATABASE openfga TO "$POSTGRES_USER";
+EOSQL
