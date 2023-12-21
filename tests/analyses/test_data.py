c3421b771e72f08ea5a66aba2dfe5f36f7756975
Matthew <matthewcurtis1999@gmail.com>
2023-12-21 14:59:35 -0800

    test: clean test-find and create fixture for analyses tests.

--- tests/analyses/test_data.py
+++ tests/analyses/test_data.py
@@ -1,65 +1,26 @@
 import asyncio
 import os
 
+
 from virtool_core.models.enums import QuickAnalyzeWorkflow
 
 from tests.analyses.test_api import create_files
+from tests.fixtures.snapshot_date import snapshot_recent
 from virtool.data.layer import DataLayer
 from virtool.fake.next import DataFaker
+from virtool.http.client import UserClient
 from virtool.mongo.core import Mongo
 from virtool.analyses.models import SQLAnalysisFile
 from virtool.config import get_config_from_app
 from virtool.pg.utils import get_row_by_id
-
 import pytest
 
 from syrupy import snapshot, SnapshotAssertion
 from syrupy.filters import props
 
 
-async def test_find(data_layer: DataLayer, mongo: Mongo, snapshot: SnapshotAssertion):
-    await asyncio.gather(
-        mongo.samples.insert_one({"_id": "test_sample", "name": "Test Sample"}),
-        mongo.indexes.insert_one(
-            {
-                "_id": "test_index",
-                "version": 11,
-                "ready": True,
-                "reference": {"id": "test_ref"},
-            }
-        ),
-        mongo.references.insert_one(
-            {
-                "_id": "test_ref",
-                "name": "Test Reference",
-                "data_type": "genome",
-            }
-        ),
-        mongo.subtraction.insert_many(
-            [
-                {
-                    "_id": "subtraction_1",
-                    "name": "Subtraction 1",
-                },
-                {
-                    "_id": "subtraction_2",
-                    "name": "Subtraction 2",
-                },
-            ],
-            session=None,
-        ),
-    )
-
-
-async def test_create(
-    data_layer: DataLayer,
-    fake2: DataFaker,
-    mongo: Mongo,
-    snapshot,
-):
-    """
-    Tests that an analysis is created with the expected fields.
-    """
+@pytest.fixture
+async def set_up_sample(mongo: "Mongo", fake2: DataFaker):
     user = await fake2.users.create()
 
     await asyncio.gather(
@@ -93,12 +54,63 @@ async def test_create(
             session=None,
         ),
     )
+    return user.id
+
+
+@pytest.mark.parametrize("number_of_analyses", [0, 1, 2])
+async def test_find(
+    data_layer: DataLayer,
+    snapshot_recent: SnapshotAssertion,
+    mongo: Mongo,
+    set_up_sample,
+    mocker,
+    number_of_analyses,
+):
+    """
+    Tests that all analysis are listed.
+    """
+    mocker.patch("virtool.samples.utils.get_sample_rights", return_value=(True, True))
+    client = mocker.Mock(spec=UserClient)
+    user_id = set_up_sample
+
+    await mongo.samples.insert_one({"_id": "test_false", "name": "Test False"})
+
+    analysis_wrong_sample = await data_layer.analyses.create(
+        "test_false",
+        "test_ref",
+        ["subtraction_1", "subtraction_2"],
+        user_id,
+        QuickAnalyzeWorkflow.nuvs,
+        0,
+    )
+    for _ in range(number_of_analyses):
+        await data_layer.analyses.create(
+            "test_sample",
+            "test_ref",
+            ["subtraction_1", "subtraction_2"],
+            user_id,
+            QuickAnalyzeWorkflow.nuvs,
+            0,
+        )
+
+    analyses_found = await data_layer.analyses.find(1, 25, client, "test_sample")
+
+    assert analyses_found.dict() == snapshot_recent()
+    assert analysis_wrong_sample not in analyses_found
+    assert analyses_found.total_count == analyses_found.found_count + 1
+
+
+async def test_create(data_layer: DataLayer, mongo: Mongo, snapshot, set_up_sample):
+    """
+    Tests that an analysis is created given an analysis id with the expected fields.
+    """
+    user_id = set_up_sample
 
     analysis = await data_layer.analyses.create(
         "test_sample",
         "test_ref",
         ["subtraction_1", "subtraction_2"],
-        user.id,
+        user_id,
         QuickAnalyzeWorkflow.nuvs,
         0,
         analysis_id="test_analysis",
@@ -112,53 +124,18 @@ async def test_create(
 
 
 async def test_create_analysis_id(
-    data_layer: DataLayer,
-    fake2: DataFaker,
-    mongo: Mongo,
-    snapshot,
+    data_layer: DataLayer, mongo: Mongo, snapshot, set_up_sample
 ):
     """
     Tests that an analysis is created with the expected fields.
     """
-    user = await fake2.users.create()
-
-    await asyncio.gather(
-        mongo.samples.insert_one({"_id": "test_sample", "name": "Test Sample"}),
-        mongo.indexes.insert_one(
-            {
-                "_id": "test_index",
-                "version": 11,
-                "ready": True,
-                "reference": {"id": "test_ref"},
-            }
-        ),
-        mongo.references.insert_one(
-            {
-                "_id": "test_ref",
-                "name": "Test Reference",
-                "data_type": "genome",
-            }
-        ),
-        mongo.subtraction.insert_many(
-            [
-                {
-                    "_id": "subtraction_1",
-                    "name": "Subtraction 1",
-                },
-                {
-                    "_id": "subtraction_2",
-                    "name": "Subtraction 2",
-                },
-            ],
-            session=None,
-        ),
-    )
+    user_id = set_up_sample
 
     analysis = await data_layer.analyses.create(
         "test_sample",
         "test_ref",
         ["subtraction_1", "subtraction_2"],
-        user.id,
+        user_id,
         QuickAnalyzeWorkflow.nuvs,
         0,
     )
