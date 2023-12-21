511e41959e37bec7e29993732c494315c51e90bd
Reece Hoffmann <reecehoffmann@outlook.com>
2023-12-21 15:46:15 -0800

    ci: fix postgres connection string

--- .github/workflows/scripts/setup_postgres.sh
+++ .github/workflows/scripts/setup_postgres.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "virtool" --dbname "virtool" <<-EOSQL
+export PGPASSWORD=virtool
+psql -v ON_ERROR_STOP=1 --username "virtool" --dbname "virtool" --host "localhost" <<-EOSQL
 	CREATE DATABASE openfga;
 	GRANT ALL PRIVILEGES ON DATABASE openfga TO "virtool";
 EOSQL
