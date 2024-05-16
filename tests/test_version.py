429719b6a217f2bed368032063b63b3cbadf962f
Reece Hoffmann <reecehoffmann@outlook.com>
2024-05-16 10:51:38 -0700

    chore: update import style

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,14 +1,11 @@
 import pytest
-
-import virtool.version
+from virtool.version import determine_server_version
 
 
 @pytest.mark.parametrize("source", [None,  "file"])
 async def test_find_server_version(source, tmp_path):
     if source == "file":
         tmp_path.joinpath("VERSION").write_text("1.0.12")
-
-        assert await virtool.version.determine_server_version(tmp_path) == "1.0.12"
-
+        assert await determine_server_version(tmp_path) == "1.0.12"
     else:
-        assert await virtool.version.determine_server_version(tmp_path) == "Unknown"
+        assert await determine_server_version(tmp_path) == "Unknown"
