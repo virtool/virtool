d6f7808067570225d1cfd8728ef28fad122955ff
Ian <igboyes@gmail.com>
2024-05-23 15:30:48 -0700

    chore: remove remaining caches code

--- virtool/samples/files.py
+++ virtool/samples/files.py
@@ -1,36 +1,28 @@
-from typing import Dict, List, Optional
+from typing import Dict, Optional
 
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession
 
+# Keep this so models are recognized by the ORM.
+import virtool.caches.models
 import virtool.utils
-from virtool.caches.models import SQLSampleArtifactCache, SQLSampleReadsCache
 from virtool.samples.models import SQLSampleArtifact, SQLSampleReads
 from virtool.uploads.models import SQLUpload
 
 
-async def get_existing_reads(
-    pg: AsyncEngine, sample: str, key: str = None
-) -> List[str]:
-    """
-    Get reads files in either `sample_reads_files` or `sample_reads_files_cache` depending on value
-    of `key`.
+async def get_existing_reads(pg: AsyncEngine, sample_id: str) -> list[str]:
+    """Get read file names for a sample.
 
-    :param pg: PostgreSQL AsyncEngine object
-    :param sample: ID that corresponds to a parent sample
-    :param key: A key for a given cache
-    :return: List of reads file names that are in a given table
+    :param pg: the application asyncengine object
+    :param sample_id: a sample id
+    :return: read file names associated with the sample
     """
     async with AsyncSession(pg) as session:
-        statement = (
-            select(SQLSampleReads).filter_by(sample=sample)
-            if not key
-            else select(SQLSampleReadsCache).filter_by(sample=sample, key=key)
+        result = await session.execute(
+            select(SQLSampleReads).filter_by(sample=sample_id),
         )
 
-        query = await session.execute(statement)
-
-    return [result.name for result in query.scalars().all()]
+    return [row.name for row in result.scalars().all()]
 
 
 async def create_artifact_file(
@@ -39,10 +31,8 @@ async def create_artifact_file(
     name_on_disk: str,
     sample: str,
     artifact_type: str,
-    key: Optional[str] = None,
 ) -> Dict[str, any]:
-    """
-    Create a row in an SQL table that represents uploaded sample artifact files. A row is created
+    """Create a row in an SQL table that represents uploaded sample artifact files. A row is created
     in either the `sample_artifact` or `sample_artifact_cache` table depending on the value of
     `key`.
 
@@ -51,16 +41,16 @@ async def create_artifact_file(
     :param name_on_disk: Name of the sample artifact file as it appears on disk
     :param sample: ID that corresponds to a parent sample
     :param artifact_type: Type of artifact to be uploaded
-    :param key: A key for a given cache
     :return: A dictionary representation of the newly created row
     """
     async with AsyncSession(pg) as session:
-        artifact = SQLSampleArtifactCache(key=key) if key else SQLSampleArtifact()
-
-        artifact.name = name
-        artifact.name_on_disk = name_on_disk
-        artifact.sample = sample
-        artifact.type = artifact_type
+        artifact = SQLSampleArtifact(
+            name=name,
+            name_on_disk=name_on_disk,
+            sample=sample,
+            type=artifact_type,
+            uploaded_at=virtool.utils.timestamp(),
+        )
 
         session.add(artifact)
         await session.flush()
@@ -78,32 +68,27 @@ async def create_reads_file(
     name: str,
     name_on_disk: str,
     sample_id: str,
-    cache: bool = False,
     upload_id: Optional[int] = None,
-    key: Optional[str] = None,
 ) -> Dict[str, any]:
-    """
-    Create a row in a SQL table that represents uploaded sample reads files.
+    """Create a row in a SQL table that represents uploaded sample reads files.
 
     :param pg: PostgreSQL AsyncEngine object
     :param size: Size of a newly uploaded file in bytes
     :param name: Name of the file (either `reads_1.fq.gz` or `reads_2.fq.gz`)
     :param name_on_disk: Name of the newly uploaded file on disk
     :param sample_id: ID that corresponds to a parent sample
-    :param cache: Whether the row should be created in the `sample_reads_files` or `sample_reads_files_cache` table
     :param upload_id: ID for a row in the `uploads` table to pair with
-    :param key: the cache key if the file is associated with a cache
     :return: List of dictionary representations of the newly created row(s)
 
     """
     async with AsyncSession(pg) as session:
-        reads = SQLSampleReadsCache(key=key) if cache else SQLSampleReads()
-
-        reads.sample = sample_id
-        reads.name = name
-        reads.name_on_disk = name_on_disk
-        reads.size = size
-        reads.uploaded_at = virtool.utils.timestamp()
+        reads = SQLSampleReads(
+            sample=sample_id,
+            name=name,
+            name_on_disk=name_on_disk,
+            size=size,
+            uploaded_at=virtool.utils.timestamp(),
+        )
 
         if upload_id and (
             upload := (
