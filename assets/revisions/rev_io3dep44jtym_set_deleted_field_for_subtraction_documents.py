0a6657ab48c36276b12a8cef159356618a3de2cd
Reece Hoffmann <reecehoffmann@outlook.com>
2024-05-28 09:54:47 -0700

    fix: place subtraction revision at end of revision history

--- assets/revisions/rev_io3dep44jtym_set_deleted_field_for_subtraction_documents.py
+++ assets/revisions/rev_io3dep44jtym_set_deleted_field_for_subtraction_documents.py
@@ -15,7 +15,7 @@ created_at = arrow.get("2024-05-27 20:43:33.597733")
 revision_id = "io3dep44jtym"
 
 alembic_down_revision = None
-virtool_down_revision = "ulapmx8i3vpg"
+virtool_down_revision = "0p3nhjg1fcfj"
 
 # Change this if an Alembic revision is required to run this migration.
 required_alembic_revision = None
