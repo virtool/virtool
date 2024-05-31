c06a9c68f11005fec4b7bc3ad5e6635d83ea6e3e
Reece Hoffmann <reecehoffmann@outlook.com>
2024-05-31 12:07:40 -0700

    fix: set job field for indexes, samples and subtractions

---
+++ assets/revisions/rev_t05gnq2g81qz_set_jobs_field_for_indexes,_samples_and_subtractions.py
@@ -0,0 +1,48 @@
+"""
+set job field for indexes, samples and subtractions
+
+Revision ID: t05gnq2g81qz
+Date: 2024-05-31 18:43:38.988811
+
+"""
+
+import arrow
+import pytest
+
+from virtool.migration import MigrationContext
+
+# Revision identifiers.
+name = "set job field for indexes, samples and subtractions"
+created_at = arrow.get("2024-05-31 18:43:38.988811")
+revision_id = "t05gnq2g81qz"
+
+alembic_down_revision = None
+virtool_down_revision = "io3dep44jtym"
+
+# Change this if an Alembic revision is required to run this migration.
+required_alembic_revision = None
+
+
+async def upgrade(ctx: MigrationContext):
+    for collection in ["indexes", "samples", "subtractions"]:
+        await ctx.mongo[collection].update_many(
+            {"job": {"$exists": False}}, {"$set": {"job": None}}
+        )
+
+
+@pytest.mark.parametrize("collection", ["indexes", "samples", "subtractions"])
+async def test_upgrade(ctx, collection, snapshot):
+    await ctx.mongo[collection].insert_many(
+        [
+            {"_id": f"{collection}_1", "name": f"{collection}_1", "job": None},
+            {"_id": f"{collection}_2", "name": f"{collection}_2", "job": ["job_id"]},
+            {
+                "_id": f"{collection}_3",
+                "name": f"{collection}_3",
+            },
+        ]
+    )
+
+    await upgrade(ctx)
+
+    assert await ctx.mongo[collection].find().to_list(None) == snapshot
