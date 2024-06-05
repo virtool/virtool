2f8bb1eb1934f1601bf2c90ad19bb5b679a42183
bryce-davidson <lifebryce@gmail.com>
2024-06-04 18:01:51 -0700

    chore: use snapshot_recent

--- tests/fake/test_wrapper.py
+++ tests/fake/test_wrapper.py
@@ -1,12 +1,6 @@
 from faker import Faker
 from virtool.fake.wrapper import FakerWrapper
 
-
-def test_wraps():
-    wrapper = FakerWrapper()
-    assert isinstance(wrapper.fake, Faker)
-
-
 def test_get_mongo_id(snapshot):
     wrapper = FakerWrapper()
     assert [wrapper.get_mongo_id() for _ in range(5)] == snapshot
