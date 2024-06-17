a8759e7ff12e59f29ca3fd9bbc9821881c256686
bryce <bryce@node-1w7jr9ss3lchv59jr2vdpz690.bryce>
2024-06-17 14:01:11 -0700

    test: remove mongo subtraction insert calls

--- tests/subtractions/test_db.py
+++ tests/subtractions/test_db.py
@@ -1,6 +1,6 @@
-import pytest
-
+from virtool.fake.next import DataFaker
 import virtool.subtractions.db
+from virtool.uploads.models import UploadType
 from virtool.data.transforms import apply_transforms
 from virtool.subtractions.db import (
     AttachSubtractionsTransform,
@@ -8,54 +8,76 @@ from virtool.subtractions.db import (
 )
 
 
-@pytest.mark.parametrize(
-    "documents",
-    [
-        {"id": "sub_1", "subtractions": ["foo", "bar"]},
-        [
-            {"id": "sub_1", "subtractions": ["foo", "bar"]},
-            {"id": "sub_2", "subtractions": ["foo"]},
-            {"id": "sub_3", "subtractions": []},
-        ],
-    ],
-)
-async def test_attach_subtractions(documents, mongo, snapshot):
-    await mongo.subtraction.insert_many(
-        [{"_id": "foo", "name": "Foo"}, {"_id": "bar", "name": "Bar"}], session=None
+async def test_attach_subtractions(fake: DataFaker, mongo, snapshot):
+    user = await fake.users.create()
+    upload = await fake.uploads.create(
+        user=user, upload_type=UploadType.subtraction, name="foobar.fq.gz"
     )
 
-    result = await apply_transforms(documents, [AttachSubtractionsTransform(mongo)])
+    subtraction_ids = [
+        subtraction.id
+        for subtraction in [
+            await fake.subtractions.create(user=user, upload=upload) for _ in range(2)
+        ]
+    ]
 
-    assert result == snapshot
+    documents = [
+        {"id": "sub_1", "subtractions": subtraction_ids},
+        [
+            {"id": "sub_1", "subtractions": subtraction_ids},
+            {"id": "sub_2", "subtractions": [subtraction_ids[0]]},
+            {"id": "sub_3", "subtractions": []},
+        ],
+    ]
+
+    for document in documents:
+        result = await apply_transforms(document, [AttachSubtractionsTransform(mongo)])
+
+        assert result == snapshot
 
 
-async def test_get_linked_samples(mongo):
+async def test_get_linked_samples(fake: DataFaker, mongo):
+    user = await fake.users.create()
+    upload = await fake.uploads.create(
+        user=user, upload_type=UploadType.subtraction, name="foobar.fq.gz"
+    )
+    subtraction = await fake.subtractions.create(user=user, upload=upload)
+
     await mongo.samples.insert_many(
         [
-            {"_id": "foo", "name": "Foo", "subtractions": ["1", "5", "3"]},
-            {"_id": "bar", "name": "Bar", "subtractions": ["2", "5", "8"]},
+            {"_id": "foo", "name": "Foo", "subtractions": ["1", subtraction.id, "3"]},
+            {"_id": "bar", "name": "Bar", "subtractions": ["2", subtraction.id, "8"]},
             {"_id": "baz", "name": "Baz", "subtractions": ["2"]},
         ],
         session=None,
     )
 
-    samples = await virtool.subtractions.db.get_linked_samples(mongo, "5")
+    samples = await virtool.subtractions.db.get_linked_samples(mongo, subtraction.id)
 
     assert samples == [{"id": "foo", "name": "Foo"}, {"id": "bar", "name": "Bar"}]
 
 
-async def test_unlink_default_subtractions(mongo):
+async def test_unlink_default_subtractions(fake: DataFaker, mongo):
+    user = await fake.users.create()
+    upload = await fake.uploads.create(
+        user=user,
+        upload_type=UploadType.subtraction,
+        name="foobar.fq.gz",
+    )
+
+    subtraction = await fake.subtractions.create(user=user, upload=upload)
+
     await mongo.samples.insert_many(
         [
-            {"_id": "foo", "subtractions": ["1", "2", "3"]},
-            {"_id": "bar", "subtractions": ["2", "5", "8"]},
-            {"_id": "baz", "subtractions": ["2"]},
+            {"_id": "foo", "subtractions": ["1", subtraction.id, "3"]},
+            {"_id": "bar", "subtractions": [subtraction.id, "5", "8"]},
+            {"_id": "baz", "subtractions": [subtraction.id]},
         ],
         session=None,
     )
 
     async with mongo.create_session() as session:
-        await unlink_default_subtractions(mongo, "2", session)
+        await unlink_default_subtractions(mongo, subtraction.id, session)
 
     assert await mongo.samples.find().to_list(None) == [
         {"_id": "foo", "subtractions": ["1", "3"]},
