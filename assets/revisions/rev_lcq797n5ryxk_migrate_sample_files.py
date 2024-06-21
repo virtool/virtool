b73b3e631ebb16a8b741980a01393c303f67c2b4
Reece Hoffmann <reecehoffmann@outlook.com>
2024-06-21 12:50:33 -0700

    fix: add sample file migration

---
+++ assets/revisions/rev_lcq797n5ryxk_migrate_sample_files.py
@@ -0,0 +1,321 @@
+"""migrate sample files
+
+Revision ID: lcq797n5ryxk
+Date: 2024-06-07 17:11:53.878606
+
+"""
+
+import os
+from asyncio import to_thread
+from pathlib import Path
+
+import arrow
+from motor.motor_asyncio import AsyncIOMotorDatabase
+from pymongo import ReturnDocument
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from virtool_core.utils import compress_file, file_stats
+
+from virtool.migration import MigrationContext
+from virtool.samples.models import SQLSampleReads
+from virtool.samples.utils import join_legacy_read_paths
+from virtool.uploads.models import SQLUpload
+
+# Revision identifiers.
+name = "migrate sample files"
+created_at = arrow.get("2024-06-07 17:11:53.878606")
+revision_id = "lcq797n5ryxk"
+
+alembic_down_revision = "None"
+virtool_down_revision = "zma2wj6b39hs"
+
+# Change this if an Alembic revision is required to run this migration.
+required_alembic_revision = None
+
+
+def check_is_legacy(sample: dict[str, any]) -> bool:
+    """Check if a sample has legacy read files.
+
+    :param sample: the sample document
+    :return: legacy boolean
+    """
+    files = sample.get("files")
+
+    return (
+        # All files have the `raw` flag set false indicating they are legacy data.
+        all(file.get("raw", False) is False for file in files)
+        and
+        # File naming matches expectations.
+        files[0]["name"] == "reads_1.fastq"
+        and (sample["paired"] is False or files[1]["name"] == "reads_2.fastq")
+    )
+
+
+def check_is_compressed(sample: dict[str, any]) -> bool:
+    """Check if a sample has compressed read files.
+
+    :param sample: the sample document
+    :return: compressed boolean
+    """
+    files = sample.get("files")
+
+    if not files or not sample.get("is_legacy") or sample.get("is_compressed"):
+        return True
+
+    names = [file["name"] for file in files]
+
+    return all(name in {"reads_1.fq.gz", "reads_2.fq.gz"} for name in names)
+
+
+def join_legacy_read_paths(data_path: Path, sample):
+    """Create a list of paths for the read files associated with the `sample`.
+
+    :param config: the application configuration
+    :param sample: the sample document
+    :return: a list of sample read paths
+    """
+    sample_path = data_path / "samples" / sample["_id"]
+
+    if sample["paired"]:
+        return [
+            sample_path / "reads_1.fastq",
+            sample_path / "reads_2.fastq",
+        ]
+
+    return [sample_path / "reads_1.fastq"]
+
+
+async def compress_sample_reads(
+    db: AsyncIOMotorDatabase,
+    data_path: Path,
+    sample: dict[str, any],
+):
+    """Compress the reads for one legacy samples.
+
+    :param db: the application database object
+    :param data_path: the location of the data directory
+    :param sample: the sample document
+
+    """
+    paths = join_legacy_read_paths(data_path, sample)
+    sample_id = sample["_id"]
+
+    files = []
+
+    for i, path in enumerate(paths):
+        target_filename = (
+            "reads_1.fq.gz" if "reads_1.fastq" in str(path) else "reads_2.fq.gz"
+        )
+
+        target_path = data_path / "samples" / sample_id / target_filename
+
+        await to_thread(compress_file, path, target_path, 1)
+
+        stats = await to_thread(file_stats, target_path)
+
+        files.append(
+            {
+                "name": target_filename,
+                "download_url": f"/download/samples/{sample_id}/{target_filename}",
+                "size": stats["size"],
+                "raw": False,
+                "from": sample["files"][i]["from"],
+            },
+        )
+
+    sample = await db.samples.find_one_and_update(
+        {"_id": sample_id},
+        {"$set": {"files": files, "is_compressed": True}},
+        return_document=ReturnDocument.AFTER,
+    )
+
+    for path in paths:
+        await to_thread(os.remove, path)
+
+    return sample
+
+
+async def upgrade(ctx: MigrationContext):
+    async for sample in ctx.mongo.samples.find({"files": {"$exists": True}}):
+        files = sample.get("files")
+        sample["files"] = [files] if isinstance(files, dict) else files
+
+        if "is_legacy" not in sample:
+            sample["is_legacy"] = check_is_legacy(sample)
+            ctx.mongo.samples.update_one(
+                {"_id": sample["_id"]},
+                {"$set": {"is_legacy": sample["is_legacy"]}},
+            )
+
+        if not check_is_compressed(sample):
+            sample = await compress_sample_reads(ctx.mongo, ctx.data_path, sample)
+
+        async with AsyncSession(
+            ctx.pg,
+        ) as pg_session, await (
+            ctx.mongo.client.start_session()
+        ) as mongo_session, mongo_session.start_transaction():
+            sample_id = sample["_id"]
+            for file in sample["files"]:
+                from_ = file.get("from")
+
+                upload = SQLUpload(
+                    name=from_["name"],
+                    name_on_disk=from_["id"],
+                    size=from_["size"],
+                    uploaded_at=from_.get("uplo aded_at"),
+                    removed=True,
+                    reserved=True,
+                )
+
+                reads = SQLSampleReads(
+                    name=file["name"],
+                    name_on_disk=file["name"],
+                    size=file["size"],
+                    sample=sample_id,
+                )
+
+                upload.reads.append(reads)
+
+                pg_session.add_all([upload, reads])
+
+            await ctx.mongo.samples.update_one(
+                {"_id": sample_id},
+                {"$unset": {"files": ""}},
+                session=mongo_session,
+            )
+
+            await pg_session.flush()
+            await pg_session.commit()
+
+
+async def test_upgrade(ctx, snapshot):
+    async with ctx.pg.begin() as conn:
+        await conn.run_sync(SQLSampleReads.metadata.create_all)
+        await conn.run_sync(SQLUpload.metadata.create_all)
+        await conn.commit()
+
+    samples = [
+        {
+            "_id": "modern_sample",
+            "is_legacy": False,
+            "paired": False,
+        },
+        {
+            "_id": "unpaired_legacy",
+            "is_legacy": True,
+            "paired": False,
+            "files": [
+                {
+                    "name": "reads_1.fastq",
+                    "size": 1,
+                    "raw": True,
+                    "from": {
+                        "id": "unpaired_legacy.fastq",
+                        "name": "unpaired_legacy.fastq",
+                        "size": 1,
+                    },
+                },
+            ],
+        },
+        {
+            "_id": "paired_unknown_legacy",
+            "paired": True,
+            "files": [
+                {
+                    "name": "reads_1.fastq",
+                    "size": 1,
+                    "raw": False,
+                    "from": {
+                        "id": "paired_unknown_legacy.fastq",
+                        "name": "paired_unknown_legacy.fastq",
+                        "size": 1,
+                    },
+                },
+                {
+                    "name": "reads_2.fastq",
+                    "size": 1,
+                    "raw": False,
+                    "from": {
+                        "id": "paired_unknown_legacy_2.fastq",
+                        "name": "paired_unknown_legacy_2.fastq",
+                        "size": 1,
+                    },
+                },
+            ],
+        },
+        {
+            "_id": "unpaired_unknown_legacy",
+            "paired": False,
+            "files": [
+                {
+                    "name": "reads_1.fastq",
+                    "size": 1,
+                    "raw": False,
+                    "from": {
+                        "id": "unpaired_unknown_legacy.fastq",
+                        "name": "unpaired_unknown_legacy.fastq",
+                        "size": 1,
+                    },
+                },
+            ],
+        },
+        {
+            "_id": "unpaired_legacy_compressed",
+            "paired": False,
+            "is_legacy": True,
+            "files": [
+                {
+                    "name": "reads_1.fq.gz",
+                    "size": 1,
+                    "raw": False,
+                    "from": {
+                        "id": "unpaired_unknown_legacy_compressed.fastq",
+                        "name": "unpaired_unknown_legacy_compressed.fastq",
+                        "size": 1,
+                    },
+                },
+            ],
+        },
+        {
+            "_id": "unpaired_unknown_legacy_file_object",
+            "paired": False,
+            "files": {
+                "name": "reads_1.fastq",
+                "size": 1,
+                "raw": False,
+                "from": {
+                    "id": "unpaired_unknown_legacy_file_object.fastq",
+                    "name": "unpaired_unknown_legacy_file_object.fastq",
+                    "size": 1,
+                },
+            },
+        },
+    ]
+
+    for sample in samples:
+        read_path = ctx.data_path / "samples" / sample["_id"]
+        read_path.mkdir(parents=True, exist_ok=True)
+        files = sample.get("files")
+
+        if files:
+            files = [files] if isinstance(files, dict) else files
+            for file in files:
+                read_path.joinpath(file["name"]).write_text(file["name"])
+
+    await ctx.mongo.samples.insert_many(samples)
+
+    await upgrade(ctx)
+
+    assert [sample async for sample in ctx.mongo.samples.find({})] == snapshot
+
+    async with AsyncSession(ctx.pg) as session:
+        assert (
+            await session.execute(select(SQLUpload))
+        ).unique().scalars().all() == snapshot
+        assert (
+            await session.execute(select(SQLSampleReads))
+        ).scalars().all() == snapshot
+
+    for sample in samples:
+        assert os.listdir(ctx.data_path / "samples" / sample["_id"]) == snapshot
