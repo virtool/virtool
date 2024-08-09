a5c6680eb03b13969152171b2da61340af665a87
Reece Hoffmann <reecehoffmann@outlook.com>
2024-08-09 11:41:54 -0700

    fix: determine unknown file size when migrating files

--- assets/revisions/rev_ulapmx8i3vpg_move_files_to_sql.py
+++ assets/revisions/rev_ulapmx8i3vpg_move_files_to_sql.py
@@ -1,5 +1,4 @@
-"""
-move files to sql
+"""move files to sql
 
 Revision ID: ulapmx8i3vpg
 Date: 2024-05-16 22:44:08.942465
@@ -34,6 +33,13 @@ async def upgrade(ctx: MigrationContext):
                 )
             ).scalar()
 
+            size = document.get("size")
+            if size is None:
+                file_path = ctx.data_path / "files" / document["_id"]
+                size = file_path.stat().st_size
+
+            user = document["user"]
+
             if not exists:
                 session.add(
                     SQLUpload(
@@ -42,11 +48,11 @@ async def upgrade(ctx: MigrationContext):
                         ready=document["ready"],
                         removed=False,
                         reserved=document["reserved"],
-                        size=document["size"],
+                        size=size,
                         type=document["type"],
-                        user=document["user"]["id"],
+                        user=user if user is None else user["id"],
                         uploaded_at=document["uploaded_at"],
-                    )
+                    ),
                 )
 
         await session.commit()
@@ -89,8 +95,21 @@ async def test_upgrade(ctx: MigrationContext, snapshot):
                 "user": {"id": "user_3_id"},
                 "uploaded_at": arrow.get("2024-05-16T22:44:08.942465").naive,
             },
-        ]
+            {
+                "_id": "file_4.fasta.gz",
+                "name": "File 4",
+                "ready": True,
+                "reserved": False,
+                "type": "reads",
+                "user": None,
+                "uploaded_at": arrow.get("2024-05-16T22:44:08.942465").naive,
+            },
+        ],
     )
+
+    read_path = ctx.data_path / "files"
+    read_path.mkdir(parents=True, exist_ok=True)
+    read_path.joinpath("file_4.fasta.gz").write_text("file contents")
 
     async with AsyncSession(ctx.pg) as session:
         session.add(
@@ -104,7 +123,7 @@ async def test_upgrade(ctx: MigrationContext, snapshot):
                 type="subtraction",
                 user="user_3",
                 uploaded_at=arrow.get("2024-05-16T22:44:08.942465").naive,
-            )
+            ),
         )
         await session.commit()
 
