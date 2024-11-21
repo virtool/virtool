63768d0060fa2e41a9e3f9c5ac8e03074ca8acc2
Reece Hoffmann <reecehoffmann@outlook.com>
2024-11-21 12:17:31 -0800

    fix: ensure subtraction `created_at` `nickname` and `file.name` fields are set

---
+++ assets/revisions/rev_oxu8ghlvuqmh_update_subtraction_nicknames_created_at_and_file_names.py
@@ -0,0 +1,88 @@
+"""update subtraction nicknames, created_at and file names
+
+Revision ID: oxu8ghlvuqmh
+Date: 2024-11-21 18:03:09.177151
+
+"""
+
+import datetime
+
+import arrow
+from syrupy.matchers import path_type
+
+from virtool.migration import MigrationContext
+
+# Revision identifiers.
+name = "update subtraction nicknames created_at and file names"
+created_at = arrow.get("2024-11-21 18:03:09.177151")
+revision_id = "oxu8ghlvuqmh"
+
+alembic_down_revision = "b79c1e6cf56c"
+virtool_down_revision = None
+
+# Change this if an Alembic revision is required to run this migration.
+required_alembic_revision = None
+
+
+async def upgrade(ctx: MigrationContext):
+    await ctx.mongo.subtractions.update_many(
+        {"nickname": {"$exists": False}},
+        {"$set": {"nickname": ""}},
+    )
+
+    async for subtraction in ctx.mongo.subtractions.find(
+        {"created_at": {"$exists": False}},
+    ):
+        index_stats = (
+            ctx.data_path / "subtractions" / subtraction["_id"] / "reference.1.bt2"
+        ).stat()
+
+        created_at = (
+            index_stats.st_ctime
+            if index_stats.st_ctime < index_stats.st_mtime
+            else index_stats.st_mtime
+        )
+
+        await ctx.mongo.subtractions.update_one(
+            {"_id": subtraction["_id"]},
+            {"$set": {"created_at": datetime.datetime.fromtimestamp(created_at)}},
+        )
+
+    await ctx.mongo.subtractions.update_many(
+        filter={"file.name": {"$exists": False}},
+        update=[
+            {
+                "$set": {
+                    "file.name": "$file.id",
+                },
+            },
+        ],
+    )
+
+
+async def test_upgrade(ctx: MigrationContext, snapshot):
+    await ctx.mongo.subtractions.insert_many(
+        [
+            {
+                "_id": "complete",
+                "created_at": datetime.datetime.now(),
+                "nickname": "complete_nickname",
+                "file": {"name": "complete_file_name", "id": "complete_file_id"},
+            },
+            {
+                "_id": "legacy",
+                "file": {"id": "legacy_file_id"},
+            },
+        ],
+    )
+
+    subtraction_path = ctx.data_path / "subtractions" / "legacy"
+    subtraction_path.mkdir(exist_ok=True, parents=True)
+    index_file = subtraction_path / "reference.1.bt2"
+    index_file.write_text("subtraction_index")
+
+    await upgrade(ctx)
+
+    assert [
+        subtraction async for subtraction in ctx.mongo.subtractions.find({})
+    ] == snapshot(matcher=path_type({".*created_at": (datetime.datetime,)}, regex=True))
