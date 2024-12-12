1e99f445b34a9b920e3ed433828e4c6c94c13cd4
Reece Hoffmann <reecehoffmann@outlook.com>
2024-12-12 11:50:08 -0800

    chore: address review

--- assets/revisions/rev_e83uz1iz5tw2_copy_users_to_postgres.py
+++ assets/revisions/rev_e83uz1iz5tw2_copy_users_to_postgres.py
@@ -5,9 +5,12 @@ Date: 2024-12-09 21:39:37.692957
 
 """
 
+from collections.abc import Sequence
+
 import arrow
 import pytest
-from sqlalchemy import Select, insert, select
+from sqlalchemy import insert, select
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
 from syrupy import SnapshotAssertion
 
@@ -27,17 +30,11 @@ virtool_down_revision = "oxu8ghlvuqmh"
 required_alembic_revision = None
 
 
-async def upgrade(ctx: MigrationContext):
+async def upgrade(ctx: MigrationContext) -> None:
+    """Copy users from mongodb to postgres."""
     async for user in ctx.mongo.users.find({}):
         async with AsyncSession(ctx.pg) as session:
-            if (
-                await session.execute(
-                    Select(SQLUser).where(SQLUser.legacy_id == user["_id"]),
-                )
-            ).scalar():
-                continue
-
-            sqluser = SQLUser(
+            sql_user = SQLUser(
                 active=user["active"],
                 b2c_display_name=user.get("b2c_display_name"),
                 b2c_given_name=user.get("b2c_given_name"),
@@ -53,16 +50,19 @@ async def upgrade(ctx: MigrationContext):
                 settings=user["settings"],
             )
 
-            session.add(sqluser)
+            session.add(sql_user)
 
-            await session.flush()
+            try:
+                await session.flush()
+            except IntegrityError:
+                continue
 
             if groups := user["groups"]:
                 await session.execute(
                     insert(SQLUserGroup).values(
                         [
                             {
-                                "user_id": sqluser.id,
+                                "user_id": sql_user.id,
                                 "group_id": group,
                                 "primary": user["primary_group"] == group,
                             }
@@ -75,28 +75,18 @@ async def upgrade(ctx: MigrationContext):
 
 
 class TestUpgrade:
+    """Verify that users are correctly moved to postgres."""
+
     @pytest.fixture(autouse=True)
-    async def setup_postgres(self, ctx: MigrationContext):
+    async def setup(self, ctx: MigrationContext, static_time):
+        """Ensure the environment is set up for each test."""
         async with ctx.pg.begin() as conn:
             await conn.run_sync(SQLGroup.metadata.create_all)
             await conn.run_sync(SQLUserGroup.metadata.create_all)
             await conn.run_sync(SQLUser.metadata.create_all)
             await conn.commit()
 
-    @pytest.fixture()
-    def verify_snapshots(self, ctx: MigrationContext, snapshot: SnapshotAssertion):
-        async def func():
-            async with AsyncSession(ctx.pg) as session:
-                users = (
-                    (await session.execute(select(SQLUser))).unique().scalars().all()
-                )
-                assert users == snapshot
-
-        return func
-
-    @pytest.fixture()
-    def base_user(self, static_time):
-        return {
+        self.base_user = {
             "_id": "9SYJ3IWB",
             "active": True,
             "force_reset": False,
@@ -114,27 +104,35 @@ class TestUpgrade:
             },
         }
 
-    @staticmethod
-    async def test_upgrade(base_user, ctx: MigrationContext, verify_snapshots):
-        """Verify a basic mongo user without groups is correctly migrated to postgres"""
-        await ctx.mongo.users.insert_one(base_user)
-        await upgrade(ctx)
-        await verify_snapshots()
+        async def fetch_users() -> Sequence[SQLUser]:
+            async with AsyncSession(ctx.pg) as session:
+                return (await session.execute(select(SQLUser))).unique().scalars().all()
 
-    @staticmethod
-    async def test_upgrade_groups(
-        base_user,
+        self.fetch_users = fetch_users
+
+    async def test_upgrade(
+        self,
         ctx: MigrationContext,
-        verify_snapshots,
-    ):
-        """Verify a mongo user with groups is correctly migrated to postgres"""
+        snapshot: SnapshotAssertion,
+    ) -> None:
+        """Verify a basic mongo user without groups is correctly migrated to postgres."""
+        await ctx.mongo.users.insert_one(self.base_user)
+        await upgrade(ctx)
+        assert await self.fetch_users() == snapshot
+
+    async def test_upgrade_groups(
+        self,
+        ctx: MigrationContext,
+        snapshot: SnapshotAssertion,
+    ) -> None:
+        """Verify a mongo user with groups is correctly migrated to postgres."""
         async with AsyncSession(ctx.pg) as session:
             group = SQLGroup(name="test_group", permissions={})
             session.add(group)
 
             await session.flush()
 
-            user = {**base_user, "groups": [group.id], "primary_group": group.id}
+            user = {**self.base_user, "groups": [group.id], "primary_group": group.id}
 
             await session.commit()
 
@@ -142,22 +140,21 @@ class TestUpgrade:
 
         await upgrade(ctx)
 
-        await verify_snapshots()
+        assert await self.fetch_users() == snapshot
 
-    @staticmethod
     async def test_upgrade_existing_sql_user(
-        base_user,
+        self,
         ctx: MigrationContext,
-        verify_snapshots,
+        snapshot: SnapshotAssertion,
         static_time,
-    ):
+    ) -> None:
         """Verify that users that have both mongo and postgres records are
-        not migrated
+        not migrated.
         """
         async with AsyncSession(ctx.pg) as session:
             session.add(
                 SQLUser(
-                    legacy_id=base_user["_id"],
+                    legacy_id=self.base_user["_id"],
                     force_reset=False,
                     handle="pre-existing sql user",
                     last_password_change=static_time.datetime,
@@ -167,8 +164,8 @@ class TestUpgrade:
             )
             await session.commit()
 
-        await ctx.mongo.users.insert_one(base_user)
+        await ctx.mongo.users.insert_one(self.base_user)
 
         await upgrade(ctx)
 
-        await verify_snapshots()
+        assert await self.fetch_users() == snapshot
