e999d6c9445ff17cef22054f2a7d96960eafbb82
Ian Boyes <igboyes@gmail.com>
2025-09-17 15:29:06 -0700

    test: don't use bitnami postgres

--- tests/setup_postgres.sh
+++ tests/setup_postgres.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-export PGPASSWORD=virtool
-psql -v ON_ERROR_STOP=1 --username "postgres" --host "localhost" <<-EOSQL
+# The official postgres image runs this as the postgres superuser
+# and sets PGPASSWORD automatically from POSTGRES_PASSWORD
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
   CREATE USER openfga WITH PASSWORD 'openfga';
 	CREATE DATABASE openfga;
-	GRANT ALL PRIVILEGES ON DATABASE openfga TO "openfga";
-	CREATE DATABASE virtool;
-	GRANT ALL PRIVILEGES ON DATABASE virtool TO "openfga";
+	GRANT ALL PRIVILEGES ON DATABASE openfga TO openfga;
+	GRANT ALL PRIVILEGES ON DATABASE virtool TO openfga;
 EOSQL
