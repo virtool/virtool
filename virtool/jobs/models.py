963965e99a451a10bf8368de8a8fa3f2c9a7981f
Reece Hoffmann <59776400+ReeceHoffmann@users.noreply.github.com>
2026-02-20 09:51:28 -0800

    fix: prevent crash when fetching iimi jobs

--- virtool/jobs/models.py
+++ virtool/jobs/models.py
@@ -89,6 +89,7 @@ class WorkflowV2(Enum):
     CREATE_SUBTRACTION = "create_subtraction"
     NUVS = "nuvs"
     PATHOSCOPE = "pathoscope"
+    IIMI = "iimi"
 
     @classmethod
     def _missing_(cls, value):
@@ -106,6 +107,7 @@ class WorkflowCounts(BaseModel):
     create_subtraction: int = 0
     nuvs: int = 0
     pathoscope: int = 0
+    iimi: int = 0
 
 
 class JobCountsV1(BaseModel):
