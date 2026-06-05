a83082274045becacbe66ae06a1e2caace9c4a28
ReeceHoffmann <reecehoffmann@gmail.com>
2026-06-05 11:56:37 -0700

    chore: update migration chain and test limits
    - point the cache index migration at the current alembic head
    - raise container file descriptor limits for test runs
    - refresh the migration apply snapshot

--- assets/alembic/versions/840040ca7266_add_caches_last_accessed_at_index.py
+++ assets/alembic/versions/840040ca7266_add_caches_last_accessed_at_index.py
@@ -1,7 +1,7 @@
 """add caches last_accessed_at index
 
 Revision ID: 840040ca7266
-Revises: c24b524f77e3
+Revises: d28bebf9934b
 Create Date: 2026-05-27 20:24:22.698090+00:00
 
 """
@@ -10,7 +10,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "840040ca7266"
-down_revision = "c24b524f77e3"
+down_revision = "d28bebf9934b"
 branch_labels = None
 depends_on = None
 
