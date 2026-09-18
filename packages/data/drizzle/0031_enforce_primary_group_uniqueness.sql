767db4e45a2103c28449bd2cfb169bc3c7dde685
Ian Boyes <igboyes@gmail.com>
2026-09-18 11:08:23 -0700

    fix: remove duplicate primary group migration

--- packages/data/drizzle/0031_enforce_primary_group_uniqueness.sql
+++
@@ -1,8 +0,0 @@
-DO $$ BEGIN
-  IF NOT EXISTS (SELECT 1 FROM public.data_migrations
-                 WHERE key = 'primary_group_uniqueness' AND version = 1 AND status = 'passed')
-  THEN RAISE EXCEPTION 'data migration primary_group_uniqueness@1 has not passed';
-  END IF;
-END $$;--> statement-breakpoint
-DROP INDEX "primary_group_unique";--> statement-breakpoint
-CREATE UNIQUE INDEX "primary_group_unique" ON "user_groups" USING btree ("user_id") WHERE "user_groups"."primary" = true;
